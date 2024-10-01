701e10e34748dbd3ade264d51b951283a02f22ee
jemehgoh <jeremygoheh@gmail.com>
2024-10-01 12:46:23 +0800

    Add Ui class and greetUser method

---
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -0,0 +1,13 @@
+package seedu.duke.ui;
+
+/**
+ * Represents the program's user interface
+ */
+public class Ui {
+    /**
+     * Greets user upon program startup
+     */
+    public void greetUser() {
+        System.out.println("Welcome to EventManagerCLI.");
+    }
+}
