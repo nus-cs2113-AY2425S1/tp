45155adf828b7793d3536d6e860ea5172c765800
jemehgoh <jeremygoheh@gmail.com>
2024-10-02 13:53:44 +0800

    Extract welcome message in Ui to a Messages container class

---
+++ src/main/java/seedu/manager/common/Messages.java
@@ -0,0 +1,8 @@
+package seedu.manager.common;
+
+/**
+ * Represents a container class for messages for the user
+ */
+public class Messages {
+    public static final String WELCOME_MESSAGE = "Welcome to EventManagerCLI.";
+}
--- src/main/java/seedu/manager/ui/Ui.java
+++ src/main/java/seedu/manager/ui/Ui.java
@@ -1,13 +1,16 @@
 package seedu.manager.ui;
 
+import seedu.manager.common.Messages;
+
 /**
  * Represents the program's user interface
  */
 public class Ui {
+
     /**
      * Greets user upon program startup
      */
     public void greetUser() {
-        System.out.println("Welcome to EventManagerCLI.");
+        System.out.println(Messages.WELCOME_MESSAGE);
     }
 }
