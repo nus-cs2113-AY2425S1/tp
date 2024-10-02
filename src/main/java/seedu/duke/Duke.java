4388ca2831a8aa93536a7c29ab1e913b036c4582
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-02 21:28:17 +0800

    Add addCategory function in main

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        ExpenseTracker expenseTracker = new ExpenseTracker();
         Scanner in = new Scanner(System.in);
+        System.out.println("Hiya! How can I assist?");
+        while (true) {
+            String input = in.nextLine().trim();
+            if (input.startsWith("add category")) {
+                expenseTracker.addCategory(input);
     }
 }
