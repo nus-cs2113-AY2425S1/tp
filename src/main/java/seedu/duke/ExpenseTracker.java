f2c10c9e83bed9380d1a2827ad189ba0b5850670
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-02 21:24:33 +0800

    Add addCategory function

--- src/main/java/seedu/duke/ExpenseTracker.java
+++ src/main/java/seedu/duke/ExpenseTracker.java
@@ -9,3 +9,17 @@ public class ExpenseTracker {
     public ExpenseTracker() {
         this.categories = new ArrayList<>();
     }
+
+    public void addCategory(String categoryName) {
+        for (Category category: categories) {
+            if (category.getName().equalsIgnoreCase(categoryName)) {
+                System.out.println("Category '" + categoryName + "' already exists!");
+                return;
+            }
+        }
+        String trimmedCategoryName = categoryName.substring("add category".length()).trim();
+        Category newCategory = new Category(trimmedCategoryName);
+        categories.add(newCategory);
+        System.out.println("Category '" + newCategory + "' added successfully.");
+    }
+}
