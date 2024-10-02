8562e053125f5873b86d63f61a4b1b93ebb1feba
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-02 21:23:13 +0800

    Add constructor to initialise categories variable

--- src/main/java/seedu/duke/ExpenseTracker.java
+++ src/main/java/seedu/duke/ExpenseTracker.java
@@ -4,3 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExpenseTracker {
+    private List<Category> categories;
+
+    public ExpenseTracker() {
+        this.categories = new ArrayList<>();
+    }
