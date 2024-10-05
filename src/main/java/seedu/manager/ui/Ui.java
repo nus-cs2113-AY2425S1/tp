6814eca594affe3b027512d8c0aca736763436d2
jemehgoh <jeremygoheh@gmail.com>
2024-10-05 13:24:16 +0800

    Add getCommand method to Ui class to handle user input

--- src/main/java/seedu/manager/ui/Ui.java
+++ src/main/java/seedu/manager/ui/Ui.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
  */
 public class Ui {
     private static final String WELCOME_MESSAGE = "Welcome to EventManagerCLI.";
+    private static final String COMMAND_PROMPT_MESSAGE = "Enter a command: ";
 
     private final Scanner userInput;
 
@@ -19,5 +20,14 @@ public class Ui {
      */
     public void greetUser() {
         System.out.println(WELCOME_MESSAGE);
+    }
+
+    /**
+     * Gets input from the user, and prints that input back to the user
+     */
+    public void getCommand() {
+        System.out.print(COMMAND_PROMPT_MESSAGE);
+        String rawInput = this.userInput.nextLine();
+        System.out.println(rawInput);
     }
 }
