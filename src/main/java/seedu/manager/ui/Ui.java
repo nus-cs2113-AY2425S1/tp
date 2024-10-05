084853fc22abe1c8e984c1cc2cda4dab34b04b5d
jemehgoh <jeremygoheh@gmail.com>
2024-10-05 11:22:26 +0800

    Add a scanner to Ui to take in user input

--- src/main/java/seedu/manager/ui/Ui.java
+++ src/main/java/seedu/manager/ui/Ui.java
@@ -1,10 +1,18 @@
 package seedu.manager.ui;
 
+import java.util.Scanner;
+
 /**
  * Represents the program's user interface
  */
 public class Ui {
     private static final String WELCOME_MESSAGE = "Welcome to EventManagerCLI.";
+
+    private final Scanner userInput;
+
+    public Ui() {
+        this.userInput = new Scanner(System.in);
+    }
 
     /**
      * Greets user upon program startup
