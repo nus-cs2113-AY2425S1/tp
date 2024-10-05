2d76d0b4330d54369ce1fc4ecb0752ec326e5b0a
jemehgoh <jeremygoheh@gmail.com>
2024-10-05 10:31:32 +0800

    Add a new EchoCommand class to echo user input

---
+++ src/main/java/command/EchoCommand.java
@@ -0,0 +1,25 @@
+package command;
+
+/**
+ * Represents an executable echo command
+ */
+public class EchoCommand extends Command{
+    private final String userInput;
+
+    /**
+     * Constructs a new EchoCommand with the given user input
+     *
+     * @param userInput The user input
+     */
+    public EchoCommand(String userInput) {
+        this.userInput = userInput;
+    }
+
+    /**
+     * Executes echo command by printing user input
+     */
+    @Override
+    public void execute() {
+        System.out.println(this.userInput);
+    }
+}
