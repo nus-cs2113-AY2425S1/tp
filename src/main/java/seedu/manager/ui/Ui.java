932c0531bd79e95865b8e2452fdd6ddda534b890
jemehgoh <jeremygoheh@gmail.com>
2024-10-05 11:06:59 +0800

    Make WELCOME_MESSAGE private

--- src/main/java/seedu/manager/ui/Ui.java
+++ src/main/java/seedu/manager/ui/Ui.java
@@ -4,7 +4,8 @@ package seedu.manager.ui;
  * Represents the program's user interface
  */
 public class Ui {
-    public static final String WELCOME_MESSAGE = "Welcome to EventManagerCLI.";
+    private static final String WELCOME_MESSAGE = "Welcome to EventManagerCLI.";
+
     /**
      * Greets user upon program startup
      */
