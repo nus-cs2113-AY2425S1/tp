b1d4e68a3008bd8ec7f3133d370f7dbf50794fbe
jemehgoh <jeremygoheh@gmail.com>
2024-10-06 16:04:53 +0800

    Update Parser to handle exit command

--- src/main/java/seedu/manager/parser/Parser.java
+++ src/main/java/seedu/manager/parser/Parser.java
@@ -2,6 +2,7 @@ package seedu.manager.parser;
 
 import seedu.manager.command.Command;
 import seedu.manager.command.EchoCommand;
+import seedu.manager.command.ExitCommand;
 
 /**
  * Represents the command parser for EventManagerCLI
@@ -9,11 +10,20 @@ import seedu.manager.command.EchoCommand;
 public class Parser {
 
     /**
-     * Returns an EchoCommand with a given user command string
+     * Returns a command based on the given user command string
      *
      * @param command The given command string from the user
      */
-    public Command parseCommand (String command){
-        return new EchoCommand(command);
+    public Command parseCommand(String command){
+        String[] commandParts = command.split(" ", 2);
+        String commandWord = commandParts[0];
+
+        switch (commandWord) {
+        case ExitCommand.COMMAND_WORD:
+            return new ExitCommand();
+
+        default:
+            return new EchoCommand(command);
+        }
     }
 }
