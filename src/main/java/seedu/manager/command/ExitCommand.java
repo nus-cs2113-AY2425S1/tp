0eb6c3c003b1d6606c3f1dabc553ef74cde5c5a5
jemehgoh <jeremygoheh@gmail.com>
2024-10-07 08:46:21 +0800

    Extract out exit message and add Javadoc headers

--- src/main/java/seedu/manager/command/ExitCommand.java
+++ src/main/java/seedu/manager/command/ExitCommand.java
@@ -1,12 +1,18 @@
 package seedu.manager.command;
 
 /**
- *
+ * Represents an executable exit command
  */
 public class ExitCommand extends Command {
     public static final String COMMAND_WORD = "exit";
+    private static final String EXIT_MESSAGE = "Thank you for using EventManagerCLI. Goodbye!";
 
+    /**
+     * Returns a command output with an exit message
+     *
+     * @return The command output with an exit message
+     */
     public CommandOutput execute() {
-        return new CommandOutput("Thank you for using EventManagerCLI. Goodbye!", true);
+        return new CommandOutput(EXIT_MESSAGE, true);
     }
 }
