5dfc2f0a2fcd3cbd0b942c0bac783b1ab9c41c0b
ctzeyong <80164379+ctzeyong@users.noreply.github.com>
2024-10-07 14:43:40 +0800

    Add JavaDoc to new classes

--- src/main/java/seedu/duke/AddIncomeCommand.java
+++ src/main/java/seedu/duke/AddIncomeCommand.java
@@ -1,14 +1,28 @@
 package seedu.duke;
 
+/**
+ * Command to add an expense to the financial list.
+ */
 public class AddIncomeCommand extends Command {
     private double amount;
     private String description;
 
+    /**
+     * Constructs an AddIncomeCommand with the specified amount and description.
+     *
+     * @param amount The amount of the income.
+     * @param description The description of the income.
+     */
     AddIncomeCommand(double amount, String description) {
         this.amount = amount;
         this.description = description;
     }
 
+    /**
+     * Executes the command by adding the income to the provided financial list.
+     *
+     * @param list The financial list where the income will be added.
+     */
     @Override
     public void execute(FinancialList list) {
         Income income = new Income(amount, description);
--- src/main/java/seedu/duke/Income.java
+++ src/main/java/seedu/duke/Income.java
@@ -1,11 +1,26 @@
 package seedu.duke;
 
+/**
+ * Represents an income transaction.
+ * An income increases the available balance.
+ */
 public class Income extends FinancialEntry{
 
+    /**
+     * Constructs an Income object with the specified amount and description.
+     *
+     * @param amount The amount of the income.
+     * @param description A description of the income.
+     */
     public Income(double amount, String description) {
         super(amount, description, "Income");
     }
 
+    /**
+     * Returns a string representation of the income.
+     *
+     * @return A string in the format "[Income] $amount - description".
+     */
     @Override
     public String toString() {
         return "[Income] - " + description + " $ " + amount;
