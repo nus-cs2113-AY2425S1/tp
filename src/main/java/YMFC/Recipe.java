cfe8d57b5b76130fdc7555a432cac2bbb3752673
seanngja <ngjooansean@gmail.com>
2024-10-08 19:18:19 +0800

    Add Recipe class with name, ingredients and steps

---
+++ src/main/java/YMFC/Recipe.java
@@ -0,0 +1,40 @@
+package YMFC;
+
+import java.util.ArrayList;
+
+public class Recipe {
+
+    public String name;
+    public ArrayList<String> ingredients;
+    public ArrayList<String> steps;
+
+    public Recipe(String name, ArrayList<String> ingredients, ArrayList<String> steps) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public ArrayList<String> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<String> steps) {
+        this.steps = steps;
+    }
+}
--- src/main/java/YMFC/YMFC.java
+++ src/main/java/YMFC/YMFC.java
@@ -1,8 +1,8 @@
-package seedu.duke;
+package YMFC;
 
 import java.util.Scanner;
 
-public class Duke {
+public class YMFC {
     /**
      * Main entry-point for the java.duke.Duke application.
      */
