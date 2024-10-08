b32fa6017a8e7059a1f3419c66f29db656e0e161
fuyixuan <yixuanfu762@gmail.com>
2024-10-08 15:00:13 +0800

    modify the attribute of Event class

--- src/main/java/seedu/manager/event/Event.java
+++ src/main/java/seedu/manager/event/Event.java
@@ -2,35 +2,27 @@ package seedu.manager.event;
 
 public abstract class Event {
     private final String eventName;
-    private String startTime;
-    private String endTime;
-    private String venue;
-    private String description;
-    public Event(String eventName, String startTime, String endTime, String venue) {
+    private String eventTime;
+    private String eventVenue;
+    public Event(String eventName, String eventTime, String eventVenue) {
         this.eventName = eventName;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.venue = venue;
+        this.eventTime = eventTime;
+        this.eventVenue = eventVenue;
     }
     public String getEventName() {
         return eventName;
     }
-    public String getStartTime() {
-        return startTime;
+    public String getEventTime() {
+        return eventTime;
     }
-    public String getEndTime() {
-        return endTime;
+    public String getEventVenue() {
+        return eventVenue;
     }
-    public String getVenue() {
-        return venue;
+    public void setEventTime(String eventTime) {
+        this.eventTime = eventTime;
     }
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
+    public void setEventVenue(String eventVenue) {
+        this.eventVenue = eventVenue;
     }
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-    public void setVenue(String venue) {
-        this.venue = venue;
-    }
+
 }
