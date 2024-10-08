5d5e7f5b53a3581e8d08c5158d3c8a9092e139b2
louisjoety <kolouis002@gmail.com>
2024-10-09 01:19:47 +0800

    Extract out magic strings for readability

--- src/main/java/seedu/exchangecoursemapper/parser/Parser.java
+++ src/main/java/seedu/exchangecoursemapper/parser/Parser.java
@@ -5,14 +5,16 @@ import seedu.exchangecoursemapper.command.ListSchoolCommand;
 import java.util.Scanner;
 
 public class Parser {
-    private Scanner scanner = new Scanner(System.in);
+    public static final String LISTINGSCHOOLS = "list schools";
+
+    private final Scanner scanner = new Scanner(System.in);
 
     public String getUserInput() {
         return scanner.nextLine();
     }
 
     public void processUserInput(String userInput) {
-        if (userInput.startsWith("list schools")) {
+        if (userInput.startsWith(LISTINGSCHOOLS)) {
             new ListSchoolCommand().execute(userInput);
         } else {
             System.out.println(userInput);
