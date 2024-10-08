30f631d42a524f503a46526d20eb352f64a5676f
KSanjith <kodeeswaransanjith@gmail.com>
2024-10-08 20:19:20 +0800

    Create command and RecipeList classes

---
+++ src/main/java/RecipeList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class RecipeList {
+    private ArrayList<Recipe> recipes;
+    private static int counter;
+
+    public RecipeList() {
+        counter = 0;
+        recipes = new ArrayList<>();
+    }
+
+    public void addRecipe(Recipe recipe){
+        this.recipes.add(recipe);
+        counter++;
+    }
+
+    public ArrayList<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public Recipe getRecipe(int id) {
+        return recipes.get(id);
+    }
+
+    public void removeRecipe(int id) {
+        recipes.remove(id);
+        counter--;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+}
