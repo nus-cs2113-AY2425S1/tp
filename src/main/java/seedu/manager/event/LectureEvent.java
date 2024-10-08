93d89a56f2fa2420d8e80f2f121381872e2d1868
fuyixuan <yixuanfu762@gmail.com>
2024-10-08 15:32:41 +0800

    modify the subclass of Event class

--- src/main/java/seedu/manager/event/LectureEvent.java
+++ src/main/java/seedu/manager/event/LectureEvent.java
@@ -2,7 +2,7 @@ package seedu.manager.event;
 
 
 public class LectureEvent extends Event {
-    public LectureEvent(String eventName, String startTime, String endTime, String venue) {
-        super(eventName, startTime, endTime, venue);
+    public LectureEvent(String eventName, String eventTime, String eventVenue) {
+        super(eventName, eventTime, eventVenue);
     }
 }
