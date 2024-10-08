406e3d27dac30924874488f4eb48d8d9d237fba9
Atulteja <atulteja@hotmail.com>
2024-10-08 15:04:59 +0800

    Corrected the input for the CreateCommand class

--- src/main/java/command/programme/CreateCommand.java
+++ src/main/java/command/programme/CreateCommand.java
@@ -2,6 +2,7 @@ package command.programme;
 import java.util.ArrayList;
 import command.Command;
 import core.Ui;
+import programme.Exercise;
 import programme.ProgrammeList;
 import programme.Programme;
 import core.History;
@@ -11,9 +12,9 @@ import core.History;
 public class CreateCommand extends Command {
     public static final String COMMAND_WORD = "create";
     private String name;
-    private ArrayList<ArrayList<ArrayList<String>>> contents;
+    private ArrayList<ArrayList<Exercise>> contents;
 
-    public CreateCommand(String name, ArrayList<ArrayList<ArrayList<String>>> contents) {
+    public CreateCommand(String name, ArrayList<ArrayList<Exercise>> contents) {
         this.name = name;
         this.contents = contents;
     }
