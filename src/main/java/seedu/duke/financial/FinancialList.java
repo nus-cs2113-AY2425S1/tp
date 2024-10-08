af3ef03db7d74413bf27369a427fe8dafcd16a3f
tsoudibi <a220284268@gmail.com>
2024-10-08 15:45:32 +0800

    Add EditExpenseCommand and editExpense method

--- src/main/java/seedu/duke/financial/FinancialList.java
+++ src/main/java/seedu/duke/financial/FinancialList.java
@@ -37,4 +37,25 @@ public class FinancialList {
             System.out.println("OOPS!!! The entry does not exist.");
         }
     }
+
+    /**
+     * edits an expense in the list by index.
+     * @param index The index of the entry to be edited.
+     * @param amount The new amount of the expense.
+     * @param description The new description of the expense.
+     */
+    public void editExpense(int index, double amount, String description) {
+        if (index >= 0 && index < entries.size()) {
+            FinancialEntry entry = entries.get(index);
+            if (entry instanceof Expense) {
+                Expense expense = (Expense) entry;
+                expense.setAmount(amount);
+                expense.setDescription(description);
+            } else {
+                System.out.println("OOPS!!! The entry is not an expense.");
+            }
+        } else {
+            System.out.println("OOPS!!! The entry does not exist.");
+        }
+    }
 }
