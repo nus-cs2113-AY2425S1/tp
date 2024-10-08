3c4fd4db55b760c021fa643667f7b5737f63ca52
3CCLY <cluyang22@hotmail.com>
2024-10-08 20:21:38 +0800

    Add Ui function to display recipes

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  * A class representing the user interface for interacting with the user.
@@ -70,6 +71,53 @@ public class Ui {
         for (String line: input) {
             System.out.println("\t" + line);
         }
+        System.out.println(LINE);
+    }
+
+    /**
+     * Display a newly added recipe.
+     *
+     * @param addedRecipe Recipe that has been added
+     * @param listCount Number of recipes currently in the list
+     */
+    public void printAddedRecipe(String addedRecipe, int listCount) {
+        System.out.println(LINE);
+        System.out.println("\tOkie dokie, one more recipe for me:");
+        System.out.println("\t " +  addedRecipe);
+        // Conditional operator to pluralize "recipe" when listCount above 1
+        System.out.println("\tYou currently have " + listCount +
+                (listCount <= 1 ? " recipe" : " recipes") + " in your list.");
+        System.out.println(LINE);
+    }
+
+    /**
+     * Display the entire list of recipe.
+     *
+     * @param list ArrayList of recipes to be displayed
+     * @param listCount Integer representing total number of recipes in <code>list</code>
+     */
+    public void printList(ArrayList<Recipe> list, int listCount) {
+        System.out.println(LINE);
+        System.out.println("\tHere's everything in my collection so far:");
+        for (int i = 0; i < listCount; i++) {
+            System.out.println("\t" + (i + 1) + "." + list.get(i));
+        }
+        System.out.println(LINE);
+    }
+
+    /**
+     * Display a recipe that been deleted.
+     *
+     * @param deletedRecipe Recipe that has been deleted
+     * @param listCount Integer representing number of recipes in the list currently
+     */
+    public void printDeletedTask(String deletedRecipe, int listCount) {
+        System.out.println(LINE);
+        System.out.println("\tAww, I shall begrudgingly let go of this recipe:");
+        System.out.println("\t  " + deletedRecipe);
+        // Conditional operator to pluralize "recipe" when listCount above 1
+        System.out.println("\tYou currently have " + listCount +
+                (listCount <= 1 ? " recipe" : " recipes") + " in your list.");
         System.out.println(LINE);
     }
 }
