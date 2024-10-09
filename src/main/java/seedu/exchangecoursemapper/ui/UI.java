a5a1603dd75d7afaa31f26817c09fdf3aee25274
wahkia <gohah021@gmail.com>
2024-10-09 17:31:34 +0800

    Add code for listing commands and refactor welcome message
    This commit introduces a new command for listing available commands.
    Users can now view a list of commands with a short description of its
    functionality. The parser has been refactored to handle the new command.
    The UI code has also been refactored to display an eye-catching welcome
    message.

--- src/main/java/seedu/exchangecoursemapper/ui/UI.java
+++ src/main/java/seedu/exchangecoursemapper/ui/UI.java
@@ -3,8 +3,15 @@ import seedu.exchangecoursemapper.parser.Parser;
 
 public class UI {
     public void displayGreeting() {
-        String greetingMessage = "LOREM IPSUM";
-        System.out.println(greetingMessage);;
+        String greetingMessage = "Welcome to ExchangeCourseMapper! The easiest way to plan your exchange courses.";
+        String banner = """
+                 ________ _______ _____   ______ _______ _______ _______\s
+                |  |  |  |    ___|     |_|      |       |   |   |    ___|
+                |  |  |  |    ___|       |   ---|   -   |       |    ___|
+                |________|_______|_______|______|_______|__|_|__|_______|
+                """;
+        System.out.println(banner);
+        System.out.println(greetingMessage);
     }
 
     public void runChat(Parser parser) {
