9058abcc673b3a99e7dcee8f9c1662acea3ed186
Ridiculouswifi <thtan02@gmail.com>
2024-10-09 17:03:27 +0800

    Resolve Checkstyle Errors

--- src/test/java/seedu/commands/UpdateCommandTest.java
+++ src/test/java/seedu/commands/UpdateCommandTest.java
@@ -5,7 +5,6 @@ import seedu.duke.Internship;
 import seedu.duke.InternshipList;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
