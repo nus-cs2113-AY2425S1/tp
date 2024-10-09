1b19643574876b71c3f174e4f9c688b9570129c9
ChunJia <chiew_chunjia@u.nus.edu>
2024-10-10 00:39:21 +0800

    Implement filter courses command

---
+++ src/main/java/seedu/exchangecoursemapper/command/FilterCoursesCommand.java
@@ -0,0 +1,52 @@
+package seedu.exchangecoursemapper.command;
+
+import seedu.exchangecoursemapper.exception.Exception;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+
+public class FilterCoursesCommand extends Command {
+    @Override
+    public void execute(String userInput) {
+        try (JsonReader jsonReader = Json.createReader(new FileReader(FILE_PATH))) {
+            JsonObject jsonObject = jsonReader.readObject();
+            String[] userInputDetails = userInput.split(" ");
+            String courseToFind = userInputDetails[1];
+            displayMappableCourses(jsonObject, courseToFind.toLowerCase());
+        } catch (IOException e) {
+            System.err.println(Exception.fileReadError());
+        }
+    }
+
+    public void displayMappableCourses(JsonObject jsonObject, String courseToFind) {
+        Set<String> universityNames = jsonObject.keySet();
+        boolean isCourseFound = false;
+        for (String universityName : universityNames) {
+            JsonArray courses = jsonObject.getJsonObject(universityName).getJsonArray("courses");
+
+            for (int i = 0; i < courses.size(); i += 1) {
+                JsonObject course = courses.getJsonObject(i);
+                String nusCourseCode = course.getString("nus_course_code");
+
+                // Check if the "nus_course_code" matches the input
+                if (nusCourseCode.toLowerCase().equals(courseToFind)) {
+                    String puCourseCode = course.getString("pu_course_code");
+                    // System.out.println("NUS Course Code: " + nusCourseCode);
+                    System.out.println("Partner University: " + universityName);
+                    System.out.println("Partner University Course Code: " + puCourseCode);
+                    System.out.println("---------------------------------");
+                    isCourseFound = true;
+                }
+            }
+        }
+
+        if (!isCourseFound) {
+            System.out.println("No courses found for the given course code.");
+        }
+    }
+}
