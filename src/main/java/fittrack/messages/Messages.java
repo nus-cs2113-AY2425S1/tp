ac033987917e77b39d66edf35ec02e49a8a410ab
Zackermax <104476724+Zackermax@users.noreply.github.com>
2024-10-09 14:32:21 +0800

    Feat: Add command parser, ui and savefile framework

---
+++ src/main/java/fittrack/messages/Messages.java
@@ -0,0 +1,23 @@
+package fittrack.messages;
+
+public class Messages {
+    public static final String SEPARATOR = "____________________________________________________________"; // Separator line to demarcate output messages
+
+    public static final String SET_USER_COMMAND = "set";
+    public static final String ADD_SESSION_COMMAND = "add";
+    public static final String EDIT_SESSION_COMMAND = "edit";
+    public static final String LIST_SESSIONS_COMMAND = "list";
+    public static final String VIEW_SESSION_COMMAND = "view";
+    public static final String DELETE_SESSION_COMMAND = "delete";
+    public static final String EXIT_COMMAND = "exit";
+
+    public static final String INIT_SENTENCE = "Hello! I'm FitTrack." + System.lineSeparator() + "Please input your gender and age:"; // Initial greeting
+    public static final String LIST_MESSAGE = "Here are your training sessions:"; // Message when listing training sessions
+    public static final String LIST_EMPTY_MESSAGE = "Your list is currently empty."; // Message when no training sessions are in the list
+    public static final String ADD_SESSION_MESSAGE = "Got it. I've added a new training session:" + System.lineSeparator() + "  "; // Message when a new training session is added
+    public static final String DELETE_SESSION_MESSAGE = "Got it. I've deleted this training session:" + System.lineSeparator() + "  "; // Message when a training session is deleted
+    public static final String NONNUMERICAL_INDEX_MESSAGE = "Please provide a valid numerical index."; // Message for non-numeric index
+    public static final String INDEX_OUT_OF_BOUNDS_MESSAGE = "Sorry, but that index is not within the list."; // Message for out-of-bounds index
+    public static final String INVALID_INPUT_MESSAGE = "I'm sorry, I don't know what that means."; // Message for invalid input
+    public static final String EXIT_MESSAGE = "Bye! Hope to see you again soon!"; // Exit message
+}
---
+++ src/main/java/fittrack/storage/Storage.java
@@ -0,0 +1,80 @@
+package fittrack.storage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Storage {
+
+    public static final String SAVE_FILE = "data/saveFile.txt"; // Path to the save file
+    public static final File saveFile = new File(SAVE_FILE); // File object for the save file
+
+    /**
+     * Initializes the save file by creating the necessary directories and file if they do not exist.
+     */
+    public static void initialiseSaveFile() {
+        Path dirPath = Paths.get("data"); // Path to the data directory
+        Path filePath = dirPath.resolve("saveFile.txt"); // Path to the save file
+        File file = new File(filePath.toString()); // File object for the save file
+
+        // Create directories if they do not exist
+        if (!file.exists()) {
+            try {
+                Files.createDirectories(dirPath);
+            } catch (IOException e) {
+                System.out.println("Directory could not be created");
+                throw new RuntimeException(e);
+            }
+        }
+
+        // Create a save file if it does not already exist
+        try {
+            if (file.createNewFile()) {
+                System.out.println("No existing save file found. New save file created: " + file.getName());
+            } else {
+                System.out.println("Accessing existing save file...");
+            }
+        } catch (IOException e) {
+            System.out.println("Save file could not be created.");
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Loads the sessions from the save file into the given session list.
+     *
+     * @param sessionList The list to populate with sessions from the save file.
+     * @throws FileNotFoundException If the save file is not found.
+     */
+    public static void loadSaveFile(ArrayList<TrainingSession> sessionList) throws FileNotFoundException {
+        Scanner s = new Scanner(saveFile); // Create a Scanner to read the save file
+        while (s.hasNext()) {
+            String line = s.nextLine(); // Read each line from the file
+
+            // IMPLEMENT READ SAVE FILE HERE
+
+        }
+        System.out.println("Save file successfully loaded.");
+    }
+
+    /**
+     * Updates the save file with the current list of sessions.
+     *
+     * @param sessionList The list of sessions to be saved.
+     * @throws IOException If an I/O error occurs while writing to the file.
+     */
+    public static void updateSaveFile(ArrayList<TrainingSession> sessionList) throws IOException {
+        try (FileWriter fw = new FileWriter(saveFile)) {
+            for (TrainingSession session : sessionList) {
+                fw.write(session.toString()); // Write the session to the file
+                fw.write(System.lineSeparator()); // Add a new line after each session
+            }
+        }
+    }
+}
