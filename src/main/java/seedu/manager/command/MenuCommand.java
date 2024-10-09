f44586a0e42899a973428bedc37725f723a015c4
fuyixuan <yixuanfu762@gmail.com>
2024-10-09 21:27:41 +0800

    .

--- src/main/java/seedu/manager/command/MenuCommand.java
+++ src/main/java/seedu/manager/command/MenuCommand.java
@@ -7,13 +7,14 @@ public class MenuCommand extends Command {
     public static final String COMMAND_WORD = "menu";
     private static final String MENU_MESSAGE = """
             Here are the possible commands:
-           \s
+           
             add -e EVENT_NAME: Add an event to the event list.
             list: List events.
             remove -e EVENT_NAME: Remove an event from the event list.
             add -p PARTICIPANT_NAME -e EVENT_NAME: Add a participant an event.
             view -e EVENT_NAME: View the list of participants of an event.
-            remove -p PARTICIPANT_NAME -e EVENT_NAME: Remove a participant from an event.\s""";
+            remove -p PARTICIPANT_NAME -e EVENT_NAME: Remove a participant from an event.
+            """;
 
     /**
      * Returns a command output with the menu message
