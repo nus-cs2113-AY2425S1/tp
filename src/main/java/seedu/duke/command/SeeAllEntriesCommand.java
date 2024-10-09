6acc352be37a100a95e0d4e84d53d84957e9f352
ctzeyong <80164379+ctzeyong@users.noreply.github.com>
2024-10-09 15:59:45 +0800

    Add function SeeAllEntriesCommand

---
+++ src/main/java/seedu/duke/command/SeeAllEntriesCommand.java
@@ -0,0 +1,21 @@
+package seedu.duke.command;
+
+import seedu.duke.financial.FinancialEntry;
+import seedu.duke.financial.FinancialList;
+
+/**
+ * Command to print all incomes recorded in the financial list.
+ */
+public class SeeAllEntriesCommand extends Command {
+
+    public SeeAllEntriesCommand() {}
+
+    @Override
+    public void execute(FinancialList list) {
+        System.out.println("Here's a list of all recorded entries:");
+        for (int i = 0; i < list.getEntryCount(); i++) {
+            FinancialEntry entry = list.getEntry(i);
+            System.out.println((i + 1) + ". " + entry);
+        }
+    }
+}
