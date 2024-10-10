3f06eea5c4a74f9a37b2d6e429a24198bb235d27
hzxnancy <hzxnancy@gmail.com>
2024-10-10 23:58:12 +0800

    Create storage for saved course mappings

--- src/main/java/seedu/exchangecoursemapper/storage/Storage.java
+++ src/main/java/seedu/exchangecoursemapper/storage/Storage.java
@@ -4,4 +4,10 @@ import java.util.ArrayList;
 
 public class Storage {
     // For our personal tracker
+    private ArrayList<String> savedCourses = new ArrayList<>();
+
+    public void addCourse(String course) {
+        savedCourses.add(course);
+    }
+
 }
