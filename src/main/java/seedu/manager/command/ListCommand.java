dfba7e01fa1ee8141980184c0ee4e9000b67cfdb
fuyixuan <yixuanfu762@gmail.com>
2024-10-11 01:41:30 +0800

    Modify the string format

--- src/main/java/seedu/manager/command/ListCommand.java
+++ src/main/java/seedu/manager/command/ListCommand.java
@@ -2,7 +2,8 @@ package seedu.manager.command;
 
 public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
-    private static final String LIST_MESSAGE = "There are %1$d events in your list! Here are your scheduled events:";
+    private static final String LIST_MESSAGE = "There are %1$d events in your list! " +
+            "Here are your scheduled events:";
 
     /**
      * Returns a command output with a list message
@@ -12,7 +13,7 @@ public class ListCommand extends Command {
     public CommandOutput execute() {
         StringBuilder outputMessage = new StringBuilder(String.format(LIST_MESSAGE, eventList.getListSize()) + "\n");
         for (int i = 0; i < eventList.getListSize(); i++) {
-            outputMessage.append(i + 1).append(". ").append(eventList.getEvent(i).toString()).append("\n");
+            outputMessage.append(String.format("%d. %s\n", i + 1, eventList.getEvent(i).toString()));
         }
 
         return new CommandOutput(outputMessage.toString(), false);
