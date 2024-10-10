a85a2d58c8327e7df0650e164bffedfa96c7c18e
louisjoety <kolouis002@gmail.com>
2024-10-10 21:55:35 +0800

    Implement successful printing unit test for list schools

--- src/test/java/seedu/exchangecoursemapper/ListSchoolCommandTest.java
+++ src/test/java/seedu/exchangecoursemapper/ListSchoolCommandTest.java
@@ -1,16 +1,36 @@
 package seedu.exchangecoursemapper;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import seedu.exchangecoursemapper.command.ListSchoolCommand;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ListSchoolCommandTest {
 
     private ListSchoolCommand listSchoolCommand;
-    private static final String INCORRECT_FILE_PATH = "C:\\Users\\Louis Joe\\IdeaProjects\\tp\\data\\wrongfile.json";
 
     @BeforeEach
     public void setUp() {
         listSchoolCommand = new ListSchoolCommand();
+    }
+
+    @Test
+    public void execute_validJsonFile_printsUniversityNames() throws Exception {
+        String result = executeAndCaptureOutput("list schools");
+
+        assertTrue(result.contains("Chulalongkorn University"));
+        assertTrue(result.contains("The University of Melbourne"));
+        assertTrue(result.contains("The Australian National University"));
+        assertTrue(result.contains("Victoria University of Wellington"));
+        assertTrue(result.contains("The University of Western Australia"));
+    }
+
+    private String executeAndCaptureOutput(String userInput) throws Exception {
+        java.io.ByteArrayOutputStream outContent = new java.io.ByteArrayOutputStream();
+        System.setOut(new java.io.PrintStream(outContent));
+        listSchoolCommand.execute(userInput);
+        return outContent.toString();
     }
 }
 
