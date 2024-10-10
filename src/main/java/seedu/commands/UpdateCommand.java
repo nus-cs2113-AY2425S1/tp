0b4507a88f526b654f64058085f8ea517618e0e0
Ridiculouswifi <thtan02@gmail.com>
2024-10-10 12:38:23 +0800

    Revise UpdateCommand to include start date

--- src/main/java/seedu/commands/UpdateCommand.java
+++ src/main/java/seedu/commands/UpdateCommand.java
@@ -2,6 +2,10 @@ package seedu.commands;
 
 import java.util.ArrayList;
 
+//@@author Ridiculouswifi
+/**
+ * Subclass of <code>Command</code> to handle updating <code>Internship</code> attributes
+ */
 public class UpdateCommand extends Command {
     @Override
     public void execute(ArrayList<String> args) {
@@ -9,8 +13,8 @@ public class UpdateCommand extends Command {
         int internshipIndex = internshipId - 1;
         args.remove(0);
 
-        String field = "";
-        String value = "";
+        String field;
+        String value;
 
         for (String arg : args) {
             String[] words = arg.split(" ", 2);
@@ -19,22 +23,31 @@ public class UpdateCommand extends Command {
             case "skills":
             case "role":
             case "company":
+            case "start":
             case "end":
                 field = words[INDEX_FIELD];
                 value = words[INDEX_DATA].replace(field, "").trim();
-                internships.getInternship(internshipIndex).updateField(field, value);
+                internships.updateField(internshipIndex, field, value);
                 break;
             default:
                 System.out.println("Unknown flag: " + words[INDEX_FIELD]);
                 break;
             }
         }
-        System.out.println(internships.toString());
+        System.out.println(internships.getInternship(internshipIndex).toString());
     }
 
     public String getUsage() {
         return """
-                Usage: update -id {ID} -{field} {status}
+                Usage: update -id {ID} -{field} {new value}
+                
+                List of fields:
+                - status (refer to below for valid statuses)
+                - skills
+                - role
+                - company
+                - start (in MM/yy format)
+                - end (in MM/yy format)
                 
                 Choose from the following statuses:
                 - Application Pending
