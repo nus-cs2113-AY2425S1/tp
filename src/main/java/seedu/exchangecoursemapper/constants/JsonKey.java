9f3ff0ee4005ff7014a86874d86e8283542bb179
ChunJia <chiew_chunjia@u.nus.edu>
2024-10-10 23:01:23 +0800

    Extract constants for JSON keys into a separate file

---
+++ src/main/java/seedu/exchangecoursemapper/constants/JsonKey.java
@@ -0,0 +1,10 @@
+package seedu.exchangecoursemapper.constants;
+
+/* JsonKey class contains constant strings to access data in the JSON database file */
+public class JsonKey {
+    public static final String COURSES_ARRAY_LABEL = "courses";
+    public static final String NUS_COURSE_CODE_KEY = "nus_course_code";
+    public static final String PU_COURSE_CODE_KEY = "pu_course_code";
+    public static final String NUS_COURSE_NAME_KEY = "nus_course_name";
+    public static final String PU_COURSE_NAME_KEY = "pu_course_name";
+}
