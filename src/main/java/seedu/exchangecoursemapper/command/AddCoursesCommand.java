4bc621649c46e504b966b2cfdb10a469002e71b9
hzxnancy <hzxnancy@gmail.com>
2024-10-10 23:58:52 +0800

    Add exceptions for storing course mappings

--- src/main/java/seedu/exchangecoursemapper/command/AddCoursesCommand.java
+++ src/main/java/seedu/exchangecoursemapper/command/AddCoursesCommand.java
@@ -1,45 +1,64 @@
 package seedu.exchangecoursemapper.command;
 
+import seedu.exchangecoursemapper.storage.Storage;
+
 public class AddCoursesCommand extends Command {
+
+    Storage storage = new Storage();
 
     @Override
     public void execute(String userInput) {
-        String description = trimString(userInput);
-        String[] descriptionSubstring = parseAddCommand(description);
+        try {
+            String description = trimString(userInput);
+            String[] descriptionSubstrings = parseAddCommand(description);
 
-        String nusCourse = descriptionSubstring[0];
-        String pu = descriptionSubstring[1];
-        String puCourse = descriptionSubstring[2];
+            String nusCourse = descriptionSubstrings[0].trim();
+            String pu = descriptionSubstrings[1].trim();
+            String puCourse = descriptionSubstrings[2].trim();
 
-        String courseToStore = nusCourse + " | " + pu + " | " + puCourse;
-        items.add(courseToStore);
+            String courseToStore = nusCourse + " | " + pu + " | " + puCourse;
+            storage.addCourse(courseToStore);
+
+            printAddMessage(courseToStore);
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public String trimString(String string) {
         String trimmedString = string.trim();
-        String[] outputSubstrings = trimmedString.split(" ",2);
+        String[] outputSubstrings = trimmedString.split(" ", 2);
 
         if (outputSubstrings.length < 2 || outputSubstrings[1].trim().isEmpty()) {
-            System.out.println("Please provide a valid course name!");
+            throw new IllegalArgumentException("Please provide a valid course name!");
         }
 
-        //return the trimmed description without the command
         return outputSubstrings[1];
     }
 
     public String[] parseAddCommand(String input) {
         String lowerCaseInput = input.toLowerCase();
-        if (!lowerCaseInput.contains("/pu") || !lowerCaseInput.contains("/pu_course")) {
-            System.out.println("Please provide all of the valid commands: /pu, /pu_course!");
-            return null;
+
+        if (!lowerCaseInput.contains("/pu") || !lowerCaseInput.contains("/pucourse")) {
+            throw new IllegalArgumentException("Please provide all of the valid commands: /pu, /pucourse!");
         }
 
-        String[] inputSubstrings = lowerCaseInput.split("/pu|/pu_course");
+        String[] inputSubstrings = lowerCaseInput.split("/pucourse|/pu");
+
         if (inputSubstrings.length < 3) {
-            System.out.println("Please provide a valid PU or PU's course code!");
-            return null;
+            throw new IllegalArgumentException("Please provide a valid PU or PU's course code!");
+        }
+
+        if (inputSubstrings[1].trim().isEmpty() || inputSubstrings[2].trim().isEmpty()
+                || inputSubstrings[0].trim().isEmpty()) {
+            throw new IllegalArgumentException("Do not leave the description empty!");
         }
 
         return inputSubstrings;
+    }
+
+    public void printAddMessage(String addCourse) {
+        System.out.println("You have successfully added the course: " + addCourse);
     }
 }
