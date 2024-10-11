cddf03ade5eaceec4001d018f777cc99b71cc8de
kennethszj <kennethszj.0123@gmail.com>
2024-10-11 16:44:05 +0800

    Add Ui class and methods

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -1,4 +1,48 @@
 package seedu.duke.ui;
+import java.util.Scanner;
+import java.util.ArrayList;
 
+
+/**
+ * The Ui class handles the user interface for the task management application.
+ * It provides methods for displaying messages and interacting with the user.
+ */
 public class Ui {
+    private Scanner scanner;
+
+    /**
+     * Constructs a Ui object and initializes the scanner for user input.
+     */
+    public Ui() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    /**
+     * Reads a command inputted by the user.
+     *
+     * @return the user input as a String
+     */
+    public String readCommand() {
+        return scanner.nextLine();
+    }
+
+    /**
+     * Displays a horizontal line to the console.
+     */
+    public void showLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * Displays a welcome message to the user.
+     */
+    public void showWelcome() {
+        showLine();
+        System.out.println("welcome to Meditask");
+        showLine();
+    }
 }
+
+
+
+
