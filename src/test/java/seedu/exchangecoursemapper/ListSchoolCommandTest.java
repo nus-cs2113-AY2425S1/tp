e536d90314e746936e5c3867528fe2fd5495c0b3
louisjoety <kolouis002@gmail.com>
2024-10-11 20:10:25 +0800

    Remove unnecessary imports to pass checkstyle

--- src/test/java/seedu/exchangecoursemapper/ListSchoolCommandTest.java
+++ src/test/java/seedu/exchangecoursemapper/ListSchoolCommandTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.exchangecoursemapper.command.ListSchoolCommand;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ListSchoolCommandTest {
 
