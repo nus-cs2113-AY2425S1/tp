af181effc61c4f5ed631bd3f81e248f62da7d056
shaunngoh <shaungoh682@gmail.com>
2024-10-11 17:48:33 +0800

    Add basic interface and structure of JavaNinja

---
+++ src/main/java/seedu/duke/Question.java
@@ -0,0 +1,18 @@
+package seedu.duke;
+
+/* Represents a question that could be of different types (MCQ / True or False) */
+
+public abstract class Question {
+    protected String text;
+    protected String correctAnswer;
+
+    public Question(String text, String answer) {
+        this.text = text;
+        this.correctAnswer = answer;
+    }
+
+    public abstract boolean checkAnswer(String answer);
+
+    public abstract void printOptions();
+
+}
