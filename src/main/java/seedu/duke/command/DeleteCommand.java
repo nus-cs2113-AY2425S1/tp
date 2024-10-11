84398b48caffdfc288f8e406e8dd9c432604d55a
Littleme1032 <117549420+Littleme1032@users.noreply.github.com>
2024-10-11 12:28:02 +0800

    Updated DeleteCommand
    Modified DeleteCommand so that it doesnt keep printing "Entry deleted" even after failing

--- src/main/java/seedu/duke/command/DeleteCommand.java
+++ src/main/java/seedu/duke/command/DeleteCommand.java
@@ -25,7 +25,11 @@ public class DeleteCommand extends Command {
      */
     @Override
     public void execute(FinancialList list) {
-        list.deleteEntry(index - 1);  // Index correction as list is 0-based
-        System.out.println("Entry deleted.");
+        if (index >= 0 && index < list.getEntryCount()) {
+            list.deleteEntry(index);
+            System.out.println("Entry deleted.");
+        } else {
+            System.out.println("OOPS!!! The entry does not exist.");
+        }
     }
 }
