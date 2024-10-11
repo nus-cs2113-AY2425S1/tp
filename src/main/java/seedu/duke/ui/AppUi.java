4a32badab7d7c7db2224f3d888f66d2b71933b49
kestryix <verilim3802@gmail.com>
2024-10-11 12:13:02 +0800

    Add javadoc comments

--- src/main/java/seedu/duke/ui/AppUi.java
+++ src/main/java/seedu/duke/ui/AppUi.java
@@ -6,17 +6,37 @@ import seedu.duke.parser.InputParser;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * Represents the user interface (UI) of the FinanceBuddy application.
+ * Responsible for interacting with the user, capturing inputs,
+ * and processing commands related to financial tracking.
+ */
 public class AppUi {
     public FinancialList financialList;
 
+    /**
+     * Constructor to initialize the AppUi object.
+     * Initializes an empty FinancialList.
+     */
     public AppUi() {
         financialList = new FinancialList();
     }
 
+    /**
+     * Matches a given command with its corresponding action.
+     *
+     * @param command          The command input by the user.
+     * @param commandArguments A map of arguments parsed from the user's input.
+     * @return A boolean indicating whether the command was successful.
+     */
     public boolean matchCommand(String command, HashMap<String, String> commandArguments) {
         return true;
     }
 
+    /**
+     * Continuously prompts the user for input and processes commands.
+     * It runs in a loop until the input signifies to stop accepting commands.
+     */
     public void commandEntry() {
         HashMap<String, String> commandArguments;
         String input;
@@ -37,6 +57,11 @@ public class AppUi {
         }
     }
 
+    /**
+     * Displays the welcome message and the application logo to the user
+     * upon starting the application.
+     */
+
     public void displayWelcomeMessage() {
         final String logo = " ______  _                                   ____              _      _        \n" +
                 "|  ____|(_)                                 |  _ \\            | |    | |       \n" +
@@ -56,6 +81,9 @@ public class AppUi {
         System.out.println(welcomeMessage);
     }
 
+    /**
+     * Runs the application, starting with displaying the welcome message.
+     */
     public void run() {
         displayWelcomeMessage();
     }
