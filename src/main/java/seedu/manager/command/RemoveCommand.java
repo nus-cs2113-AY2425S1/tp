d9e2f36dbaf7abf6ce2f4be6f0f15cb04b27356a
KuanHsienn <liangkuanhsien@gmail.com>
2024-10-11 17:28:24 +0800

    Improve parsing for RemoveCommand

--- src/main/java/seedu/manager/command/RemoveCommand.java
+++ src/main/java/seedu/manager/command/RemoveCommand.java
@@ -7,16 +7,14 @@ public class RemoveCommand extends Command {
     public static final String REMOVE_SUCCESS = "Event removed successfully";
     public static final String REMOVE_FAILURE = "Event not found";
     protected String eventName;
-    protected EventList events;
 
-    public RemoveCommand(String desc, EventList events) {
-        this.eventName = desc;
-        this.events = events;
+
+    public RemoveCommand(String eventName) {
+        this.eventName = eventName;
     }
 
     public CommandOutput execute() {
-        boolean isRemoved = events.removeEvent(eventName);
-        this.events.getList();
+        boolean isRemoved = this.eventList.removeEvent(eventName);
         if (isRemoved) {
             return new CommandOutput(REMOVE_SUCCESS, false);
         } else {
--- src/main/java/seedu/manager/parser/Parser.java
+++ src/main/java/seedu/manager/parser/Parser.java
@@ -2,6 +2,7 @@ package seedu.manager.parser;
 
 import seedu.manager.command.Command;
 import seedu.manager.command.AddCommand;
+import seedu.manager.command.RemoveCommand;
 import seedu.manager.command.ExitCommand;
 import seedu.manager.command.MenuCommand;
 import seedu.manager.command.EchoCommand;
@@ -28,6 +29,8 @@ public class Parser {
         switch (commandWord) {
         case AddCommand.COMMAND_WORD:
             return parseAddCommand(commandParts);
+        case RemoveCommand.COMMAND_WORD:
+            return parseRemoveCommand(commandParts);
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
         case ExitCommand.COMMAND_WORD:
@@ -69,5 +72,31 @@ public class Parser {
 
         // Create and return the AddCommand with parsed parameters
         return new AddCommand(eventName, time, venue);
+    }
+
+    /**
+     * Parses the 'remove' command and its arguments.
+     *
+     * @param commandParts The split command input from the user.
+     * @return The RemoveCommand object with parsed event details.
+     */
+    private Command parseRemoveCommand(String[] commandParts) {
+        Map<String, String> parameters = new HashMap<>();
+        String currentOption = null;
+
+        for (int i = 1; i < commandParts.length; i++) {
+            String part = commandParts[i];
+            if (part.startsWith("-")) {
+                currentOption = part;
+            } else if (currentOption != null) {
+                parameters.put(currentOption, part);
+                currentOption = null;
+            }
+        }
+
+        // Extract event details from the parsed parameters
+        String eventName = parameters.get("-e"); // Must be present
+
+        return new RemoveCommand(eventName);
     }
 }
