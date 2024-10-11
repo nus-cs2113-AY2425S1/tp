acca0e67a05106d7e1e105943d1f4606f7d401fe
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-10-12 00:15:50 +0800

    Add HelpCommand class to handle help command

---
+++ src/main/java/seedu/duke/command/HelpCommand.java
@@ -0,0 +1,40 @@
+package seedu.duke.command;
+
+import seedu.duke.financial.FinancialList;
+
+/**
+ * Command to print help menu.
+ */
+public class HelpCommand extends Command{
+    /**
+     * Constructor for Help Command object.
+     */
+    public HelpCommand() {}
+
+
+    /**
+     * Executes help command by printing out help menu.
+     */
+    @Override
+    public void execute(FinancialList financialList) {
+        String helpMenu = "--------------------------------------------\n" +
+                "List of commands:\n" +
+                "--------------------------------------------\n" +
+                "1. list\n " +
+                "   - Shows a list of all logged transactions\n" +
+                "2. expense DESCRIPTION /a AMOUNT [/d DATE]\n " +
+                "   - Adds a new expense\n" +
+                "3. income DESCRIPTION /a AMOUNT [/d DATE]\n " +
+                "   - Adds a new income\n" +
+                "4. edit INDEX [/des DESCRIPTION] [/a AMOUNT] [/d DATE]\n " +
+                "   - Edits the transaction at the specified INDEX\n" +
+                "5. delete INDEX\n " +
+                "   - Deletes the transaction at the specified INDEX\n" +
+                "6. exit\n " +
+                "   - Exits the program\n" +
+                "7. help\n " +
+                "   - Shows a list of all valid commands\n" +
+                "--------------------------------------------\n";
+        System.out.println(helpMenu);
+    }
+}
