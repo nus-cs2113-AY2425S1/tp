a6cdb619a87a68797e610fdee1aa169a47b284d4
AndrewNguyen4 <nguyenminhduc5etp@gmail.com>
2024-10-11 12:53:39 +0800

    Remove wildcard import in ExpenseListTest

--- src/test/java/wheresmymoney/ExpenseListTest.java
+++ src/test/java/wheresmymoney/ExpenseListTest.java
@@ -2,7 +2,10 @@ package wheresmymoney;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class ExpenseListTest {
 
