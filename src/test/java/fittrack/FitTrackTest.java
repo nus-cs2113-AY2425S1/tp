95195f65a21918cc0c9bfbc8e922675fcad72ec7
Zackermax <104476724+Zackermax@users.noreply.github.com>
2024-10-12 17:40:07 +0800

    Feat: JUnit tests for Ui class

---
+++ src/test/java/fittrack/FitTrackTest.java
@@ -0,0 +1,11 @@
+package fittrack;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FitTrackTest {
+    @Test
+    public void sampleTest() {
+        assertEquals(0, 0);
+    }
+}
