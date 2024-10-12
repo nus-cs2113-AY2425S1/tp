9f4f0613a2ef1a10d4bee3b25de5e6d1dd0add3c
TVageesan <59349293+TVageesan@users.noreply.github.com>
2024-10-12 17:51:51 +0800

    Change 'Date' to 'LocalDateTime' in History.java

--- src/main/java/core/History.java
+++ src/main/java/core/History.java
@@ -3,11 +3,10 @@ package core;
 import programme.Day;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.HashMap;
 
 public class History {
-    private HashMap<Date, Day> history;
+    private HashMap<LocalDateTime, Day> history;
 
     public void logDay(Day completed, LocalDateTime date) {
 
