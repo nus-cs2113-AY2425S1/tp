d610c0f1b869fb070b78d703ca1fc17f5e4d6a94
louisjoety <kolouis002@gmail.com>
2024-10-12 13:54:23 +0800

    Include logger messages into list schools function

--- src/main/java/seedu/exchangecoursemapper/command/ListSchoolCommand.java
+++ src/main/java/seedu/exchangecoursemapper/command/ListSchoolCommand.java
@@ -8,21 +8,30 @@ import javax.json.JsonReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ListSchoolCommand extends Command {
+    private static final Logger logger = Logger.getLogger(ListSchoolCommand.class.getName());
+
     @Override
     public void execute(String userInput) {
+        logger.log(Level.INFO, "Executing ListSchoolCommand");
         try (JsonReader jsonReader = Json.createReader(new FileReader(FILE_PATH))) {
             JsonObject jsonObject = jsonReader.readObject();
+            logger.log(Level.INFO, "Successfully read JSON file");
             displaySchoolList(jsonObject);
         } catch (IOException e) {
+            logger.log(Level.WARNING, "Failed to read the file");
             System.err.println(Exception.fileReadError());
         }
+        logger.log(Level.INFO, "Execution of ListSchoolCommand complete");
     }
 
     private static void displaySchoolList(JsonObject jsonObject) {
         Set<String> universityNames = jsonObject.keySet();
         for (String universityName : universityNames) {
+            logger.log(Level.INFO, "Displaying university names ...");
             System.out.println(universityName);
         }
     }
