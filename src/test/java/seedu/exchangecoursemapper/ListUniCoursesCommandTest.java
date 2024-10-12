75cc5e26e3d87ffbcad286baab364f123eaa775e
Chiang-HuiXin <huixinchiang@gmail.com>
2024-10-12 18:47:56 +0800

    Implement JUnit test for getUniCourses method

--- src/test/java/seedu/exchangecoursemapper/ListUniCoursesCommandTest.java
+++ src/test/java/seedu/exchangecoursemapper/ListUniCoursesCommandTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -32,6 +37,54 @@ public class ListUniCoursesCommandTest {
         String userInput = "set";
         String getPuNameOutput = listUniCoursesCommand.getPuName(userInput);
         assertEquals("", getPuNameOutput);
+    }
+
+    @Test
+    public void getUniCourses_withValidUni_success() throws IOException {
+        JsonReader jsonReader = Json.createReader(new FileReader("./data/database.json"));
+        JsonObject jsonObject = jsonReader.readObject();
+        jsonReader.close();
+
+        String uniName = "Chulalongkorn University";
+        listUniCoursesCommand.getUniCourses(jsonObject, uniName);
+
+        String expectedOutput = """
+                ICE2190472: Netcentric Architecture
+                CS2105: Introduction to Computer Networks
+                -----------------------------------------------------
+                ICSE2190479: Graphics Computing
+                CS3241: Computer Graphics
+                -----------------------------------------------------
+                2603637: Machine Learning
+                CS3244: Machine Learning
+                -----------------------------------------------------
+                """;
+
+        String actualOutput = outputStreamCaptor.toString();
+        assertEquals(normalizeLineEndings(expectedOutput), normalizeLineEndings(actualOutput));
+    }
+
+    @Test
+    public void getUniCourses_withInvalidUni_success() throws IOException {
+        JsonReader jsonReader = Json.createReader(new FileReader("./data/database.json"));
+        JsonObject jsonObject = jsonReader.readObject();
+        jsonReader.close();
+
+        String invalidUniName = "Invalid Uni";
+
+        listUniCoursesCommand.getUniCourses(jsonObject, invalidUniName);
+
+        String expectedOutput = """
+                University not found: Invalid Uni
+                -----------------------------------------------------
+                """;
+
+        String actualOutput = outputStreamCaptor.toString();
+        assertEquals(normalizeLineEndings(expectedOutput), normalizeLineEndings(actualOutput));
+    }
+
+    String normalizeLineEndings(String input) {
+        return input.replaceAll("\\r\\n", "\n").replaceAll("\\r", "\n").trim();
     }
 
 }
