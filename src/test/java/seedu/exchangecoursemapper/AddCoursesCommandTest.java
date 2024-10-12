d8acf631066aacca52fcf8cafc83c86185828efd
hzxnancy <hzxnancy@gmail.com>
2024-10-12 15:14:13 +0800

    Add tests for parseAddCommand method

--- src/test/java/seedu/exchangecoursemapper/AddCoursesCommandTest.java
+++ src/test/java/seedu/exchangecoursemapper/AddCoursesCommandTest.java
@@ -14,15 +14,15 @@ public class AddCoursesCommandTest {
         String input = "add cs2102 /pu university of melbourne /coursepu INFO20003";
         String trimmedInput = addCoursesCommand.trimString(input);
 
-        assertEquals("cs2102 /pu university of melbourne" +
-                " /coursepu INFO20003", trimmedInput);
+        assertEquals("cs2102 /pu university of melbourne" + " /coursepu INFO20003", trimmedInput);
     }
 
     @Test
     void trimString_commandOnly_exceptionThrown() {
         String input = "add";
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                () -> {addCoursesCommand.trimString(input);});
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            addCoursesCommand.trimString(input);
+        });
         assertEquals("Please provide the nus course code, " +
                 "name of partner university (PU) and the PU course code.", exception.getMessage());
     }
@@ -30,18 +30,72 @@ public class AddCoursesCommandTest {
     @Test
     void trimString_commandWithSpaces_exceptionThrown() {
         String input = "add      ";
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                () -> {addCoursesCommand.trimString(input);});
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            addCoursesCommand.trimString(input);
+        });
         assertEquals("Please provide the nus course code, " +
                 "name of partner university (PU) and the PU course code.", exception.getMessage());
     }
 
     @Test
-    void execute() {
+    void parseAddCommand_oneCommandOnly_exceptionThrown() {
+        String input = "/pu";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            addCoursesCommand.parseAddCommand(input);
+        });
+        assertEquals("Please provide all of the valid commands: /pu, /coursepu!", exception.getMessage());
     }
 
     @Test
-    void parseAddCommand() {
+    void parseAddCommand_oneCommandOnlyWithSpaces_exceptionThrown() {
+        String input = "/pu           ";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            addCoursesCommand.parseAddCommand(input);
+        });
+        assertEquals("Please provide all of the valid commands: /pu, /coursepu!", exception.getMessage());
     }
-    
+
+    @Test
+    void parseAddCommand_mergeCommands_exceptionThrown() {
+        String input = "/pu/coursepu";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            addCoursesCommand.parseAddCommand(input);
+        });
+        assertEquals("Commands shouldn't be adjacent to one another!", exception.getMessage());
+    }
+
+    @Test
+    void parseAddCommands_missingNUSCourseCode_exceptionThrown() {
+        String input = "/pu university of melbourne /coursepu INFO20003 ";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            addCoursesCommand.parseAddCommand(input);
+        });
+        assertEquals("Please provide a valid NUS course code or PU or PU's course code!", exception.getMessage());
+    }
+
+    @Test
+    void parseAddCommands_missingPU_exceptionThrown() {
+        String input = "cs2102 /pu /coursepu INFO20003";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            addCoursesCommand.parseAddCommand(input);
+        });
+        assertEquals("Please provide a valid NUS course code or PU or PU's course code!", exception.getMessage());
+    }
+
+    @Test
+    void parseAddCommands_missingPUCourseCode_exceptionThrown() {
+        String input = "cs2102 /pu university of melbourne /coursepu";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            addCoursesCommand.parseAddCommand(input);
+        });
+        assertEquals("Please provide a valid NUS course code or PU or PU's course code!", exception.getMessage());
+    }
+
+    @Test
+    void parseAddCommand_properInput_success() {
+        String input = "cs2102 /pu university of melbourne /coursepu INFO20003";
+        String[] inputSubstrings = addCoursesCommand.parseAddCommand(input);
+        String[] expected = {"cs2102", "university of melbourne", "INFO20003"};
+        assertArrayEquals(expected, inputSubstrings);
+    }
 }
