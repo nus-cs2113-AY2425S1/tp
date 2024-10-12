338040625c177301874f135d508aeb765b979f66
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-12 20:08:21 +0800

    Refactor libraries used in DukeTest.java to prevent errors when used with Gradle

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,7 +1,7 @@
 package seedu.duke;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
