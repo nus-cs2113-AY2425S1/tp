6a117b6296881620c73fc5c39eec76cbc9bda882
Jinlin <e1346020@u.nus.edu>
2024-10-13 23:15:22 +0800

    Add JUnit tests for QuizManager.

--- src/main/java/seedu/javaninja/QuizManager.java
+++ src/main/java/seedu/javaninja/QuizManager.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class QuizManager {
     private List<Topic> topics;
     private Quiz currentQuiz;
+    private QuizManager quizManager;
 
     public QuizManager() {
         this.topics = new ArrayList<>();
@@ -16,11 +17,10 @@ public class QuizManager {
     private void loadTopics() {
         Topic loopsTopic = new Topic("Loops");
         loopsTopic.addQuestion(new Mcq(
-            "Which of the following is a loop structure in Java?",
-            "b",
-            Arrays.asList("a) if-else", "b) for", "c) switch", "d) try-catch")));
+                "Which of the following is a loop structure in Java?",
+                "b",
+                Arrays.asList("a) if-else", "b) for", "c) switch", "d) try-catch")));
         topics.add(loopsTopic);
-
     }
 
     public void selectTopic(String topicName) {
@@ -45,5 +45,19 @@ public class QuizManager {
         }
     }
 
+    // Adds a new topic to the list of topics
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    // Returns the current number of topics
+    public int getTopicsCount() {
+        return topics.size();
+    }
+
+    // Removes an existing topic from the list
+    public void removeTopic(Topic topic) {
+        topics.remove(topic);
+    }
 
 }
