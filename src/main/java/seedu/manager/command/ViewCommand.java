d79068ab45e6782c4cbb51b36311f7700bba53d3
jemehgoh <jeremygoheh@gmail.com>
2024-10-14 21:58:15 +0800

    Add error handling for view command

--- src/main/java/seedu/manager/command/ViewCommand.java
+++ src/main/java/seedu/manager/command/ViewCommand.java
@@ -2,14 +2,18 @@ package seedu.manager.command;
 
 import seedu.manager.event.Event;
 
+import java.util.Optional;
+
 /**
  * Represents a command to view the list of participants in an event.
  * The view command will search for an event by its name and display all its participants if found.
  */
-public class ViewCommand extends Command{
+public class ViewCommand extends Command {
     public static final String COMMAND_WORD = "view";
     private static final String VIEW_MESSAGE = "There are %d participants in %s! " +
             "Here are your participants:";
+    private static final String INVALID_EVENT_MESSAGE = "Event not found!";
+
     protected String eventName;
 
     /**
@@ -22,28 +26,25 @@ public class ViewCommand extends Command{
     }
 
     /**
-     * Returns a command output with a list message
+     * Returns a command output with a view message
      *
-     * @return The command output with a list message
+     * @return The command output with a view message
      */
     public CommandOutput execute() {
-        Event event = null;
-        for (int i = 0; i < eventList.getListSize(); i++) {
-            if (eventList.getEvent(i).getEventName().equals(eventName)) {
-                event = eventList.getEvent(i);
+        Optional<Event> eventToView = eventList.getEventByName(this.eventName);
+
+        if (eventToView.isPresent()) {
+            StringBuilder outputMessage = new StringBuilder(
+                    String.format(VIEW_MESSAGE, eventToView.get().getParticipantCount(), eventName) + "\n");
+            int count = 1;
+            for (String participant : eventToView.get().getParticipantList()) {
+                outputMessage.append(String.format("%d. %s\n", count, participant));
+                count++;
             }
+
+            return new CommandOutput(outputMessage.toString(), false);
+        } else {
+            return new CommandOutput(INVALID_EVENT_MESSAGE, false);
         }
-
-        //to handle errors in the future
-
-        StringBuilder outputMessage = new StringBuilder(
-                String.format(VIEW_MESSAGE, event.getParticipantCount(), eventName) + "\n");
-        int count = 1;
-        for (String participant : event.getParticipantList()) {
-            outputMessage.append(String.format("%d. %s\n", count , participant));
-            count++;
-        }
-
-        return new CommandOutput(outputMessage.toString(), false);
     }
 }
