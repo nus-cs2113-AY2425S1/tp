a99e48567dda2652eec0c03bfeb5622ef22f2eb4
ctzeyong <80164379+ctzeyong@users.noreply.github.com>
2024-10-14 21:32:03 +0800

    Add JUnit test for SeeAllEntriesCommand

---
+++ src/test/java/seedu/duke/command/SeeAllEntriesCommandTest.java
@@ -0,0 +1,95 @@
+package seedu.duke.command;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.duke.financial.Expense;
+import seedu.duke.financial.FinancialList;
+import seedu.duke.financial.Income;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Test class for the SeeAllIncomesCommand.
+ * This class contains unit tests to verify the functionality of the SeeAllIncomesCommand
+ * when executed on a FinancialList containing various financial entries.
+ */
+class SeeAllEntriesCommandTest {
+
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private FinancialList financialList;
+    private SeeAllEntriesCommand testCommand;
+
+    /**
+     * Set up the test environment before each test.
+     * Initializes the FinancialList and SeeAllIncomesCommand instances,
+     * and redirects System.out to capture console output.
+     */
+    @BeforeEach
+    void setUp() {
+        financialList = new FinancialList();
+        testCommand = new SeeAllEntriesCommand();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    /**
+     * Clean up the test environment after each test.
+     * Restores the original System.out PrintStream.
+     */
+    @AfterEach
+    void tearDown() {
+        System.setOut(originalOut);
+    }
+
+    /**
+     * Test the execute method with a mixed list of incomes and expenses.
+     * Expects entries to be printed with indexes.
+     */
+    @Test
+    void execute_mixedList_expectPrintedIncomes() {
+
+        financialList.addEntry(new Expense(3.50, "lunch"));
+        financialList.addEntry(new Income(3000.00, "salary"));
+        financialList.addEntry(new Expense(4.50, "dinner"));
+        financialList.addEntry(new Expense(20.00, "movie ticket"));
+        financialList.addEntry(new Income(100.00, "allowance"));
+        financialList.addEntry(new Income(15.00, "ang pow money"));
+
+        testCommand.execute(financialList);
+
+        String output = outputStream.toString();
+        String expectedOutput = "--------------------------------------------" + System.lineSeparator() +
+                "Here's a list of all recorded entries:" + System.lineSeparator() +
+                "1. [Expense] - lunch $ 3.50" + System.lineSeparator() +
+                "2. [Income] - salary $ 3000.00" + System.lineSeparator() +
+                "3. [Expense] - dinner $ 4.50" + System.lineSeparator() +
+                "4. [Expense] - movie ticket $ 20.00" + System.lineSeparator() +
+                "5. [Income] - allowance $ 100.00" + System.lineSeparator() +
+                "6. [Income] - ang pow money $ 15.00" + System.lineSeparator() +
+                "--------------------------------------------" + System.lineSeparator();
+
+        assertEquals(expectedOutput, output);
+    }
+
+    /**
+     * Test the execute method with an empty list.
+     * Expects a message indicating no entries were found.
+     */
+    @Test
+    void execute_onlyExpenseList_expectNothing() {
+
+        testCommand.execute(financialList);
+
+        String output = outputStream.toString();
+        String expectedOutput = "--------------------------------------------" + System.lineSeparator() +
+                "No entries found." + System.lineSeparator() +
+                "--------------------------------------------" + System.lineSeparator();
+
+        assertEquals(expectedOutput, output);
+    }
+}
+
