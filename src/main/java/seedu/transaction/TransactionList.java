f1a33365bcdf0fe60ebe783e09dcaa381b5d32ac
LinhHuynhLeDan <22520759@ms.uit.edu.vn>
2024-10-15 00:13:43 +0800

    Fix code style

--- src/main/java/seedu/transaction/TransactionList.java
+++ src/main/java/seedu/transaction/TransactionList.java
@@ -5,7 +5,6 @@ import seedu.utils.DateTimeUtils;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
