0c98ff53e55521a130fb0e2e059cd4684ffb8b59
TheDinos <marcussvps2002@gmail.com>
2024-10-15 06:05:05 +0800

    Update test

--- src/test/java/fittrack/trainingsession/TrainingSessionTest.java
+++ src/test/java/fittrack/trainingsession/TrainingSessionTest.java
@@ -26,28 +26,28 @@ public class TrainingSessionTest {
 
     @Test
     public void editExercise_PullUpStation() {
-        new TrainingSession(testDatetimeString, testDescription, testUser).editExercise(1, 1);
+        new TrainingSession(testDatetimeString, testDescription, testUser).editExercise(0, 1);
         assertEquals("Exercise edited! Here's your new input: " + System.lineSeparator() +
                 1 + System.lineSeparator(), outContent.toString());
     }
 
     @Test
     public void editExercise_ShuttleRunStation() {
-        new TrainingSession(testDatetimeString, testDescription, testUser).editExercise(2, 11);
+        new TrainingSession(testDatetimeString, testDescription, testUser).editExercise(1, 11);
         assertEquals("Exercise edited! Here's your new input: " + System.lineSeparator() +
                 11 + "s" + System.lineSeparator(), outContent.toString());
     }
 
     @Test
     public void testGetSessionDescription(){
-        assertEquals(testDescription,
+        assertEquals(testDescription + " | " + testDatetimeOutput,
                 new TrainingSession(testDatetimeString, testDescription, testUser).getSessionDescription());
     }
 
     @Test
     public void testPrintSessionDescription(){
         new TrainingSession(testDatetimeString, testDescription, testUser);
-        assertEquals(testDescription, outContent.toString());
+        assertEquals(testDescription + " | " + testDatetimeOutput, outContent.toString());
     }
 
     @Test
@@ -55,12 +55,12 @@ public class TrainingSessionTest {
         new TrainingSession(testDatetimeString, testDescription, testUser).viewSession();
         assertEquals("Training Session: " + testDescription + System.lineSeparator() +
                 "Training Datetime: " + testDatetimeOutput + System.lineSeparator() +
-                "Pull Up Station | 0" + System.lineSeparator() +
-                "Shuttle Run Station | 0" + System.lineSeparator() +
-                "Sit and Reach Station | 0" + System.lineSeparator() +
-                "Sit Up Station | 0" + System.lineSeparator() +
-                "Standing Broad Jump Station | 0" + System.lineSeparator() +
-                "Walk and Run Station | 0" + System.lineSeparator() +
+                "Pull Up Station | 0 | 0" + System.lineSeparator() +
+                "Shuttle Run Station | 0s | 0" + System.lineSeparator() +
+                "Sit and Reach Station | 0cm | 0" + System.lineSeparator() +
+                "Sit Up Station | 0 | 0" + System.lineSeparator() +
+                "Standing Broad Jump Station | 0cm | 0" + System.lineSeparator() +
+                "Walk and Run Station | 00:00 | 0" + System.lineSeparator() +
                 "Total points: " + 0 + System.lineSeparator() +
                         "Overall Award: No award" + System.lineSeparator(),
                 outContent.toString());
