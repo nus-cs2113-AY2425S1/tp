f44226cdf0dff02994bb0070b2bd90eb4ba9316f
LBTtirstocc <158111213+YubotKwng@users.noreply.github.com>
2024-10-14 16:46:28 +0800

    increment review
    the review will return score in percentage and generated simple comment based on the score

--- src/main/java/seedu/javaninja/Quiz.java
+++ src/main/java/seedu/javaninja/Quiz.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 public class Quiz {
     private Topic topic;
     private int currentQuestionIndex;
+    private int correctAnswers;
     private Scanner scanner;
 
     public Quiz(Topic topic) {
         this.topic = topic;
         this.currentQuestionIndex = 0;
+        this.correctAnswers = 0;
         this.scanner = new Scanner(System.in);
     }
 
@@ -27,15 +29,21 @@ public class Quiz {
             currentQuestionIndex++;
         }
 
-        System.out.println("Quiz finished");
+        System.out.println("Quiz finished. Your score is: " + getScore() + "%");
     }
 
     public void answerQuestion (String answer) {
         Question currentQuestion = topic.getQuestions().get(currentQuestionIndex);
         if (currentQuestion.checkAnswer(answer)) {
             System.out.println("Correct!");
+            correctAnswers++;
         } else {
             System.out.println("Incorrect!");
         }
+    }
+
+    public int getScore() {
+        int totalQuestions = topic.getQuestions().size();
+        return (int) ((double) correctAnswers / totalQuestions * 100);
     }
 }
