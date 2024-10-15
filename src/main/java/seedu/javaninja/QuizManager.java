6766eee8d0a597fb70e5f3eea01d389f4d5c2309
shaunngoh <shaungoh682@gmail.com>
2024-10-15 22:30:55 +0800

    Add Questions.txt

--- src/main/java/seedu/javaninja/QuizManager.java
+++ src/main/java/seedu/javaninja/QuizManager.java
@@ -1,26 +1,67 @@
 package seedu.javaninja;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Locale;
 
 public class QuizManager {
     private List<Topic> topics;
     private Quiz currentQuiz;
     private QuizManager quizManager;
+    private final static String FILE_PATH = "./data/Questions.txt";
 
     public QuizManager() {
         this.topics = new ArrayList<>();
-        loadTopics();
+        loadTopicsFromFile();
     }
 
-    private void loadTopics() {
-        Topic loopsTopic = new Topic("Loops");
-        loopsTopic.addQuestion(new Mcq(
-                "Which of the following is a loop structure in Java?",
-                "b",
-                Arrays.asList("a) if-else", "b) for", "c) switch", "d) try-catch")));
-        topics.add(loopsTopic);
+    private void loadTopicsFromFile() {
+        File file = new File(FILE_PATH);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                parseTopic(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
+
+    private Topic getOrCreateTopic(String topicName) {
+        for (Topic topic : topics) {
+            if (topic.getName().equalsIgnoreCase(topicName)) {
+                return topic;
+            }
+        }
+        Topic newTopic = new Topic(topicName);
+        topics.add(newTopic);
+        return newTopic;
+    }
+
+    public void parseTopic(String line) {
+        String[] parts = line.split("\\|");
+
+        String topicName = parts[0].trim();
+        String questionType = parts[1].trim();
+        String questionText = parts[2].trim();
+        String correctAnswer = parts[3].trim();
+
+        Topic topic = getOrCreateTopic(topicName);
+        switch (questionType) {
+        case "Mcq":
+            List<String> options = new ArrayList<>();
+            for (int i = 4; i < parts.length; i++) {
+                options.add(parts[i]);
+            }
+            topic.addQuestion(new Mcq(questionText, correctAnswer, options));
+            break;
+        default:
+            System.out.println("Invalid question type");
+        }
     }
 
     public void selectTopic(String topicName) {
