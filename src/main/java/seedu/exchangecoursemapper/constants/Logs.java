0977e1c08fb986398652f058031d60b5f96508c6
louisjoety <kolouis002@gmail.com>
2024-10-15 22:56:37 +0800

    Fix checkstyle errors

--- src/main/java/seedu/exchangecoursemapper/constants/Logs.java
+++ src/main/java/seedu/exchangecoursemapper/constants/Logs.java
@@ -1,7 +1,7 @@
 package seedu.exchangecoursemapper.constants;
 
 public class Logs {
-    public static final String EXECUTING_COMMAND  = "Executing Command";
+    public static final String EXECUTING_COMMAND  = "Executing command";
     public static final String SUCCESS_READ_JSON_FILE = "Successfully read JSON file";
     public static final String FAILURE_READ_JSON_FILE = "Failed to read JSON file";
     public static final String COMPLETE_EXECUTION  = "Execution complete";
