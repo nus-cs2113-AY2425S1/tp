28b1701bf897868d3a6601e8c20b4b0143f7110d
Chiang-HuiXin <huixinchiang@gmail.com>
2024-10-17 17:32:28 +0800

    Modify Assertions.java and Logs.java
    To include new constants from ListUniCoursesCommand

--- src/main/java/seedu/exchangecoursemapper/constants/Assertions.java
+++ src/main/java/seedu/exchangecoursemapper/constants/Assertions.java
@@ -1,8 +1,13 @@
 package seedu.exchangecoursemapper.constants;
 
 public class Assertions {
-    public static final String NULL_JSON_FILE  = "JSON object should not be null";
-    public static final String EMPTY_JSON_FILE  = "JSON file is empty";
+    public static final String NULL_JSON_FILE = "JSON object should not be null";
+    public static final String EMPTY_JSON_FILE = "JSON file is empty";
     public static final String NO_NUS_COURSE_CODE_PARSED = "Nus course code should not be null";
     public static final String NO_COURSE_INFORMATION = "Course information should not be null";
+    public static final String EMPTY_USER_INPUT = "User input should not be empty";
+    public static final String EMPTY_PU_NAME = "Pu name should not be empty";
+    public static final String NULL_UNIVERSITY_OBJECT = "University object should not be null";
+    public static final String NO_COURSE_OBJECT = "Course object should not be empty";
 }
+
--- src/main/java/seedu/exchangecoursemapper/constants/Logs.java
+++ src/main/java/seedu/exchangecoursemapper/constants/Logs.java
@@ -9,4 +9,12 @@ public class Logs {
     public static final String LIST_MAPPABLE_COURSES = "Displaying mappable PU courses ...";
     public static final String NO_NUS_COURSE_CODE_FILTER = "No NUS course code provided to filter";
     public static final String FILTER_COURSES_LIMIT = "More than one NUS course code provided to filter";
+    public static final String UNKNOWN_UNIVERSITY = "Unknown university encountered";
+    public static final String EXTRACT_PU_NAME = "Extracted PU name";
+    public static final String NO_PU_NAME = "Partner University name is empty";
+    public static final String SEARCH_UNIVERSITY = "Searching university ...";
+    public static final String UNIVERSITY_FOUND = "University found";
+    public static final String NO_COURSES_FOUND = "No course found for PU";
+    public static final String LISTING_COURSES = "Listing courses ...";
+    public static final String COURSE_DETAILS = "Printing course details ...";
 }
