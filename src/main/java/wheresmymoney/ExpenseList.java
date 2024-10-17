2ff9da513c151f06729240605ca9449cfad4e74d
Terence Chan Zun Mun <zunmun@gmail.com>
2024-10-17 22:25:47 +0800

    Added assertion and logging to parser

--- src/main/java/wheresmymoney/ExpenseList.java
+++ src/main/java/wheresmymoney/ExpenseList.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.logging.*;
 import java.util.ArrayList;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
