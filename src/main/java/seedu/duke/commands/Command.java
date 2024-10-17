6b75c38ace5bbd8999d8dde0ca19834bdccaf96b
Metanyu <nguyenthu.vu2511@gmail.com>
2024-10-17 23:50:40 +0800

    edit command class to follow convention

--- src/main/java/seedu/duke/commands/Command.java
+++ src/main/java/seedu/duke/commands/Command.java
@@ -3,8 +3,9 @@ package seedu.duke.commands;
 import seedu.duke.data.task.TaskList;
 
 public abstract class Command {
-    public abstract CommandResult execute();
     protected TaskList tasks;
+
+    public abstract CommandResult execute();
     public void setData(TaskList tasks) {
         this.tasks = tasks;
     }
