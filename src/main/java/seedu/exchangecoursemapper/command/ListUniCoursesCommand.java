7927a1dcfeddd975c1684b46daf097531d25cf25
Chiang-HuiXin <huixinchiang@gmail.com>
2024-10-17 17:27:09 +0800

    Add assertions and loggings for ListUniCoursesCommand

--- src/main/java/seedu/exchangecoursemapper/command/ListUniCoursesCommand.java
+++ src/main/java/seedu/exchangecoursemapper/command/ListUniCoursesCommand.java
@@ -1,5 +1,7 @@
 package seedu.exchangecoursemapper.command;
 
+import seedu.exchangecoursemapper.constants.Assertions;
+import seedu.exchangecoursemapper.constants.Logs;
 import seedu.exchangecoursemapper.exception.Exception;
 import seedu.exchangecoursemapper.exception.UnknownUniversityException;
 
@@ -10,6 +12,8 @@ import javax.json.JsonReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static seedu.exchangecoursemapper.constants.JsonKey.PU_COURSE_CODE_KEY;
 import static seedu.exchangecoursemapper.constants.JsonKey.PU_COURSE_NAME_KEY;
@@ -18,28 +22,36 @@ import static seedu.exchangecoursemapper.constants.JsonKey.NUS_COURSE_NAME_KEY;
 import static seedu.exchangecoursemapper.constants.Messages.LINE_SEPARATOR;
 
 public class ListUniCoursesCommand extends Command {
+    private static final Logger logger = Logger.getLogger(ListUniCoursesCommand.class.getName());
 
     @Override
     public void execute(String userInput) {
+        logger.log(Level.INFO, Logs.EXECUTING_COMMAND);
         try (JsonReader jsonReader = Json.createReader(new FileReader(FILE_PATH))) {
             JsonObject jsonObject = jsonReader.readObject();
+            logger.log(Level.INFO, Logs.SUCCESS_READ_JSON_FILE);
+            assert jsonObject != null : Assertions.NULL_JSON_FILE;
+            assert !jsonObject.isEmpty() : Assertions.EMPTY_JSON_FILE;
             String puName = getPuName(userInput);
             getUniCourses(jsonObject, puName);
         } catch (IOException e) {
+            logger.log(Level.WARNING, Logs.FAILURE_READ_JSON_FILE);
             System.err.println(Exception.fileReadError());
         } catch (UnknownUniversityException e) {
-            System.err.println(e.getMessage());
-            System.out.println(LINE_SEPARATOR);
-        } catch (java.lang.Exception e) {
+            logger.log(Level.WARNING, Logs.UNKNOWN_UNIVERSITY, e.getMessage());
             System.err.println(e.getMessage());
             System.out.println(LINE_SEPARATOR);
         }
     }
 
-    public String getPuName (String userInput) throws java.lang.Exception {
+    public String getPuName (String userInput) {
+        assert userInput != null : Assertions.EMPTY_USER_INPUT;
+
         String puName = userInput.replaceFirst("set", "").trim();
+        logger.log(Level.INFO, Logs.EXTRACT_PU_NAME);
 
         if (puName.isEmpty()) {
+            logger.log(Level.WARNING, Logs.NO_PU_NAME);
             throw new IllegalArgumentException(Exception.emptyUniversityName());
         }
 
@@ -47,29 +59,42 @@ public class ListUniCoursesCommand extends Command {
     }
 
     public void getUniCourses (JsonObject jsonObject, String puName) throws UnknownUniversityException {
-        String lowerCasePuName = puName.toLowerCase();
+        assert jsonObject != null : Assertions.NULL_JSON_FILE;
+        assert puName != null : Assertions.EMPTY_PU_NAME;
 
+        String lowerCasePuName = puName.toLowerCase();
         Set<String> universityNames = jsonObject.keySet();
         boolean found = false;
 
+        logger.log(Level.INFO, Logs.SEARCH_UNIVERSITY, puName);
+
         for (String universityName : universityNames) {
+            assert universityName != null && !universityName.isEmpty();
             if (universityName.toLowerCase().equals(lowerCasePuName)) {
                 found = true;
-
                 JsonObject universityObject = jsonObject.getJsonObject(universityName);
+
+                logger.log(Level.INFO, Logs.UNIVERSITY_FOUND, universityName);
+                assert universityObject != null : Assertions.NULL_UNIVERSITY_OBJECT;
+
                 JsonArray courseArray = universityObject.getJsonArray("courses");
 
                 if (courseArray == null) {
+                    logger.log(Level.WARNING, Logs.NO_COURSES_FOUND);
                     throw new IllegalArgumentException("No courses found for university: " + puName);
                 }
 
+                logger.log(Level.INFO, Logs.LISTING_COURSES);
                 for (int i = 0; i < courseArray.size(); i++) {
                     JsonObject courseObject = courseArray.getJsonObject(i);
+                    assert courseObject != null : Assertions.NO_COURSE_OBJECT;
+
                     String puCourseCode = courseObject.getString(PU_COURSE_CODE_KEY);
                     String puCourseName = courseObject.getString(PU_COURSE_NAME_KEY);
                     String nusCourseCode = courseObject.getString(NUS_COURSE_CODE_KEY);
                     String nusCourseName = courseObject.getString(NUS_COURSE_NAME_KEY);
 
+                    logger.log(Level.INFO, Logs.COURSE_DETAILS);
                     System.out.println(puCourseCode + ": " + puCourseName);
                     System.out.println(nusCourseCode + ": " + nusCourseName);
                     System.out.println(LINE_SEPARATOR);
@@ -78,6 +103,7 @@ public class ListUniCoursesCommand extends Command {
             }
         }
         if (!found && (!puName.isEmpty())) {
+            logger.log(Level.WARNING, Logs.UNKNOWN_UNIVERSITY, puName);
             throw new UnknownUniversityException("University not found: " + puName);
         }
     }
