81221bcc0d3bce84ef7674fbf08a604ef6d2c04a
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-10-17 22:15:21 +0800

    Made setters of Expense class more defensive
    Add null checks, if null then custom exception thrown
    Add assertions after null check

--- src/main/java/wheresmymoney/Expense.java
+++ src/main/java/wheresmymoney/Expense.java
@@ -22,14 +22,26 @@ public class Expense {
     public String getCategory() {
         return category;
     }
-    public void setPrice(Float price) {
+    public void setPrice(Float price) throws WheresMyMoneyException {
+        if (price == null) {
+            throw new WheresMyMoneyException("Price shouldn't be null.");
+        }
         this.price = price;
+        assert this.price != null : "Expense's price is null.";
     }
-    public void setDescription(String description) {
+    public void setDescription(String description) throws WheresMyMoneyException {
+        if (description == null) {
+            throw new WheresMyMoneyException("Description shouldn't be null.");
+        }
         this.description = description;
+        assert this.description != null : "Expense's description is null.";
     }
-    public void setCategory(String category) {
+    public void setCategory(String category) throws WheresMyMoneyException {
+        if (category == null) {
+            throw new WheresMyMoneyException("Category shouldn't be null.");
+        }
         this.category = category;
+        assert this.category != null : "Expense's category is null.";
     }
 
 }
