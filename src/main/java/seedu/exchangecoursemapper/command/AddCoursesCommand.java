7a532709969e6f6fc033f2e29c80b639e0bbb461
hzxnancy <hzxnancy@gmail.com>
2024-10-17 21:57:07 +0800

    Add logging for AddCoursesCommand

--- src/main/java/seedu/exchangecoursemapper/command/AddCoursesCommand.java
+++ src/main/java/seedu/exchangecoursemapper/command/AddCoursesCommand.java
@@ -2,22 +2,31 @@ package seedu.exchangecoursemapper.command;
 
 import seedu.exchangecoursemapper.storage.Storage;
 import seedu.exchangecoursemapper.exception.Exception;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class AddCoursesCommand extends Command {
 
     Storage storage = new Storage();
+    private static final Logger logger = Logger.getLogger(AddCoursesCommand.class.getName());
 
     @Override
     public void execute(String userInput) {
         try {
+            logger.log(Level.INFO, "Trim spaces and remove 'add' command. ");
             String description = trimString(userInput);
+            logger.log(Level.INFO, "Check user input and split it into substrings");
             String[] descriptionSubstrings = parseAddCommand(description);
 
+            logger.log(Level.INFO, "Extract from NUS course code, PU and PU course code from array.");
             String nusCourse = descriptionSubstrings[0].trim();
             String pu = descriptionSubstrings[1].trim();
             String puCourse = descriptionSubstrings[2].trim();
 
+            logger.log(Level.INFO,"Format output");
             String courseToStore = nusCourse + " | " + pu + " | " + puCourse;
+            logger.log(Level.INFO, "Adding course to storage: " + courseToStore);
             storage.addCourse(courseToStore);
 
             printAddMessage(courseToStore);
@@ -32,28 +41,34 @@ public class AddCoursesCommand extends Command {
         String[] outputSubstrings = trimmedString.split(" ", 2);
 
         if (outputSubstrings.length < 2 || outputSubstrings[1].trim().isEmpty()) {
+            logger.log(Level.WARNING, "No input after add keyword.");
             throw new IllegalArgumentException(Exception.noInputAfterAdd());
         }
 
+        logger.log(Level.INFO, "Return trimmed input without 'add' command");
         return outputSubstrings[1];
     }
 
     public String[] parseAddCommand(String input) {
+
         input = input.replaceAll("(?i)/pu", "/pu")
                 .replaceAll("(?i)/coursepu", "/coursepu")
                 .trim().replaceAll(" +", " ");
 
         if ((!input.contains("/pu") || !input.contains("/coursepu"))) {
+            logger.log(Level.WARNING, "Missing /pu or /coursepu keyword.");
             throw new IllegalArgumentException(Exception.missingKeyword());
         }
 
         if (input.contains("/pu/coursepu") || input.contains("/coursepu/pu")){
+            logger.log(Level.WARNING, "Concurrent keywords.");
             throw new IllegalArgumentException(Exception.adjacentInputError());
         }
 
         String[] inputSubstrings = input.split(" /coursepu | /pu ");
 
         if (inputSubstrings.length < 3) {
+            logger.log(Level.WARNING, "Invalid course code or partner university.");
             throw new IllegalArgumentException(Exception.invalidCourseCodes());
         }
 
