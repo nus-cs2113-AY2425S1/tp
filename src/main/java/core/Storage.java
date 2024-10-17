45df38736e8ab8af53a29639ef0044bcd0957a06
BevLow <lowbeverlypersonal@gmail.com>
2024-10-17 23:31:40 +0800

    Fixing Checkstype error

--- src/main/java/core/Storage.java
+++ src/main/java/core/Storage.java
@@ -1,7 +1,10 @@
 package core;
 
-import com.google.gson.*;
-
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 import programme.ProgrammeList;
 
 import java.io.File;
