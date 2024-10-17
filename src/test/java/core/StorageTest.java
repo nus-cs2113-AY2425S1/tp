6a47bf001b5b2f3dc751ba874770f04a51cd506d
BevLow <lowbeverlypersonal@gmail.com>
2024-10-17 23:41:09 +0800

    Fixing Checkstype error for StorageTest

--- src/test/java/core/StorageTest.java
+++ src/test/java/core/StorageTest.java
@@ -10,7 +10,9 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
