b7caf105f3fc3789f0c1539b3bd7e5487b0710d0
jemehgoh <jeremygoheh@gmail.com>
2024-10-18 23:16:13 +0800

    Add functionality to mark events not done to MarkCommand

--- src/main/java/seedu/manager/command/MarkCommand.java
+++ src/main/java/seedu/manager/command/MarkCommand.java
@@ -11,30 +11,34 @@ public class MarkCommand extends Command {
     public static final String COMMAND_WORD = "mark";
 
     private static final String EVENT_MARK_MESSAGE = "Event marked as done";
+    private static final String EVENT_UNMARK_MESSAGE = "Event marked not done";
     private static final String INVALID_EVENT_MESSAGE = "Event not found!";
 
     private String eventName;
+    private boolean toMark;
 
     /**
      * Constructs a new MarkCommand with the given event name
      *
      * @param eventName the given event name
      */
-    public MarkCommand(String eventName) {
+    public MarkCommand(String eventName, boolean toMark) {
         super(false);
         this.eventName = eventName;
+        this.toMark = toMark;
     }
 
     /**
-     * Executes a mark command by marking the specified event as done
+     * Executes a mark command by marking the specified event as done or not done,
+     * depending on the value of toMark.
      */
     @Override
     public void execute() {
         Optional<Event> eventToMark = this.eventList.getEventByName(this.eventName);
 
         if (eventToMark.isPresent()) {
-            eventToMark.get().setDone(true);
-            this.message = EVENT_MARK_MESSAGE;
+            eventToMark.get().setDone(this.toMark);
+            this.message = (this.toMark) ? EVENT_MARK_MESSAGE : EVENT_UNMARK_MESSAGE;
         } else {
             this.message = INVALID_EVENT_MESSAGE;
         }
