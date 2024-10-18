7981e24309721eda4d86edaf5d6157061f1943f7
Ajay Shanker <ajayshanker1998@gmail.com>
2024-10-19 00:29:47 +0800

    Integrate Storage Support

--- src/main/java/seedu/duke/MediTask.java
+++ src/main/java/seedu/duke/MediTask.java
@@ -8,6 +8,7 @@ import seedu.duke.data.hospital.Patient;
 import seedu.duke.data.state.State;
 import seedu.duke.data.state.StateType;
 import seedu.duke.parser.Parser;
+import seedu.duke.storage.StorageFile;
 import seedu.duke.ui.Ui;
 
 public class MediTask {
@@ -18,10 +19,16 @@ public class MediTask {
 
         Ui ui = new Ui();
         Hospital hospital = new Hospital();
+        StorageFile storage = new StorageFile();
+
         ui.showWelcome();
         // Start in MAIN_STATE
         State currentState = new State(StateType.MAIN_STATE);
         assert currentState != null : "Current state should not be null.";
+
+        // Load data from file
+        hospital = storage.load();
+
         HospitalCommand.setHospital(hospital);
 
         // variable to hold the selected patient
@@ -33,21 +40,21 @@ public class MediTask {
                 ui.showMainScreen();
                 String commandInput = ui.readCommand();
 
-                // parse and execute the command, and also pass in  the currentState
+                // parse and execute the command, and also pass in the currentState
                 Parser parser = new Parser(commandInput, currentState);
                 Command command = parser.parseCommand();
 
                 if (command != null) {
                     command.execute();
 
-                    //handle patient selection
+                    // handle patient selection
                     if (command instanceof seedu.duke.commands.SelectPatientCommand) {
                         try {
                             // convert user input to index
                             int patientIndex = Integer.parseInt(commandInput.split(" ")[1]) - 1;
-                            //check if patient index fall within the number of patients in the hospital
+                            // check if patient index fall within the number of patients in the hospital
                             assert patientIndex >= 0 && patientIndex < hospital.getSize() : "Invalid patient index.";
-                            //get selected patient
+                            // get selected patient
                             selectedPatient = hospital.getPatient(patientIndex);
                         } catch (Exception e) {
                             System.out.println("invalid patient selection.");
@@ -55,24 +62,26 @@ public class MediTask {
                     }
                 }
             } else if (currentState.getState() == StateType.TASK_STATE) {
-                //show task screen for the selected patient
+                // show task screen for the selected patient
                 if (selectedPatient != null) {
                     assert selectedPatient != null : "A patient must be selected in TASK_STATE.";
-                    //display selectedPatient name
+                    // display selectedPatient name
                     ui.showTaskScreen(selectedPatient.getName());
                 }
                 String commandInput = ui.readCommand();
 
-                //parse and execute commands in TASK_STATE
-                //pass the State object
+                // parse and execute commands in TASK_STATE
+                // pass the State object
                 Parser parser = new Parser(commandInput, currentState);
                 Command command = parser.parseCommand();
                 if (command != null) {
                     command.setData(selectedPatient.getTaskList());
-                    CommandResult result =  command.execute();
+                    CommandResult result = command.execute();
                     ui.showToUser(result.getFeedbackToUser());
                 }
             }
+            // Save hospital data to file
+            storage.save(hospital);
         }
     }
 }
