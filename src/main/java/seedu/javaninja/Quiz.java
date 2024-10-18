2b29b433580111d3062875aed8665533f5bb1584
shaunngoh <shaungoh682@gmail.com>
2024-10-18 10:56:29 +0800

    Add logging to Quiz

--- src/main/java/seedu/javaninja/Quiz.java
+++ src/main/java/seedu/javaninja/Quiz.java
@@ -2,12 +2,14 @@ package seedu.javaninja;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Quiz {
     private Topic topic;
     private int currentQuestionIndex;
     private int correctAnswers;
     private Scanner scanner;
+    private static final Logger logger = Logger.getLogger(Quiz.class.getName());
 
     public Quiz(Topic topic) {
         assert topic != null : "Topic must not be null";
@@ -18,6 +20,7 @@ public class Quiz {
     }
 
     public void start() {
+        logger.info("Quiz started for topic: " + topic.getName());
         assert topic.getQuestions() != null && !topic.getQuestions().isEmpty()
             : "Quiz must have questions";
         List<Question> questions = topic.getQuestions();
@@ -33,6 +36,7 @@ public class Quiz {
         }
 
         System.out.println("Quiz finished. Your score is: " + getScore() + "%");
+        logger.info("Quiz finished for topic: " + topic.getName() + ". Score: " + getScore() + "%");
     }
 
     public void answerQuestion (String answer) {
