fad09c835945e2930c3e97eab7cd62b021217778
ChunJia <chiew_chunjia@u.nus.edu>
2024-10-20 22:49:39 +0800

    Modify code to pass checkstyle test

--- src/main/java/seedu/exchangecoursemapper/command/Command.java
+++ src/main/java/seedu/exchangecoursemapper/command/Command.java
@@ -7,15 +7,11 @@ import javax.json.JsonObject;
 import javax.json.JsonReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public abstract class Command {
-    private static final Logger logger = Logger.getLogger(Command.class.getName());
     public static final String FILE_PATH = "/database.json";
 
     public JsonObject createJsonObject() throws IOException {
-        logger.log(Level.INFO, Logs.EXECUTING_COMMAND);
         InputStream inputStream = getClass().getResourceAsStream(FILE_PATH);
         if (inputStream == null) {
             throw new IOException();
