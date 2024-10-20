f7b186acac4f5a9e316ed2128fbdf03cd74af1b8
BevLow <lowbeverlypersonal@gmail.com>
2024-10-20 16:57:27 +0800

    Add Unit Test for Command Class

--- src/test/java/command/CommandTest.java
+++ src/test/java/command/CommandTest.java
@@ -1,12 +1,31 @@
 package command;
 
+import core.History;
+import core.Ui;
 import org.junit.jupiter.api.Test;
+import programme.ProgrammeList;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class CommandTest {
     @Test
-    public void assertCommand() {
-        assertTrue(true);
+    public void testIsExitReturnFalse() {
+        Command command = new TestCommand();
+        assertFalse(command.isExit());
+    }
+
+    @Test
+    public void testExecute() {
+        Ui mockUi = mock(Ui.class);
+        ProgrammeList mockPList = mock(ProgrammeList.class);
+        History mockHistory = mock(History.class);
+
+        Command command = new TestCommand();
+
+        command.execute(mockUi, mockPList, mockHistory);
+
+        verify(mockUi).showMsg("Test Command Executed");
     }
 }
