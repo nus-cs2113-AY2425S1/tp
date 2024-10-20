f79c6203bdb71dc0b70e6349895cafa3a13f53f2
Atulteja <atulteja@hotmail.com>
2024-10-20 15:01:22 +0800

    Fixed test

--- src/test/java/programme/DayTest.java
+++ src/test/java/programme/DayTest.java
@@ -66,10 +66,7 @@ public class DayTest {
                 
                 1. Bench Press: 3 sets of 10 reps at 50 kg
                 2. Triceps Extension: 3 sets of 12 reps at 20 kg
-                3. Seated Press: 3 sets of 10 reps at 50 kg
-                
-                
-                """.trim();
+                3. Seated Press: 3 sets of 10 reps at 50 kg""".trim();
 
         assertEquals(expectedString, dayString);
     }
