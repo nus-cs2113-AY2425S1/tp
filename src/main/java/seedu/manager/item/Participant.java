6e0f5e2c3428c265beccaf27aaf21f8463b250ee
jemehgoh <jeremygoheh@gmail.com>
2024-10-21 13:20:17 +0800

    Merge Participant class into its parent Item class

--- src/main/java/seedu/manager/item/Participant.java
+++ src/main/java/seedu/manager/item/Participant.java
@@ -5,14 +5,54 @@ import seedu.manager.event.Event;
 /**
  * Represents an event participant, in the participant list of an {@link Event}.
  */
-public class Participant extends Item {
+public class Participant {
+    private final String name;
+    private boolean isPresent;
 
     /**
-     * Constructs a new Item with a given name
+     * Constructs a new Participant with a given name
      *
      * @param name the given name
      */
     public Participant(String name) {
-        super(name);
+        this.name = name;
+        this.isPresent = false;
+    }
+
+    /**
+     * Returns the name of the participant.
+     *
+     * @return the name of the participant
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the participant as present or not present.
+     *
+     * @param isPresent whether the participant is present or not present
+     */
+    public void setPresent(boolean isPresent) {
+        this.isPresent = isPresent;
+    }
+
+    /**
+     * Returns "present" if isPresent is true, "not present" otherwise.
+     *
+     * @return "present" if isPresent is true, "not present" otherwise.
+     */
+    public String markIfPresent() {
+        return (this.isPresent) ? "present" : "not present";
+    }
+
+    /**
+     * Returns a formatted string of the item
+     *
+     * @return a formatted string of the item
+     */
+    @Override
+    public String toString() {
+        return String.format("%s (status: %s)", this.name, this.markIfPresent());
     }
 }
