f23aacbe61b30e739be0103afbefbabf01d8d5a4
TVageesan <59349293+TVageesan@users.noreply.github.com>
2024-10-22 18:29:57 +0800

    Remove unused imports

--- src/test/java/command/programme/ListCommandTest.java
+++ src/test/java/command/programme/ListCommandTest.java
@@ -1,9 +1,5 @@
 package command.programme;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 public class ListCommandTest {
 
 }
--- src/test/java/command/programme/ViewCommandTest.java
+++ src/test/java/command/programme/ViewCommandTest.java
@@ -2,8 +2,6 @@ package command.programme;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
 public class ViewCommandTest {
     @Test
     public void testNegativeInteger_throwsException() {}
