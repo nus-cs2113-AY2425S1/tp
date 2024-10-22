9fe0743cf11e7afe14d1da7d36bfa319ab287e0d
gskang <windstick26@gmail.com>
2024-10-22 19:52:16 +0800

    Add new class Ingredient

---
+++ src/main/java/ymfc/recipe/Ingredient.java
@@ -0,0 +1,13 @@
+package ymfc.recipe;
+
+public class Ingredient {
+    String name;
+
+    public Ingredient(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/ymfc/recipelist/Ingredients.java
@@ -0,0 +1,52 @@
+package ymfc.recipelist;
+
+import ymfc.recipe.Ingredient;
+import ymfc.recipe.Recipe;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Ingredients {
+    ArrayList<Ingredient> ingredients = new ArrayList<>();
+
+    public Ingredients() {
+        this.ingredients = new ArrayList<>();
+    }
+
+    public void addRecipe(Ingredient ingredient){
+        this.ingredients.add(ingredient);
+    }
+
+    public ArrayList<Ingredient> getRecipes() {
+        return ingredients;
+    }
+
+    public Ingredient getIngredient(int id) {
+        return ingredients.get(id);
+    }
+
+    public void removeRecipe(int id) {
+        assert !ingredients.isEmpty() : "List should not be empty when deleting ingredient";
+        ingredients.remove(id);
+    }
+
+    public boolean removeRecipeByName(String name) {
+        assert !ingredients.isEmpty() : "List should not be empty when deleting ingredient";
+        assert name != null : "Ingredient name should not be null";
+        for (int i = 0; i < ingredients.size(); i++) {
+            if (ingredients.get(i).getName().equalsIgnoreCase(name)) {
+                ingredients.remove(i);
+i                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCounter() {
+        return ingredients.size();
+    }
+
+    public void sortAlphabetically() {
+        ingredients.sort(Comparator.comparing(Ingredient::getName, String.CASE_INSENSITIVE_ORDER));
+    }
+}
