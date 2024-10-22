fca1bd1027a4cb0b44ddeb3caae4aed6fd77d299
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-22 20:12:34 +0800

    Add formatAmount() to display correctly formatted amount for expenses
    Import DecimalFormat class. Implement formatAmount() to format expenses such that whole numbers display without decimals, and values with cents show two decimal places.

--- src/main/java/seedu/duke/Expense.java
+++ src/main/java/seedu/duke/Expense.java
@@ -1,10 +1,10 @@
 package seedu.duke;
 
+import java.text.DecimalFormat;
+
 public class Expense {
     private String name;
-
     private double amount;
-
     private String category;
 
     public Expense(String name, double amount, String category) {
@@ -29,8 +29,18 @@ public class Expense {
         this.category = category;
     }
 
+    public String formatAmount() {
+        DecimalFormat wholeNumberFormat = new DecimalFormat("$#");
+        DecimalFormat decimalFormat = new DecimalFormat("$#.00");
+        if (amount % 1 == 0) {
+            return wholeNumberFormat.format(amount);
+        } else {
+            return decimalFormat.format(amount);
+        }
+    }
+
     @Override
     public String toString() {
-        return " Item: " + name + ", Amount: $" + amount + ", Category: " + getCategory();
+        return " Item: " + name + ", Amount: " + formatAmount() + ", Category: " + getCategory();
     }
 }
