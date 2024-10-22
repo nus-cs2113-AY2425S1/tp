e35be37e127111e067618bed18fe34a097011a0c
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-10-22 17:17:04 +0800

    Update SeeAllExpensesCommand to filter by date

--- src/main/java/seedu/duke/command/SeeAllExpensesCommand.java
+++ src/main/java/seedu/duke/command/SeeAllExpensesCommand.java
@@ -3,6 +3,9 @@ package seedu.duke.command;
 import seedu.duke.financial.FinancialEntry;
 import seedu.duke.financial.FinancialList;
 import seedu.duke.financial.Expense;
+import seedu.duke.financial.Income;
+
+import java.time.LocalDate;
 
 /**
  * The SeeAllExpensesCommand class is responsible for displaying all recorded expenses
@@ -10,8 +13,24 @@ import seedu.duke.financial.Expense;
  * execute method to perform this functionality.
  */
 public class SeeAllExpensesCommand extends Command{
+    private LocalDate start;
+    private LocalDate end;
 
-    public SeeAllExpensesCommand() {}
+    public SeeAllExpensesCommand(LocalDate start, LocalDate end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * Method to determine if an entry should be listed out based on its date and if it is an expense.
+     *
+     * @param entry Financial Entry to analyze.
+     * @return true if entry should be listed out, false otherwise.
+     */
+    private boolean shouldBeIncluded(FinancialEntry entry) {
+        return entry instanceof Expense && (end == null || entry.getDate().isBefore(end))
+                && (start == null || entry.getDate().isAfter(start));
+    }
 
     /**
      * Executes the command to display all recorded expenses in the financial list.
@@ -23,22 +42,24 @@ public class SeeAllExpensesCommand extends Command{
      */
     @Override
     public void execute(FinancialList list) {
+        System.out.println("--------------------------------------------");
         String expenseList = "";
         int expenseCount = 0;
+
         for (int i = 0; i < list.getEntryCount(); i++) {
             FinancialEntry entry = list.getEntry(i);
-            if (entry instanceof Expense) {
+            if (shouldBeIncluded(entry)) {
                 expenseList += ((++expenseCount) + ". " + entry + System.lineSeparator());
             }
         }
-        if (expenseCount == 0) {
-            System.out.println("No recorded expenses found.");
-            System.out.println("--------------------------------------------");
-        } else {
-            System.out.println("Here's a list of all recorded expenses:");
-            System.out.print(expenseList);
-            System.out.println("--------------------------------------------");
-        }
-    }
 
+        if (expenseCount == 0) {
+            System.out.println("No entries found.");
+            System.out.println("--------------------------------------------");
+            return;
+        }
+        System.out.println("Here's a list of all recorded expenses:");
+        System.out.print(expenseList);
+        System.out.println("--------------------------------------------");
+    }
 }
