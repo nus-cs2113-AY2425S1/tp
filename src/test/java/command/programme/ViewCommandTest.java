7cae879f1fe69902ba16764eb31d64e7fd6be92a
BevLow <lowbeverlypersonal@gmail.com>
2024-10-22 17:02:10 +0800

    Added testIsExit_returnFalse for Edit, List, sStart and View Command Test

--- src/test/java/command/programme/ViewCommandTest.java
+++ src/test/java/command/programme/ViewCommandTest.java
@@ -2,11 +2,15 @@ package command.programme;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class ViewCommandTest {
     @Test
-    public void sampleTest() {
-        assertTrue(true);
+    public void testIsExit_returnsFalse() {
+        ViewCommand viewCommand = new ViewCommand(0);
+        assertFalse(viewCommand.isExit());
     }
+
+    @Test
+    public void testNegativeInteger_throwsException() {}
 }
