872fff2bc527a539df14172e428fb95e159b39f8
KHsienKit <hsienkitkhor@gmail.com>
2024-10-23 22:34:11 +0800

    Create constructor, attributes, getter and setter

--- src/main/java/wheresmymoney/RecurringExpense.java
+++ src/main/java/wheresmymoney/RecurringExpense.java
@@ -1,5 +1,29 @@
 package wheresmymoney;
 
 public class RecurringExpense extends Expense {
-    
+    protected String lastUpdated;
+    protected String frequency;
+
+    public RecurringExpense(Float price, String description, String category, 
+            String lastUpdated, String frequency) {
+        super(price, description, category);
+        this.lastUpdated = lastUpdated;
+        this.frequency = frequency;
+    }
+
+    public String getLastUpdated() {
+        return this.lastUpdated;
+    }
+
+    public String getFrequency() {
+        return this.frequency;
+    }
+
+    public void setLastUpdated(String date) {
+        this.lastUpdated = date;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
 }
