12cb469fda5adac38ae0e4de7eb734fc101b9e3d
Gao Yening <gao2351624311@gmail.com>
2024-10-23 16:19:06 +0800

    Create Storage class under datastorage package responsible for saving data in JSON and loading

---
+++ src/main/java/seedu/datastorage/Storage.java
@@ -0,0 +1,15 @@
+package seedu.datastorage;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+public class Storage {
+}
