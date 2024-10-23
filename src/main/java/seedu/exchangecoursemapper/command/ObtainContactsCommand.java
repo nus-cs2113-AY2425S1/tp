8b7440612b748537905909a8121144f64ee6ca4e
louisjoety <kolouis002@gmail.com>
2024-10-23 14:52:14 +0800

    Remove loose string literals

--- src/main/java/seedu/exchangecoursemapper/command/ObtainContactsCommand.java
+++ src/main/java/seedu/exchangecoursemapper/command/ObtainContactsCommand.java
@@ -2,10 +2,17 @@ package seedu.exchangecoursemapper.command;
 
 import seedu.exchangecoursemapper.exception.Exception;
 
+import static seedu.exchangecoursemapper.constants.Regex.BACKSLASH;
+import static seedu.exchangecoursemapper.constants.Regex.SPACE;
+
+
 import java.io.IOException;
 import javax.json.JsonObject;
 
 public class ObtainContactsCommand extends Command {
+    public static final String EMAIL = "email";
+    public static final String NUMBER = "number";
+
     @Override
     public void execute(String userInput) {
         try  {
@@ -28,14 +35,14 @@ public class ObtainContactsCommand extends Command {
     }
 
     private String getSchoolName(String userInput) {
-        String inputWithoutCommand = userInput.substring(userInput.indexOf(" ") + 1).trim();
-        String[] inputParts = inputWithoutCommand.split(" /");
+        String inputWithoutCommand = userInput.substring(userInput.indexOf(SPACE) + 1).trim();
+        String[] inputParts = inputWithoutCommand.split(BACKSLASH);
         return inputParts[0].trim();
     }
 
     private String getContactType(String userInput) {
-        String inputWithoutCommand = userInput.substring(userInput.indexOf(" ") + 1).trim();
-        String[] inputParts = inputWithoutCommand.split(" /");
+        String inputWithoutCommand = userInput.substring(userInput.indexOf(SPACE) + 1).trim();
+        String[] inputParts = inputWithoutCommand.split(BACKSLASH);
 
         if (inputParts.length != 2) {
             throw new IllegalArgumentException("Invalid input format");
@@ -45,16 +52,16 @@ public class ObtainContactsCommand extends Command {
 
     private void handleContactType(JsonObject schoolInfo, String schoolName, String contactType) {
         switch (contactType) {
-        case "email":
-            String email = schoolInfo.getString("email", null);
+        case EMAIL:
+            String email = schoolInfo.getString(EMAIL);
             if (email != null) {
                 System.out.println("Email for " + schoolName + ": " + email);
             } else {
                 System.out.println("Email not available for " + schoolName);
             }
             break;
-        case "number":
-            String number = schoolInfo.getString("number", null);
+        case NUMBER:
+            String number = schoolInfo.getString(NUMBER);
             if (number != null) {
                 System.out.println("Phone number for " + schoolName + ": " + number);
             } else {
@@ -62,7 +69,7 @@ public class ObtainContactsCommand extends Command {
             }
             break;
         default:
-            System.out.println("Invalid contact type. Please use 'email' or 'number'.");
+            System.out.println("Invalid contact type.");
         }
     }
 
