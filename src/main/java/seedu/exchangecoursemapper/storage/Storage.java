5ae22eafdba144f153f8f7c28362c0f0eaeacf6d
hzxnancy <hzxnancy@gmail.com>
2024-10-23 12:56:56 +0800

    Add course class for storage

--- src/main/java/seedu/exchangecoursemapper/storage/Storage.java
+++ src/main/java/seedu/exchangecoursemapper/storage/Storage.java
@@ -1,12 +1,13 @@
 package seedu.exchangecoursemapper.storage;
 
 import java.util.ArrayList;
+import seedu.exchangecoursemapper.courses.Course;
 
 public class Storage {
     // For our personal tracker
-    private ArrayList<String> savedCourses = new ArrayList<>();
+    private ArrayList<Course> savedCourses = new ArrayList<>();
 
-    public void addCourse(String course) {
+    public void addCourse(Course course) {
         savedCourses.add(course);
     }
 
