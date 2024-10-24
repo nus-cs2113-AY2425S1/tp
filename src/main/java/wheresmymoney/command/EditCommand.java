594aca750d121c15429e8fc6bb86970252363777
KHsienKit <hsienkitkhor@gmail.com>
2024-10-24 16:15:34 +0800

    Add edit function to recurring expenses

--- src/main/java/wheresmymoney/command/EditCommand.java
+++ src/main/java/wheresmymoney/command/EditCommand.java
@@ -2,6 +2,7 @@ package wheresmymoney.command;
 
 import wheresmymoney.ExpenseList;
 import wheresmymoney.Parser;
+import wheresmymoney.RecurringExpenseList;
 import wheresmymoney.exception.InvalidInputException;
 import wheresmymoney.exception.WheresMyMoneyException;
 
@@ -13,14 +14,27 @@ public class EditCommand extends Command {
         super(argumentsMap);
     }
 
+    /**
+     * Edits an expense or recurring expense
+     * 
+     * @param expenseList List of expenses
+     * @param recurringExpenseList List of recurring expenses
+     */
     @Override
-    public void execute(ExpenseList expenseList) throws WheresMyMoneyException {
+    public void execute(ExpenseList expenseList, RecurringExpenseList recurringExpenseList) 
+            throws WheresMyMoneyException {
         try {
             int index = Integer.parseInt(argumentsMap.get(Parser.ARGUMENT_MAIN)) - 1;
             String category = argumentsMap.get(Parser.ARGUMENT_CATEGORY);
             float price = Float.parseFloat(argumentsMap.get(Parser.ARGUMENT_PRICE));
             String description = argumentsMap.get(Parser.ARGUMENT_DESCRIPTION);
-            expenseList.editExpense(index, price, description, category);
+            if (argumentsMap.containsKey(Parser.ARGUMENT_RECUR)) {
+                String lastAddedDate = argumentsMap.get(Parser.ARGUMENT_DATE);
+                String frequency = argumentsMap.get(Parser.ARGUMENT_FREQUENCY);
+                recurringExpenseList.editRecurringExpense(index, price, description, category, lastAddedDate, frequency);
+            } else {
+                expenseList.editExpense(index, price, description, category);
+            }
         } catch (NullPointerException | NumberFormatException e) {
             throw new InvalidInputException("Invalid Arguments.");
         }
