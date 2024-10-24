d98a92b215dae73efb0cbdbe306193e834a5db26
Juven Lim <117549420+Littleme1032@users.noreply.github.com>
2024-10-24 23:52:08 +0800

    Update checkstyle
    Update checkstyle

--- src/test/java/seedu/duke/command/AddExpenseCommandTest.java
+++ src/test/java/seedu/duke/command/AddExpenseCommandTest.java
@@ -142,7 +142,7 @@ class AddExpenseCommandTest {
         assertEquals(expectedOutput, output);
     }
 
-     /**
+    /**
      * Test the execute method of AddExpenseCommand with an empty date string.
      * Verifies that a FinanceBuddyException is thrown when an empty date string is provided.
      */
