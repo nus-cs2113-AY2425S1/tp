b2fce6122838efd2fad62cdbba68836a1d425b5e
KHsienKit <hsienkitkhor@gmail.com>
2024-10-24 16:08:47 +0800

    Modify add command for recurring expenses

--- src/main/java/wheresmymoney/command/AddCommand.java
+++ src/main/java/wheresmymoney/command/AddCommand.java
@@ -2,6 +2,7 @@ package wheresmymoney.command;
 
 import wheresmymoney.ExpenseList;
 import wheresmymoney.Parser;
+import wheresmymoney.RecurringExpenseList;
 import wheresmymoney.exception.InvalidInputException;
 import wheresmymoney.exception.WheresMyMoneyException;
 
@@ -14,12 +15,25 @@ public class AddCommand extends Command {
     }
 
     @Override
-    public void execute(ExpenseList expenseList) throws WheresMyMoneyException {
+    /**
+     * Add a expense or recurring expense after prasing through arguments
+     * @param expenseList List of normal expenses
+     * @param recurringExpenseList List of recurring expenses
+     */
+    public void execute(ExpenseList expenseList, RecurringExpenseList recurringExpenseList) 
+            throws WheresMyMoneyException {
         try {
+            System.out.println(argumentsMap.containsKey(Parser.ARGUMENT_RECUR));
             float price = Float.parseFloat(argumentsMap.get(Parser.ARGUMENT_PRICE));
             String description = argumentsMap.get(Parser.ARGUMENT_DESCRIPTION);
             String category = argumentsMap.get(Parser.ARGUMENT_CATEGORY);
-            expenseList.addExpense(price, description, category);
+            if (argumentsMap.containsKey(Parser.ARGUMENT_RECUR)) {
+                String lastAddedDate = argumentsMap.get(Parser.ARGUMENT_DATE);
+                String frequency = argumentsMap.get(Parser.ARGUMENT_FREQUENCY);
+                recurringExpenseList.addRecurringExpense(price, description, category, lastAddedDate, frequency);
+            } else {
+                expenseList.addExpense(price, description, category);
+            }
         }catch(NullPointerException | NumberFormatException e){
             throw new InvalidInputException("Invalid Arguments");
         }
