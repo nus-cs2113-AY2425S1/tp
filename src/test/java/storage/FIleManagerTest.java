96737a64f3211e0853e6dd8c1a6142611cf89676
BevLow <lowbeverlypersonal@gmail.com>
2024-10-24 21:48:00 +0800

    Added FileManagerTest

---
+++ src/test/java/storage/FIleManagerTest.java
@@ -0,0 +1,12 @@
+package storage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FIleManagerTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
