1f98da40616139ae392266db0899dc94fd6e645c
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-24 10:00:56 +0800

    Import specific libraries to resolve checkstyle error

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,7 +1,9 @@
 package seedu.duke;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ExpenseTest {
     @Test
