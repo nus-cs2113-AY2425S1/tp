94b319d2f2a36d319d1db5239f10c9ac4147fa32
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-10-25 18:27:38 +0800

    Implement inheritance in list expenses/incomes command classes
    SeeAllExpensesCommand, SeeAllIncomesCommand and SeeAllEntriesCommand
    have a lot of duplicated code.
    
    Let's amend SeeAllExpensesCommand and SeeAllIncomesCommand to extend
    SeeAllEntriesCommand.
    
    This allows us to reduce the amount of duplicate code between the 3
    classes and reduce the amount of code in SeeAllIncomesCommand and
    SeeAllExpensesCommand.

--- src/main/java/seedu/duke/command/SeeAllExpensesCommand.java
+++ src/main/java/seedu/duke/command/SeeAllExpensesCommand.java
@@ -1,75 +1,41 @@
 package seedu.duke.command;
 
 import seedu.duke.financial.FinancialEntry;
-import seedu.duke.financial.FinancialList;
 import seedu.duke.financial.Expense;
 
 import java.time.LocalDate;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * The SeeAllExpensesCommand class is responsible for displaying all recorded expenses
  * from the provided FinancialList. It extends the Command class and overrides the 
  * execute method to perform this functionality.
  */
-public class SeeAllExpensesCommand extends Command{
-    private static final Logger logger = Logger.getLogger(SeeAllExpensesCommand.class.getName());
-  
-    private LocalDate start;
-    private LocalDate end;
+public class SeeAllExpensesCommand extends SeeAllEntriesCommand{
+    protected final String ENTRIES_LISTED_MESSAGE = "Here's a list of all recorded expenses:";
+    protected final String NO_ENTRIES_MESSAGE = "No expenses found.";
 
     public SeeAllExpensesCommand(LocalDate start, LocalDate end) {
-        this.start = start;
-        this.end = end;
+        super(start, end);
     }
 
+    @Override
+    protected String getNoEntriesMessage() {
+        return this.NO_ENTRIES_MESSAGE;
+    }
+
+    @Override
+    protected String getEntriesListedMessage() {
+        return this.ENTRIES_LISTED_MESSAGE;
+    }
     /**
      * Method to determine if an entry should be listed out based on its date and if it is an expense.
      *
      * @param entry Financial Entry to analyze.
      * @return true if entry should be listed out, false otherwise.
      */
-    private boolean shouldBeIncluded(FinancialEntry entry) {
+    @Override
+    protected boolean shouldBeIncluded(FinancialEntry entry) {
         return entry instanceof Expense && (end == null || entry.getDate().isBefore(end))
                 && (start == null || entry.getDate().isAfter(start));
-    }
-
-    /**
-     * Executes the command to display all recorded expenses in the financial list.
-     * Iterates through the financial list and collects all entries that are instances of Expense.
-     * If no expenses are found, it prints a message indicating no recorded expenses.
-     * Otherwise, it prints a list of all recorded expenses.
-     *
-     * @param list The financial list containing financial entries.
-     */
-    @Override
-    public void execute(FinancialList list) {
-        if (list == null) {
-            logger.log(Level.SEVERE, "Financial list is null");
-            assert list != null : "Financial list cannot be null";
-            throw new IllegalArgumentException("Financial list cannot be null");
-        }
-
-        System.out.println("--------------------------------------------");
-        String expenseList = "";
-        int expenseCount = 0;
-
-        for (int i = 0; i < list.getEntryCount(); i++) {
-            FinancialEntry entry = list.getEntry(i);
-            if (shouldBeIncluded(entry)) {
-                expenseList += ((++expenseCount) + ". " + entry + System.lineSeparator());
-            }
-        }
-
-        if (expenseCount == 0) {
-            System.out.println("No recorded expenses found.");
-            System.out.println("--------------------------------------------");
-            return;
-        }
-        System.out.println("Here's a list of all recorded expenses:");
-        System.out.print(expenseList);
-        System.out.println("--------------------------------------------");
     }
 }
--- src/main/java/seedu/duke/command/SeeAllIncomesCommand.java
+++ src/main/java/seedu/duke/command/SeeAllIncomesCommand.java
@@ -1,7 +1,6 @@
 package seedu.duke.command;
 
 import seedu.duke.financial.FinancialEntry;
-import seedu.duke.financial.FinancialList;
 import seedu.duke.financial.Income;
 
 import java.time.LocalDate;
@@ -9,13 +8,22 @@ import java.time.LocalDate;
 /**
  * Command to print all incomes recorded in the financial list.
  */
-public class SeeAllIncomesCommand extends Command {
-    private LocalDate start;
-    private LocalDate end;
+public class SeeAllIncomesCommand extends SeeAllEntriesCommand {
+    protected final String ENTRIES_LISTED_MESSAGE = "Here's a list of all recorded incomes:";
+    protected final String NO_ENTRIES_MESSAGE = "No incomes found.";
 
     public SeeAllIncomesCommand(LocalDate start, LocalDate end) {
-        this.start = start;
-        this.end = end;
+        super(start, end);
+    }
+
+    @Override
+    protected String getNoEntriesMessage() {
+        return this.NO_ENTRIES_MESSAGE;
+    }
+
+    @Override
+    protected String getEntriesListedMessage() {
+        return this.ENTRIES_LISTED_MESSAGE;
     }
 
     /**
@@ -24,39 +32,9 @@ public class SeeAllIncomesCommand extends Command {
      * @param entry Financial Entry to analyze.
      * @return true if entry should be listed out, false otherwise.
      */
-    private boolean shouldBeIncluded(FinancialEntry entry) {
+    @Override
+    protected boolean shouldBeIncluded(FinancialEntry entry) {
         return entry instanceof Income && (end == null || entry.getDate().isBefore(end))
                 && (start == null || entry.getDate().isAfter(start));
-    }
-
-    /**
-     * Executes the command to display all recorded incomes in the financial list.
-     * Iterates through the financial list and collects all entries that are instances of Income.
-     * If no incomes are found, it prints a message indicating no recorded incomes.
-     * Otherwise, it prints a list of all recorded incomes.
-     *
-     * @param list The financial list containing financial entries.
-     */
-    @Override
-    public void execute(FinancialList list) {
-        System.out.println("--------------------------------------------");
-        String incomeList = "";
-        int incomeCount = 0;
-
-        for (int i = 0; i < list.getEntryCount(); i++) {
-            FinancialEntry entry = list.getEntry(i);
-            if (shouldBeIncluded(entry)) {
-                incomeList += ((++incomeCount) + ". " + entry + System.lineSeparator());
-            }
-        }
-
-        if (incomeCount == 0) {
-            System.out.println("No recorded incomes found.");
-            System.out.println("--------------------------------------------");
-            return;
-        }
-        System.out.println("Here's a list of all recorded incomes:");
-        System.out.print(incomeList);
-        System.out.println("--------------------------------------------");
     }
 }
