69dbdf8d4364b1e9c9a14f2f1790184a5836aba1
ChunJia <chiew_chunjia@u.nus.edu>
2024-10-25 23:04:51 +0800

    Revert changes made to PersonalTrackerCommand

--- src/main/java/seedu/exchangecoursemapper/command/PersonalTrackerCommand.java
+++ src/main/java/seedu/exchangecoursemapper/command/PersonalTrackerCommand.java
@@ -2,6 +2,23 @@ package seedu.exchangecoursemapper.command;
 
 import seedu.exchangecoursemapper.storage.Storage;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.IOException;
+import java.io.InputStream;
+
 public abstract class PersonalTrackerCommand {
+    public static final String FILE_PATH = "/database.json";
+
+    public JsonObject createJsonObject() throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream(FILE_PATH);
+        if (inputStream == null) {
+            throw new IOException();
+        }
+        JsonReader jsonReader = Json.createReader(inputStream);
+        return jsonReader.readObject();
+    }
+
     public abstract void execute(String userInput, Storage storage);
 }
