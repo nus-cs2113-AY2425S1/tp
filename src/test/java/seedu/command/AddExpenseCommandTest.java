d97f7b56adb193fb8f40ecb896776ebd417c7c34
Jiaming <shenjiaming27@gmail.com>
2024-10-25 13:34:20 +0800

    Add JUnit test for AddExpenseCommand

---
+++ src/test/java/seedu/command/AddExpenseCommandTest.java
@@ -0,0 +1,68 @@
+package seedu.command;
+
+import org.junit.jupiter.api.Test;
+import seedu.transaction.TransactionList;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddExpenseCommandTest {
+
+    @Test
+    public void execute_addExpenseAllValidFields_success() {
+        TransactionList transactionList = new TransactionList();
+        AddExpenseCommand command = new AddExpenseCommand(transactionList);
+
+        command.setArguments(Map.of("", "dinner", "a/", "1000", "d/", "2024-10-01 1800", "c/", "Food"));
+        List<String> result = command.execute();
+
+        assertEquals("Expense added successfully!", result.get(0));
+    }
+
+    @Test
+    public void execute_addExpenseAllValidFields_noCategory_success() {
+        TransactionList transactionList = new TransactionList();
+        AddExpenseCommand command = new AddExpenseCommand(transactionList);
+
+        command.setArguments(Map.of("", "dinner", "a/", "1000", "d/", "2024-10-01 1800"));
+        List<String> result = command.execute();
+
+        assertEquals("Expense added successfully!", result.get(0));
+    }
+
+    @Test
+    public void execute_addExpenseInvalidAmount_success() {
+        TransactionList transactionList = new TransactionList();
+        AddExpenseCommand command = new AddExpenseCommand(transactionList);
+
+        command.setArguments(Map.of("", "dinner", "a/", "test", "d/", "2024-10-01 1800", "c/", "Food"));
+        List<String> result = command.execute();
+
+        assertEquals("Error creating Expense!: Invalid amount", result.get(0));
+    }
+
+    @Test
+    public void execute_addExpenseInvalidDate_success() {
+        TransactionList transactionList = new TransactionList();
+        AddExpenseCommand command = new AddExpenseCommand(transactionList);
+
+        command.setArguments(Map.of("", "dinner", "a/", "1000", "d/", "2024", "c/", "Food"));
+        List<String> result = command.execute();
+
+        assertEquals("Error creating Expense!: Your date and/or time is invalid!", result.get(0));
+    }
+
+    @Test
+    public void execute_addExpenseNoDate_success() {
+        TransactionList transactionList = new TransactionList();
+        AddExpenseCommand command = new AddExpenseCommand(transactionList);
+
+        command.setArguments(Map.of("", "dinner", "a/", "1000", "c/", "Food"));
+        List<String> result = command.execute();
+
+        assertEquals("Expense added successfully!", result.get(0));
+    }
+
+}
