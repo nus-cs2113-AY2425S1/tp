3e54101a6c40c03a7b1621d80843ba00fd990dfb
AdiMangalam <adimangalam2003@gmail.com>
2024-10-26 03:41:14 +0800

    Added multiple JUnit test cases for ExpenseTracker Class

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -1,6 +1,5 @@
 package seedu.duke;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
