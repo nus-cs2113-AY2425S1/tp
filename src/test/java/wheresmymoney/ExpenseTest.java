4af26620d039cc8dc7449a77a264d455709e34bf
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-10-25 14:00:03 +0800

    Add and update JUnit tests for Expense

--- src/test/java/wheresmymoney/ExpenseTest.java
+++ src/test/java/wheresmymoney/ExpenseTest.java
@@ -3,51 +3,54 @@ package wheresmymoney;
 import org.junit.jupiter.api.Test;
 import wheresmymoney.exception.WheresMyMoneyException;
 
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ExpenseTest {
     
-    private Expense initNullExpense() {
-        return new Expense(null, null, null);
+    private Expense initExpenseWithNoNullParts() throws WheresMyMoneyException {
+        return new Expense(0.0F, "desc", "category", "25-10-2024");
     }
 
     @Test
-    public void getPrice_nullInput_priceIsNull() {
-        Expense expense = initNullExpense();
-        assertNull(expense.getPrice());
+    public void Expense_nullPrice_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> new Expense(null, "desc", "category", "25-10-2024"));
     }
-
     @Test
-    public void getDescription_nullInput_descIsNull() {
-        Expense expense = initNullExpense();
-        assertNull(expense.getDescription());
+    public void Expense_nullDescription_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> new Expense(0.0F, null, "category", "25-10-2024"));
     }
-
     @Test
-    public void getCategory_nullInput_categoryIsNull() {
-        Expense expense = initNullExpense();
-        assertNull(expense.getCategory());
+    public void Expense_nullCategory_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> new Expense(0.0F, "desc", null, "25-10-2024"));
     }
+    @Test
+    public void Expense_nullDateAdded_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> new Expense(0.0F, "desc", "category", null));
+    }
+    
 
     @Test
     public void setPrice_nullInput_throwsWheresMyMoneyException() {
-        Expense expense = new Expense(0.0F, "desc", "category");
         assertThrows(WheresMyMoneyException.class,
-                () -> expense.setPrice(null));
+                () -> initExpenseWithNoNullParts().setPrice(null));
     }
-
     @Test
     public void setDescription_nullInput_throwsWheresMyMoneyException() {
-        Expense expense = new Expense(0.0F, "desc", "category");
         assertThrows(WheresMyMoneyException.class,
-                () -> expense.setDescription(null));
+                () -> initExpenseWithNoNullParts().setDescription(null));
     }
-
     @Test
     public void setCategory_nullInput_throwsWheresMyMoneyException() {
-        Expense expense = new Expense(0.0F, "desc", "category");
         assertThrows(WheresMyMoneyException.class,
-                () -> expense.setCategory(null));
+                () -> initExpenseWithNoNullParts().setCategory(null));
+    }
+    @Test
+    public void setDateAdded_nullInput_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+                () -> initExpenseWithNoNullParts().setDateAdded(null));
     }
 }
