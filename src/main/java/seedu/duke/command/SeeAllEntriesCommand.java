6fde87ddf61f1409d9678a422ef573e9c115b67b
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-10-25 18:20:31 +0800

    Add logging and assertions to SeeAllEntriesCommand

--- src/main/java/seedu/duke/command/SeeAllEntriesCommand.java
+++ src/main/java/seedu/duke/command/SeeAllEntriesCommand.java
@@ -4,13 +4,20 @@ import seedu.duke.financial.FinancialEntry;
 import seedu.duke.financial.FinancialList;
 
 import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Command to print all entries recorded in the financial list.
  */
 public class SeeAllEntriesCommand extends Command {
-    private LocalDate start;
-    private LocalDate end;
+    protected final String ENTRIES_LISTED_MESSAGE = "Here's a list of all recorded entries:";
+    protected static final String LINE_SEPARATOR = "--------------------------------------------";
+    protected static final Logger logger = Logger.getLogger(SeeAllExpensesCommand.class.getName());
+    protected final String NO_ENTRIES_MESSAGE = "No entries found.";
+
+    protected LocalDate start;
+    protected LocalDate end;
 
     /**
      * Constructor for SeeAllEntriesCommand.
@@ -41,6 +48,12 @@ public class SeeAllEntriesCommand extends Command {
     @Override
     public void execute(FinancialList list) {
         System.out.println("--------------------------------------------");
+        if (list == null) {
+            logger.log(Level.SEVERE, "Financial list is null");
+            assert list != null : "Financial list cannot be null";
+            throw new IllegalArgumentException("Financial list cannot be null");
+        }
+
         String entryList = "";
         int entryCount = 0;
 
