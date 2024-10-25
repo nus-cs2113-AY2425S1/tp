ae07b34cdce4112e83cc9971affa134addfa7b1a
tsoudibi <a220284268@gmail.com>
2024-10-25 00:23:58 -0600

    Refactor import statements in AppUiTest.java

--- src/test/java/seedu/duke/ui/AppUiTest.java
+++ src/test/java/seedu/duke/ui/AppUiTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import seedu.duke.financial.Expense;
 import seedu.duke.financial.FinancialEntry;
-import seedu.duke.financial.FinancialList;
 import seedu.duke.financial.Income;
 import seedu.duke.storage.Storage;
 
