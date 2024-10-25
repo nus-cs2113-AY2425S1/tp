dcd7e7b67b3fbad41860b6180390418206f10d2c
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-10-25 17:00:57 +0800

    Replace null checks as setters will throw exception if they detect null

--- src/test/java/wheresmymoney/ExpenseListTest.java
+++ src/test/java/wheresmymoney/ExpenseListTest.java
@@ -6,9 +6,10 @@ import wheresmymoney.exception.WheresMyMoneyException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class ExpenseListTest {
