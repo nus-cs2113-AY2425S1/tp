f20c7bc3e48376f64aa83368f130b2db49bef46c
Ajay Shanker <26241915+AjayShanker-geek@users.noreply.github.com>
2024-10-26 19:12:54 +0800

    Add Exit Command

--- src/main/java/seedu/duke/parser/ExitParser.java
+++ src/main/java/seedu/duke/parser/ExitParser.java
@@ -1,13 +1,15 @@
 package seedu.duke.parser;
 
 import seedu.duke.commands.Command;
+import seedu.duke.commands.ExitCommand;
 import seedu.duke.data.state.State;
 
+/**
+ * Parses the exit command.
+ */
 public class ExitParser implements CommandParser {
     @Override
     public Command execute(String line, State state) {
-        System.out.println("Exiting program...");
-        System.exit(0); // Terminates the program gracefully
-        return null;
+        return new ExitCommand();
     }
 }
