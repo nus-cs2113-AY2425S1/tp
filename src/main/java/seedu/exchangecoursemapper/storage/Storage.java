7a4ca916389d027de9493ab83b6d2f68b8f47830
wahkia <gohah021@gmail.com>
2024-10-26 13:34:18 +0800

    Create methods for saving to and loading from myList.json

--- src/main/java/seedu/exchangecoursemapper/storage/Storage.java
+++ src/main/java/seedu/exchangecoursemapper/storage/Storage.java
@@ -1,12 +1,17 @@
 package seedu.exchangecoursemapper.storage;
 
-import java.io.IOException;
+
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import seedu.exchangecoursemapper.courses.Course;
 
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonWriter;
 
 public class Storage {
     // In Storage class
@@ -28,4 +33,17 @@ public class Storage {
         }
     }
 
+    private void saveToJson(JsonObject jsonObject) throws IOException {
+        try (OutputStream os = new FileOutputStream(MYLIST_FILE_PATH)) {
+            JsonWriter writer = Json.createWriter(os);
+            writer.writeObject(jsonObject);
+        }
+    }
+
+    private JsonObject loadFromJson() throws IOException {
+        try (InputStream is = new FileInputStream(MYLIST_FILE_PATH)) {
+            JsonReader reader = Json.createReader(is);
+            return reader.readObject();
+        }
+    }
 }
