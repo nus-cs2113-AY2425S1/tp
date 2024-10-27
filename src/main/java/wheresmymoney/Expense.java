92b43ba0f66e07bc2edd59d0e82a472162ebf2bb
AndrewNguyen4 <nguyenminhduc5etp@gmail.com>
2024-10-28 05:18:41 +0800

    Change datatype of Expense.dateAdded from String to LocalDate
    - When adding/editing expense, dateAdded is a String. First, check if this string is correctly formatted to represent a date of dd-MM-yyyy. If true, then proceed to convert and add/edit.
    - When writing to CSV, dateAdded is converted back to String before written.

--- src/main/java/wheresmymoney/Expense.java
+++ src/main/java/wheresmymoney/Expense.java
@@ -2,11 +2,13 @@ package wheresmymoney;
 
 import wheresmymoney.exception.WheresMyMoneyException;
 
+import java.time.LocalDate;
+
 public class Expense {
     protected Float price;
     protected String description;
     protected String category;
-    protected String dateAdded;
+    protected LocalDate dateAdded;
 
     
     public Expense(Float price, String description, String category)
@@ -14,14 +16,17 @@ public class Expense {
         this.setPrice(price);
         this.setDescription(description);
         this.setCategory(category);
-        this.dateAdded = DateUtils.dateFormatToString(DateUtils.getCurrentDate());
+        this.dateAdded = DateUtils.getCurrentDate();
     }
     public Expense(Float price, String description, String category, String dateAdded)
             throws WheresMyMoneyException {
+        if (!DateUtils.isInDateFormat(dateAdded)){
+            throw new WheresMyMoneyException("Invalid date format" + DateUtils.DATE_FORMAT);
+        }
         this.setPrice(price);
         this.setDescription(description);
         this.setCategory(category);
-        this.setDateAdded(dateAdded);
+        this.setDateAdded(DateUtils.stringToDate(dateAdded));
     }
     
     public Float getPrice() {
@@ -33,7 +38,7 @@ public class Expense {
     public String getCategory() {
         return category;
     }
-    public String getDateAdded() {
+    public LocalDate getDateAdded() {
         return dateAdded;
     }
     
@@ -62,13 +67,9 @@ public class Expense {
         this.category = category;
         assert this.category != null : "Expense's category shouldn't be null.";
     }
-    public void setDateAdded(String dateAdded) throws WheresMyMoneyException {
+    public void setDateAdded(LocalDate dateAdded) throws WheresMyMoneyException {
         if (dateAdded == null) {
             throw new WheresMyMoneyException("Expense's date added shouldn't be null.");
-        } else if (dateAdded.isBlank()) {
-            throw new WheresMyMoneyException("Expense's date added shouldn't be blank.");
-        } else if (!DateUtils.isInDateFormat(dateAdded)) {
-            throw new WheresMyMoneyException("Expense's date added is not in recognised format.");
         }
         this.dateAdded = dateAdded;
         assert this.dateAdded != null : "Expense's date added shouldn't be null.";
--- src/main/java/wheresmymoney/ExpenseList.java
+++ src/main/java/wheresmymoney/ExpenseList.java
@@ -90,6 +90,9 @@ public class ExpenseList {
      */
     public void editExpense(int index, Float price, String description, String category, String dateAdded)
             throws WheresMyMoneyException {
+        if (!DateUtils.isInDateFormat(dateAdded)){
+            throw new WheresMyMoneyException("Invalid date format" + DateUtils.DATE_FORMAT);
+        }
         try {
             Logging.log(Level.INFO, "Attempting to edit expense.");
             Expense expense = getExpenseAtIndex(index);
@@ -97,7 +100,7 @@ public class ExpenseList {
             expense.setPrice(price);
             expense.setDescription(description);
             expense.setCategory(category);
-            expense.setDateAdded(dateAdded);
+            expense.setDateAdded(DateUtils.stringToDate(dateAdded));
             Logging.log(Level.INFO, "Successfully edited expense.");
         } catch (WheresMyMoneyException e) {
             Logging.log(Level.INFO, "Failure when editing expense.");
@@ -196,7 +199,7 @@ public class ExpenseList {
                     expense.getCategory(),
                     expense.getDescription(),
                     expense.getPrice().toString(),
-                    expense.getDateAdded()
+                    DateUtils.dateFormatToString(expense.getDateAdded())
             };
             writer.writeNext(row);
         }
