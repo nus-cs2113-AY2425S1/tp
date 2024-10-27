32ec0a1852c50f7e865772d891944f2ebaf2d123
TVageesan <59349293+TVageesan@users.noreply.github.com>
2024-10-27 17:20:06 +0800

    Move DateSerializer to Storage class

--- src/main/java/storage/DateSerializer.java
+++ src/main/java/storage/DateSerializer.java
@@ -1,4 +1,4 @@
-package history;
+package storage;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -5,7 +5,6 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import dailyrecord.DailyRecord;
-import history.DateSerializer;
 import history.History;
 import programme.ProgrammeList;
 
