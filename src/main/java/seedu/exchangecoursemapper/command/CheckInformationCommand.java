77d443003505746d068f85242bef26e31a3d2a9c
louisjoety <kolouis002@gmail.com>
2024-10-28 20:12:42 +0800

    Add javadoc to check information  command parent class

--- src/main/java/seedu/exchangecoursemapper/command/CheckInformationCommand.java
+++ src/main/java/seedu/exchangecoursemapper/command/CheckInformationCommand.java
@@ -1,5 +1,10 @@
 package seedu.exchangecoursemapper.command;
 
 public abstract class CheckInformationCommand extends Command{
+    /**
+     * Executes the command based on the provided user input.
+     *
+     * @param userInput the input provided by the user to execute the command.
+     */
     public abstract void execute(String userInput);
 }
