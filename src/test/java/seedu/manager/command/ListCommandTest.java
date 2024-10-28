6f798475be029ffcaa96763c9859b706d096adb2
LTK-1606 <tzekang1606@gmail.com>
2024-10-28 21:51:02 +0800

    Fix style errors

--- src/test/java/seedu/manager/command/ListCommandTest.java
+++ src/test/java/seedu/manager/command/ListCommandTest.java
@@ -33,8 +33,10 @@ public class ListCommandTest {
     @Test
     public void execute_twoEvents_success() {
         String expectedMessage = "There are 2 events in your list! Here are your scheduled events:\n"
-                + "1. Event name: Event 1 / Event time: 2024-10-10 10:00 / Event venue: Venue A / Event Priority: HIGH / Done: N\n"
-                + "2. Event name: Event 2 / Event time: 2024-11-11 12:00 / Event venue: Venue B / Event Priority: MEDIUM / Done: N\n";
+                + "1. Event name: Event 1 / Event time: 2024-10-10 10:00 / Event venue: Venue A / " +
+                "Event Priority: HIGH / Done: N\n"
+                + "2. Event name: Event 2 / Event time: 2024-11-11 12:00 / Event venue: Venue B / " +
+                "Event Priority: MEDIUM / Done: N\n";
 
         assertEquals(expectedMessage, listCommand.getMessage());
         assertFalse(listCommand.getCanExit());
