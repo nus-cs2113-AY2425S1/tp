86802586a679b25d91a0efdb879cda69ead9b249
lowjunchen <e1138420@u.nus.edu>
2024-10-28 21:30:47 +0800

    Run coverage tests for Parser to achieve at least 100%, and optimize code

--- src/main/java/seedu/duke/parser/DeleteParser.java
+++ src/main/java/seedu/duke/parser/DeleteParser.java
@@ -2,6 +2,7 @@ package seedu.duke.parser;
 
 import seedu.duke.commands.Command;
 import seedu.duke.commands.DeletePatientCommand;
+import seedu.duke.commands.DeleteTaskCommand;
 import seedu.duke.data.state.State;
 import seedu.duke.data.state.StateType;
 import seedu.duke.parser.parserutils.Index;
@@ -15,10 +16,9 @@ public class DeleteParser implements CommandParser {
         if (state.getState() == StateType.MAIN_STATE) {
             int id = parseInt(new Index().extract(line));
             return new DeletePatientCommand(id);
-        } else if (state.getState() == StateType.TASK_STATE) {
+        } else {
             int id = parseInt(new Index().extract(line));
-            return new DeletePatientCommand(id);
+            return new DeleteTaskCommand(id);
         }
-        return null;
     }
 }
