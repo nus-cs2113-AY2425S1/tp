9c818ec31e644baf5fb8f61bba6d721fe5326d0a
kestryix <verilim3802@gmail.com>
2024-10-28 11:17:27 +0800

    Fix checkstyle indentation

--- src/main/java/seedu/duke/FinanceBuddy.java
+++ src/main/java/seedu/duke/FinanceBuddy.java
@@ -31,36 +31,36 @@ public class FinanceBuddy {
                 command = commandArguments.get(InputParser.COMMAND);
 
                 switch (command) {
-                    case "list":
-                        logic.listHelper(commandArguments);
-                        break;
-                    case "expense":
-                        logic.addExpense(commandArguments);
-                        storage.update(financialList);
-                        break;
-                    case "income":
-                        logic.addIncome(commandArguments);
-                        storage.update(financialList);
-                        break;
-                    case "edit":
-                        logic.editEntry(commandArguments);
-                        storage.update(financialList);
-                        break;
-                    case "delete":
-                        logic.deleteEntry(commandArguments);
-                        storage.update(financialList);
-                        break;
-                    case "help":
-                        logic.printHelpMenu();
-                        break;
-                    case "exit":
-                        ExitCommand exitCommand = new ExitCommand();
-                        exitCommand.execute(financialList);
-                        isRunning = exitCommand.shouldContinueLoop();
-                        return;
-                    default:
-                        ui.showUnknownCommandMessage();
-                        break;
+                case "list":
+                    logic.listHelper(commandArguments);
+                    break;
+                case "expense":
+                    logic.addExpense(commandArguments);
+                    storage.update(financialList);
+                    break;
+                case "income":
+                    logic.addIncome(commandArguments);
+                    storage.update(financialList);
+                    break;
+                case "edit":
+                    logic.editEntry(commandArguments);
+                    storage.update(financialList);
+                    break;
+                case "delete":
+                    logic.deleteEntry(commandArguments);
+                    storage.update(financialList);
+                    break;
+                case "help":
+                    logic.printHelpMenu();
+                    break;
+                case "exit":
+                    ExitCommand exitCommand = new ExitCommand();
+                    exitCommand.execute(financialList);
+                    isRunning = exitCommand.shouldContinueLoop();
+                    return;
+                default:
+                    ui.showUnknownCommandMessage();
+                    break;
                 }
             } catch (FinanceBuddyException e) {
                 ui.showErrorMessage(e.getMessage());
