c98261bc6a57c70c5ade8897878f1fdffad5f456
Terence Chan Zun Mun <zunmun@gmail.com>
2024-10-28 16:48:52 +0800

    Add parseLineToArgumentMap tests

--- src/test/java/wheresmymoney/ParserTest.java
+++ src/test/java/wheresmymoney/ParserTest.java
@@ -2,9 +2,32 @@ package wheresmymoney;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ParserTest {
     @Test
-    public void sampleTest() {
-        assert(true);
+    public void parseLineToArgumentsMap_commandOnly_command() {
+        String inputLine = "command";
+        HashMap<String, String> argumentsMap = Parser.parseLineToArgumentsMap(inputLine);
+        assert(argumentsMap.containsKey(Parser.ARGUMENT_COMMAND));
+        assertEquals(argumentsMap.get(Parser.ARGUMENT_COMMAND), "command");
+    }
+
+    @Test
+    public void parseLineToArgumentsMap_commandWithTrailingWhitespace_command() {
+        String inputLine = "command   ";
+        HashMap<String, String> argumentsMap = Parser.parseLineToArgumentsMap(inputLine);
+        assert(argumentsMap.containsKey(Parser.ARGUMENT_COMMAND));
+        assertEquals(argumentsMap.get(Parser.ARGUMENT_COMMAND), "command");
+    }
+
+    @Test
+    public void parseLineToArgumentsMap_commandWithMainArgument_arguments() {
+        String inputLine = "command   main";
+        HashMap<String, String> argumentsMap = Parser.parseLineToArgumentsMap(inputLine);
+        assertEquals(argumentsMap.get(Parser.ARGUMENT_COMMAND), "command");
+        assertEquals(argumentsMap.get(Parser.ARGUMENT_MAIN), "main");
     }
 }
