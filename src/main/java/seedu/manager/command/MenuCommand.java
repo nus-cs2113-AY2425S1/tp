cbb7a6b8ee1ac9d6936f0a27d6cf80852e0f890f
KuanHsienn <liangkuanhsien@gmail.com>
2024-10-30 00:56:04 +0800

    Modify Storage parsing implementation

--- src/main/java/seedu/manager/command/MenuCommand.java
+++ src/main/java/seedu/manager/command/MenuCommand.java
@@ -12,12 +12,13 @@ public class MenuCommand extends Command {
             add -e EVENT -t TIME -v VENUE: Add an event to the event list.
             list: List events.
             remove -e EVENT: Remove an event from the event list.
-            add -p PARTICIPANT -e EVENT: Add a participant to an event.
+            add -p PARTICIPANT -n NUMBER -email EMAIL -e EVENT: Add a participant to an event.
             view -e EVENT: View the list of participants of an event.
             remove -p PARTICIPANT -e EVENT: Remove a participant from an event.
             mark -e EVENT -s STATUS: Mark an event as done or not done.
             mark -p PARTICIPANT -e EVENT -s STATUS: Mark a participant as present or absent.
-            sort -by KEYWORD: Sorts events by name/time/priority.""";
+            sort -by KEYWORD: Sorts events by name/time/priority.
+            exit: Exit program""";
 
     /**
      * Constructs a new MenuCommand
--- src/main/java/seedu/manager/parser/Parser.java
+++ src/main/java/seedu/manager/parser/Parser.java
@@ -12,12 +12,16 @@ import seedu.manager.command.ListCommand;
 import seedu.manager.command.ViewCommand;
 import seedu.manager.command.SortCommand;
 import seedu.manager.exception.InvalidCommandException;
+import seedu.manager.event.EventList;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Set;
 import java.util.logging.Logger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 import static java.util.logging.Level.WARNING;
 
@@ -341,6 +345,42 @@ public class Parser {
         } catch (IndexOutOfBoundsException exception) {
             logger.log(WARNING, "Invalid command format");
             throw new InvalidCommandException(INVALID_SORT_MESSAGE);
+        }
+    }
+
+    //@@author KuanHsienn
+    /**
+     * Parses a CSV file containing event details and loads the events into the specified EventList.
+     *
+     * This method reads each line from the specified file, expecting the format to be:
+     * <pre>
+     * eventName, eventTime, eventVenue
+     * </pre>
+     * where:
+     * - eventName is a String representing the name of the event.
+     * - eventTime is a String formatted as "yyyy-MM-dd HH:mm" that will be parsed into a LocalDateTime object.
+     * - eventVenue is a String representing the venue of the event.
+     *
+     * If a line does not contain exactly three parts, it is skipped.
+     *
+     * @param events The EventList where the parsed events will be added.
+     * @param filePath The path to the file containing the event details.
+     * @throws IOException If there is an error reading from the file or if the file cannot be found.
+     */
+    public void parseFile(EventList events, String filePath) throws IOException {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+            for (String line : Files.readAllLines(Paths.get(filePath))) {
+                String[] parts = line.split(","); // CSV format
+                if (parts.length == 3) {
+                    String eventName = parts[0].trim();
+                    LocalDateTime time = LocalDateTime.parse(parts[1].trim(), formatter);
+                    String venue = parts[2].trim();
+                    events.addEvent(eventName, time, venue);
+                }
+            }
+        } catch (IOException exception) {
+            throw new IOException("Error loading events from file: " + filePath + ".");
         }
     }
 }
