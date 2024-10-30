ebf9277d03eca03155b47ad1f411e64498689d16
Jiaming <shenjiaming27@gmail.com>
2024-10-30 17:04:09 +0800

    Add AmountUtils class to parse amountStrings to double

--- src/main/java/seedu/utils/AmountUtils.java
+++ src/main/java/seedu/utils/AmountUtils.java
@@ -1,4 +1,36 @@
 package seedu.utils;
 
+/**
+ * Utility class for validating and parsing monetary amounts.
+ */
 public class AmountUtils {
+
+    /**
+     * Validates if the provided amount string is a valid positive decimal number.
+     *
+     * @param amountString The amount in string format to validate.
+     * @return true if the amount is valid, false otherwise.
+     */
+    public static boolean isValidAmount(String amountString) {
+        try {
+            double amount = Double.parseDouble(amountString);
+            return amount > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Parses the amount from a string to a double.
+     *
+     * @param amountString The amount in string format to parse.
+     * @return The parsed amount as a double.
+     * @throws IllegalArgumentException if the amount is invalid.
+     */
+    public static double parseAmount(String amountString) throws IllegalArgumentException {
+        if (!isValidAmount(amountString)) {
+            throw new IllegalArgumentException("Invalid amount format: " + amountString);
+        }
+        return Double.parseDouble(amountString);
+    }
 }
