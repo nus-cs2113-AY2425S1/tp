8794a9833f0162b888a787ddcdb18a1c3a74aaa3
Ridiculouswifi <thtan02@gmail.com>
2024-10-30 19:49:24 +0800

    Add CalendarCommand class

---
+++ src/main/java/seedu/commands/CalendarCommand.java
@@ -0,0 +1,43 @@
+package seedu.commands;
+
+//@@author Ridiculouswifi
+
+import seedu.duke.Deadline;
+import seedu.duke.Internship;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Subclass of <code>Command</code> to display all deadlines from every <code>Intenship</code> entry.
+ */
+public class CalendarCommand extends Command {
+    @Override
+    public void execute(ArrayList<String> args) {
+        ArrayList<Deadline> deadlines = getDeadlines();
+        ArrayList<Deadline> sortedDeadlines = sortDeadlines(deadlines);
+    }
+
+    protected ArrayList<Deadline> getDeadlines() {
+        List<Internship> allInternships = internships.getAllInternships();
+        ArrayList<Deadline> deadlines = new ArrayList<>();
+        for (Internship internship : allInternships) {
+            deadlines.addAll(internship.getDeadlines());
+        }
+        return deadlines;
+    }
+
+    protected ArrayList<Deadline> sortDeadlines(ArrayList<Deadline> deadlines) {
+        deadlines.sort(Comparator.comparing(deadline -> deadline.getDate()));
+        return deadlines;
+    }
+
+    @Override
+    public String getUsage() {
+        return """
+                calendar
+                Usage: calendar""";
+    }
+}
