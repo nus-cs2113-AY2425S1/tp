afc4a3cb1697df3deebe787ae39edeb32a2aa6a4
KuanHsienn <liangkuanhsien@gmail.com>
2024-10-30 22:54:00 +0800

    Update EXPECTED.TXT

--- src/main/java/seedu/manager/command/EditCommand.java
+++ src/main/java/seedu/manager/command/EditCommand.java
@@ -2,11 +2,11 @@ package seedu.manager.command;
 
 /**
  * Represents a command to edit an event to the event list.
- * The add command will store the event's name, time, and venue.
+ * The edit command will store the event's name, time, and venue.
  */
 public class EditCommand extends Command{
     public static final String COMMAND_WORD = "edit";
-    private static final String EDIT_PARTICIPANT_MESSAGE = "Participant email successfully updated";
+    private static final String EDIT_PARTICIPANT_MESSAGE = "Participant contact information successfully updated";
     private static final String EDIT_FAILURE_MESSAGE = "Event/Participant not found";
     protected String eventName;
     protected String participantName;
@@ -19,7 +19,7 @@ public class EditCommand extends Command{
      * Constructs an EditCommand object with the specified participant name, participant number,
      * participant email, and event name.
      *
-     * @param participantName The name of the participant to be updated.
+     * @param participantName The name of the participant.
      * @param participantNumber The new phone number of the participant.
      * @param participantEmail The new email address of the participant.
      * @param eventName The name of the event associated with the participant.
