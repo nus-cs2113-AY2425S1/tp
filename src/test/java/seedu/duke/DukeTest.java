c43b00ca3ed88f4ba61198046c22c1eddaba30ff
glenda_1506 <glenda.xuanyin@gmail.com>
2024-10-31 09:01:20 +0800

    Merge CategoryManagerTest.java and ExpenseManagerTest.java into DukeTest.java

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -2,9 +2,8 @@
 package seedu.duke;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class ExpenseTest {
     @Test
@@ -123,5 +122,152 @@ class BudgetTest {
         Category category = new Category("Entertainment");
         Budget budget = new Budget(category, 200);
         assertEquals("Budget for category 'Entertainment' is $200", budget.toString());
+    }
+}
+
+//@@author AdiMangalam
+class CategoryManagerTest {
+    @Test
+    void addCategoryNewCategory() {
+        CategoryManager categoryManager = new CategoryManager();
+        TrackerData trackerData = new TrackerData();
+
+        categoryManager.addCategory(trackerData, "add-category Food");
+
+        assertEquals(1, trackerData.getCategories().size());
+        assertEquals("Food", trackerData.getCategories().get(0).getName());
+    }
+
+    @Test
+    void addCategoryExistingCategory() {
+        CategoryManager categoryManager = new CategoryManager();
+        TrackerData trackerData = new TrackerData();
+
+        trackerData.getCategories().add(new Category("Food"));
+
+        categoryManager.addCategory(trackerData, "add-category Food");
+
+        assertEquals(1, trackerData.getCategories().size());
+    }
+
+    @Test
+    void addCategory_doesNotAddIfNameIsEmpty() {
+        CategoryManager categoryManager = new CategoryManager();
+        TrackerData trackerData = new TrackerData();
+
+        categoryManager.addCategory(trackerData, "add-category  ");
+
+        assertTrue(trackerData.getCategories().isEmpty());
+    }
+}
+
+class ExpenseManagerTest {
+
+    @Test
+    public void addExpenseNewCategory() {
+        ExpenseManager expenseManager = new ExpenseManager();
+        TrackerData trackerData = new TrackerData();
+
+        expenseManager.addExpense(trackerData,"Coffee", 2.0, "Food");
+
+        assertEquals(1, trackerData.getExpenses().size());
+        assertEquals("Coffee", trackerData.getExpenses().get(0).getName());
+        assertEquals(2.00, trackerData.getExpenses().get(0).getAmount());
+        assertEquals("Food", trackerData.getExpenses().get(0).getCategory().getName());
+
+        assertEquals(1, trackerData.getCategories().size());
+        assertEquals("Food", trackerData.getCategories().get(0).getName());
+    }
+
+    @Test
+    void addExpenseExistingCategory() {
+        TrackerData trackerData = new TrackerData();
+        ExpenseManager expenseManager = new ExpenseManager();
+
+        Category existingCategory = new Category("Food");
+        trackerData.getCategories().add(existingCategory);
+
+        expenseManager.addExpense(trackerData, "Dinner", 20.00, "Food");
+
+        assertEquals(1, trackerData.getCategories().size());
+        assertEquals("Food", trackerData.getExpenses().get(0).getCategory().getName());
+    }
+
+    @Test
+    void deleteExpenseValidIndex() {
+        TrackerData trackerData = new TrackerData();
+        ExpenseManager expenseManager = new ExpenseManager();
+
+        expenseManager.addExpense(trackerData, "Coffee", 3.5, "Food");
+        expenseManager.addExpense(trackerData, "Bus", 1.2, "Transport");
+
+        assertEquals(2, trackerData.getCategories().size());
+
+        expenseManager.deleteExpense(trackerData, 0);
+        assertEquals(1, trackerData.getExpenses().size());
+        assertEquals("Bus", trackerData.getExpenses().get(0).getName());
+        assertEquals(1.2, trackerData.getExpenses().get(0).getAmount());
+        assertEquals("Transport", trackerData.getExpenses().get(0).getCategory().getName());
+
+        expenseManager.deleteExpense(trackerData, 0);
+        assertTrue(trackerData.getExpenses().isEmpty());
+    }
+
+    @Test
+    void deleteExpenseInvalidIndex() {
+        TrackerData trackerData = new TrackerData();
+        ExpenseManager expenseManager = new ExpenseManager();
+
+        expenseManager.addExpense(trackerData, "Chocolates", 2.5, "Snacks");
+
+        expenseManager.deleteExpense(trackerData, 1);
+
+        assertEquals(1, trackerData.getExpenses().size());
+        assertEquals("Chocolates", trackerData.getExpenses().get(0).getName());
+    }
+
+    @Test
+    void tagExpenseValidInput() {
+        TrackerData trackerData = new TrackerData();
+        ExpenseManager expenseManager = new ExpenseManager();
+
+
+        expenseManager.addExpense(trackerData, "Train", 4, "Transport");
+
+        Category existingCategory = new Category("Travel");
+        trackerData.getCategories().add(existingCategory);
+
+        expenseManager.tagExpense(trackerData, "e/1 c/Travel");
+
+        assertEquals("Travel", trackerData.getExpenses().get(0).getCategory().getName());
+        assertEquals(2,trackerData.getCategories().size());
+    }
+
+    @Test
+    void tagExpenseInvalidIndex() {
+        TrackerData trackerData = new TrackerData();
+        ExpenseManager expenseManager = new ExpenseManager();
+
+        Category category = new Category("Food");
+        trackerData.getCategories().add(category);
+
+        expenseManager.addExpense(trackerData, "Waffle", 11.5, "Food");
+        expenseManager.tagExpense(trackerData, "e/2 c/Snack");
+
+        assertEquals("Food", trackerData.getExpenses().get(0).getCategory().getName());
+    }
+
+    @Test
+    void tagExpenseInvalidCategory() {
+        TrackerData trackerData = new TrackerData();
+        ExpenseManager expenseManager = new ExpenseManager();
+
+        Expense expense = new Expense("Bus Fare", 2.50, new Category("Transport"));
+        trackerData.getExpenses().add(expense);
+
+        expenseManager.addExpense(trackerData,"Bus", 1.25, "Transport");
+        expenseManager.tagExpense(trackerData, "e/1 c/Travel");
+
+        assertEquals("Transport", trackerData.getExpenses().get(0).getCategory().getName());
     }
 }
