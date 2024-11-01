4cde40b3740749764c354b5a529f2459af01941e
Ayushi <e0905342@u.nus.edu>
2024-11-01 14:49:19 +0800

    chars

--- src/main/java/fittrack/messages/Messages.java
+++ src/main/java/fittrack/messages/Messages.java
@@ -64,8 +64,8 @@ public class Messages {
             + System.lineSeparator() + "list-food                                                        list-food"
             + System.lineSeparator() + "add-calories (calorie amount)                                    add-calories"
             + "200"
-            + System.lineSeparator() + "delete-calories (index)                                          delete-calories"
-            +"1"
+            + System.lineSeparator() + "delete-calories (index)                                          "
+            + "delete-calories 1"
             + System.lineSeparator() + "list-calories                                                    list-calories"
             + System.lineSeparator() + "exit                                                             exit";
     public static final String LIST_SESSION_MESSAGE = "Here are your training sessions:";
