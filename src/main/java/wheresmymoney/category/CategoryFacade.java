750211b8b860d8a63eb52bb5ee46cde29fb7ab3e
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-11-02 01:50:03 +0800

    Add javadoc for CategoryFacade

--- src/main/java/wheresmymoney/category/CategoryFacade.java
+++ src/main/java/wheresmymoney/category/CategoryFacade.java
@@ -5,6 +5,17 @@ import wheresmymoney.ExpenseList;
 import wheresmymoney.exception.StorageException;
 import wheresmymoney.exception.WheresMyMoneyException;
 
+/**
+ * The {@code CategoryFacade} class provides a simplified interface for category management.
+ *
+ * <p>
+ * It acts as a facade to the underlying CategoryTracker and CategoryFilter components,
+ * allowing commands to perform operations on categories such as:
+ * <li> CRUD operations </li>
+ * <li> filtering operations </li>
+ * <li> file operations </li>
+ * </p>
+ */
 public class CategoryFacade {
     private CategoryTracker categoryTracker;
     private CategoryFilter categoryFilter;
@@ -21,31 +32,75 @@ public class CategoryFacade {
         return categoryTracker;
     }
     
+    /**
+     * The interface for {@code AddCommand} when the user adds a new Expense.
+     *
+     * @param category the category of the newly added Expense
+     * @param price the price of the newly added Expense
+     * @throws WheresMyMoneyException if there is an error while adding the category
+     */
     public void addCategory(String category, float price) throws WheresMyMoneyException {
         categoryTracker.addCategory(category, price);
         categoryTracker.checkLimitOf(category);
     }
+    /**
+     * The interface for {@code DeleteCommand} when the user deletes an Expense.
+     *
+     * @param category the category of the Expense to be deleted
+     * @param price the price of the Expense to be deleted
+     * @throws WheresMyMoneyException if there is an error while deleting the category
+     */
     public void deleteCategory(String category, Float price) throws WheresMyMoneyException {
         categoryTracker.deleteCategory(category, price);
     }
+    /**
+     * The interface for {@code EditCommand} when the user edits an Expense.
+     *
+     * @param oldCategory the current name of the category to be edited
+     * @param newCategory the new name for the category
+     * @param price the price of the Expense to be edited
+     * @throws WheresMyMoneyException if there is an error while editing the category
+     */
     public void editCategory(String oldCategory, String newCategory, float price) throws WheresMyMoneyException {
         categoryTracker.editCategory(oldCategory, newCategory, price);
         categoryTracker.checkLimitOf(newCategory);
     }
     
+    /**
+     * The interface for {@code LoadCommand} to load category information from a CSV file.
+     *
+     * @param expenseList the list of expenses to track category information
+     * @throws WheresMyMoneyException if there is an error while loading category info
+     */
     public void loadCategoryInfo(ExpenseList expenseList) throws WheresMyMoneyException {
         categoryStorage.loadFromCsv("./category_spending_limit.csv",
                 categoryStorage.trackCategoriesOf(expenseList.getExpenseList()));x`
     }
+    /**
+     * The interface for {@code LoadCommand} to show filtered categories
+     * based on spending limits.
+     */
     public void displayFilteredCategories() {
         categoryFilter.getCategoriesFiltered();
         categoryFilter.displayExceededCategories();
         categoryFilter.displayNearingCategories();
     }
+    /**
+     * The interface for {@code SaveCommand} to save current category information to a CSV file.
+     *
+     * @throws StorageException if there is an error while saving category info
+     */
     public void saveCategoryInfo() throws StorageException {
         CategoryStorage.saveToCsv("./category_spending_limit.csv",
                 categoryTracker.getTracker());
     }
+    /**
+     * The interface for {@code SetCommand} to set a spending limit for a specified category.
+     *
+     * @param category the name of the category for which to set the spending limit for
+     * @param limit the spending limit to set for the category
+     * @throws WheresMyMoneyException if there is an error while setting the spending limit
+     */
     public void setCategorySpendingLimit(String category, float limit) throws WheresMyMoneyException {
         categoryTracker.setSpendingLimitFor(category, limit);
     }
