a708cf26ed8b2b19ea04d790d0edd2f15fd17a02
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-11-01 13:50:27 +0800

    Add JUnit tests

--- src/test/java/wheresmymoney/CategoryDataTest.java
+++ src/test/java/wheresmymoney/CategoryDataTest.java
@@ -1,4 +1,84 @@
-import static org.junit.jupiter.api.Assertions.*;
+package wheresmymoney;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import wheresmymoney.exception.WheresMyMoneyException;
+
 class CategoryDataTest {
-  
+    
+    private CategoryData initCategoryDataWithNoNullParts() throws WheresMyMoneyException {
+        return new CategoryData(50.0F, 100.0F);
+    }
+    
+    @Test
+    public void createCategoryData_nullCurrExpenditure_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> new CategoryData(null));
+    }
+    @Test
+    public void createCategoryData_nullCurrExpenditureWithNotNullMaxExpenditure_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> new CategoryData(null, 100.00F));
+    }
+    @Test
+    public void createCategoryData_NotNullCurrExpenditureWithNullMaxExpenditure_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> new CategoryData(50.00F, null));
+    }
+    
+    @Test
+    public void setCurrExpenditure_nullInput_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> initCategoryDataWithNoNullParts().setCurrExpenditure(null));
+    }
+    @Test
+    public void setMaxExpenditure_nullInput_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> initCategoryDataWithNoNullParts().setMaxExpenditure(null));
+    }
+    
+    @Test
+    public void increaseCurrExpenditureBy_nullInput_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> initCategoryDataWithNoNullParts().increaseCurrExpenditureBy(null));
+    }
+    @Test
+    public void decreaseCurrExpenditureBy_nullInput_throwsWheresMyMoneyException() {
+        assertThrows(WheresMyMoneyException.class,
+            () -> initCategoryDataWithNoNullParts().decreaseCurrExpenditureBy(null));
+    }
+    
+    @Test
+    public void isNearingLimit_expenditureBelow80Percent_returnsFalse() throws WheresMyMoneyException {
+        CategoryData categoryData = new CategoryData(79.99F, 100.0F);
+        assertFalse(categoryData.isNearingLimit());
+    }
+    @Test
+    public void isNearingLimit_expenditureEquals80Percent_returnsTrue() throws WheresMyMoneyException {
+        CategoryData categoryData = new CategoryData(80.00F, 100.0F);
+        assertTrue(categoryData.isNearingLimit());
+    }
+    @Test
+    public void isNearingLimit_expenditureAbove80Percent_returnsTrue() throws WheresMyMoneyException {
+        CategoryData categoryData = new CategoryData(80.01F, 100.0F);
+        assertTrue(categoryData.isNearingLimit());
+    }
+    @Test
+    public void hasExceededLimit_expenditureBelowMax_returnsFalse() throws WheresMyMoneyException {
+        CategoryData categoryData = new CategoryData(99.99F, 100.00F);
+        assertFalse(categoryData.hasExceededLimit());
+    }
+    @Test
+    public void hasExceededLimit_expenditureEqualsMax_returnsFalse() throws WheresMyMoneyException {
+        CategoryData categoryData = new CategoryData(100.00F, 100.00F);
+        assertFalse(categoryData.hasExceededLimit());
+    }
+    @Test
+    public void hasExceededLimit_expenditureAboveMax_returnsTrue() throws WheresMyMoneyException {
+        CategoryData categoryData = new CategoryData(100.01F, 100.00F);
+        assertTrue(categoryData.hasExceededLimit());
+    }
 }
--- src/test/java/wheresmymoney/CategoryTrackerTest.java
+++ src/test/java/wheresmymoney/CategoryTrackerTest.java
@@ -1,4 +1,50 @@
-import static org.junit.jupiter.api.Assertions.*;
+package wheresmymoney;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import wheresmymoney.exception.WheresMyMoneyException;
+
 class CategoryTrackerTest {
-  
+    
+    @Test
+    void getCategoryDataOf_categoryInMap_returnsCategoryData() {
+        try {
+            CategoryTracker tracker = new CategoryTracker();
+            tracker.addCategory("category", 0F);
+            CategoryData actual = tracker.getCategoryDataOf("category");
+            CategoryData expected = new CategoryData(0F, 100F);
+            assertEquals(actual.getCurrExpenditure(), expected.getCurrExpenditure());
+            assertEquals(actual.getMaxExpenditure(), expected.getMaxExpenditure());
+        } catch (WheresMyMoneyException e) {
+            fail("Exception thrown when Category and Price are valid.");
+        }
+    }
+    @Test
+    void getCategoryDataOf_categoryNotInMap_throwsWheresMyMoneyException() {
+        CategoryTracker tracker = new CategoryTracker();
+        assertThrows(WheresMyMoneyException.class,
+                () -> tracker.getCategoryDataOf("category"));
+    }
+    
+    @Test
+    void addCategory_categoryNotInMap_categoryAddedToMap() {
+        CategoryTracker tracker = new CategoryTracker();
+    }
+    @Test
+    void addCategory_categoryInMap_incrementsThatCategoryCurrExpenditure() {
+        CategoryTracker tracker = new CategoryTracker();
+    }
+    
+    @Test
+    void deleteCategory_categoryInMap_decrementsCurrExpenditure() {
+        CategoryTracker tracker = new CategoryTracker();
+    }
+    @Test
+    void deleteCategory_categoryInMap_removesCategoryFromMap() {
+        CategoryTracker tracker = new CategoryTracker();
+    }
 }
