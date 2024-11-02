2dea2f1cd591b79c4126d560c126539a8e4a5787
nirala-ts <tanishka.nirala@gmail.com>
2024-11-02 13:34:01 +0800

    Remove * from import statements

--- src/test/java/parser/FlagParserTest.java
+++ src/test/java/parser/FlagParserTest.java
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FlagParserTest {
 
