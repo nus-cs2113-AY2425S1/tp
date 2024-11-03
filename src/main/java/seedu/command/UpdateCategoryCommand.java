43e1c8fb5c54d6f825cd617c2de422a6b537f9db
LinhHuynhLeDan <22520759@ms.uit.edu.vn>
2024-11-03 15:59:21 +0800

    Format command to follow the parser format

--- src/main/java/seedu/command/UpdateCategoryCommand.java
+++ src/main/java/seedu/command/UpdateCategoryCommand.java
@@ -1,47 +1,97 @@
 package seedu.command;
 
 import seedu.category.Category;
-import seedu.transaction.TransactionEditor;
+import seedu.category.CategoryList;
+import seedu.message.ErrorMessages;
+import seedu.message.CommandResultMessages;
+import seedu.transaction.Transaction;
+import seedu.transaction.TransactionList;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class UpdateCategoryCommand extends Command {
-    private final int transactionIndex;
-    private final Category newCategory;
-    private final TransactionEditor transactionEditor;
+    public static final String COMMAND_WORD = "categorize";
+    public static final String COMMAND_GUIDE = "categorize i/ INDEX c/ CATEGORY: " +
+            "Categorize an expense at the given index";
+    public static final String[] COMMAND_MANDATORY_KEYWORDS = {"i/", "c/"};
+    public static final String[] COMMAND_EXTRA_KEYWORDS = {};
 
-    public UpdateCategoryCommand(int transactionIndex, Category newCategory, TransactionEditor transactionEditor) {
-        this.transactionIndex = transactionIndex;
-        this.newCategory = newCategory;
-        this.transactionEditor = transactionEditor;
+    private final TransactionList transactions;
+    private final CategoryList categories;
+
+    public UpdateCategoryCommand(TransactionList transactions, CategoryList categories) {
+        this.transactions = transactions;
+        this.categories = categories;
     }
 
     @Override
     public List<String> execute() {
-        boolean isUpdated = transactionEditor.updateCategory(transactionIndex, newCategory);
-        if (isUpdated) {
-            return List.of("Category updated successfully for transaction at index " +  transactionIndex);
-        } else {
-            return List.of("Failed to update category. Please check the transaction index.");
+        if (!isArgumentsValid()) {
+            List<String> messages = new ArrayList<>();
+            messages.add(ErrorMessages.LACK_ARGUMENTS_ERROR_MESSAGE);
+            messages.add(COMMAND_GUIDE);
+            return messages;
+        }
+
+        String indexString = arguments.get(COMMAND_MANDATORY_KEYWORDS[0]);
+        String categoryString = arguments.get(COMMAND_MANDATORY_KEYWORDS[1]);
+
+        try {
+            int transactionIndex = Integer.parseInt(indexString) - 1;
+            Category newCategory = categories.findCategory(categoryString);
+
+            if (newCategory == null) {
+                throw new Exception(ErrorMessages.CATEGORY_NOT_FOUND);
+            }
+
+            Transaction temp = transactions.updateCategory(transactionIndex, newCategory);
+
+            return List.of(CommandResultMessages.UPDATE_TRANSACTION_SUCCESS + temp.toString());
+        } catch (NumberFormatException e) {
+            return List.of(CommandResultMessages.UPDATE_TRANSACTION_FAIL + ErrorMessages.INVALID_NUMBER_FORMAT);
+        } catch (Exception e) {
+            return List.of(CommandResultMessages.UPDATE_TRANSACTION_FAIL + e.getMessage());
         }
     }
 
+    /**
+     * Gets the mandatory keywords for the command.
+     *
+     * @return An array of strings containing the mandatory keywords associated with this command.
+     */
     @Override
     protected String[] getMandatoryKeywords() {
-        return new String[0];
+        return COMMAND_MANDATORY_KEYWORDS;
     }
 
+    /**
+     * Gets the extra keywords for the command.
+     *
+     * @return An array of strings containing the extra keywords associated with this command.
+     */
     @Override
     protected String[] getExtraKeywords() {
-        return new String[0];
+        return COMMAND_EXTRA_KEYWORDS;
     }
 
+    /**
+     * Gets the word for the command.
+     *
+     * @return A string representing the command word.
+     */
     @Override
     protected String getCommandWord() {
-        return "";
+        return COMMAND_WORD;
     }
 
+    /**
+     * Gets the guide for the command.
+     *
+     * @return A string representing the command guide.
+     */
     @Override
     protected String getCommandGuide() {
-        return "";
+        return COMMAND_GUIDE;
     }
 }
