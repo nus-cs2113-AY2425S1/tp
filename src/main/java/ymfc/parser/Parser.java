87ebb615ad79bd1e96155fe3d83394c16b2344ab
gskang <gskang2002@gmail.com>
2024-11-03 21:43:32 +0800

    Fix checkstyleerror

--- src/main/java/ymfc/parser/Parser.java
+++ src/main/java/ymfc/parser/Parser.java
@@ -23,10 +23,14 @@ import ymfc.list.IngredientList;
 import ymfc.list.RecipeList;
 import ymfc.recipe.Recipe;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.HashSet;
 
 /**
  * Parse user input commands
