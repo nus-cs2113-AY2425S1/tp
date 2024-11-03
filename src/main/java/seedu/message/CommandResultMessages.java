655cf1c853f7345409fe56dc5adacce584c6d7bb
LinhHuynhLeDan <22520759@ms.uit.edu.vn>
2024-11-03 15:55:40 +0800

    Add message files

---
+++ src/main/java/seedu/message/CommandResultMessages.java
@@ -0,0 +1,30 @@
+package seedu.message;
+
+public class CommandResultMessages {
+    public static final String UPDATE_TRANSACTION_SUCCESS = "Transaction updated: ";
+    public static final String UPDATE_TRANSACTION_FAIL = "Failed to update transaction: ";
+
+    public static final String DELETE_CATEGORY_SUCCESS = "Category deleted: ";
+    public static final String DELETE_CATEGORY_FAIL = "Failed to delete category: ";
+
+    public static final String DELETE_TRANSACTION_SUCCESS = "Transaction deleted: ";
+    public static final String DELETE_TRANSACTION_FAIL = "Failed to delete transaction: ";
+
+    public static final String ADD_CATEGORY_SUCCESS = "Category added: ";
+    public static final String ADD_CATEGORY_FAIL = "Error creating category: ";
+
+    public static final String ADD_TRANSACTION_SUCCESS = "Transaction added: ";
+    public static final String ADD_TRANSACTION_FAIL = "Error creating transaction: ";
+
+    public static final String SET_BUDGET_SUCCESS = "Budget set: ";
+    public static final String SET_BUDGET_FAIL = "Error setting budget: ";
+
+    public static final String FIND_KEYWORD_EMPTY = "No match found";
+
+    public static final String VIEW_TRANSACTION_FAIL= "Failed to retrieve transaction: ";
+    public static final String VIEW_TRANSACTION_EMPTY = "No transaction to show!";
+
+    public static final String VIEW_CATEGORY_EMPTY = "No category to show!";
+
+    public static final String END_MESSAGE = "Closing! See you tomorrow!";
+}
