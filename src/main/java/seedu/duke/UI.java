80b507917bf6805dcb90b323c5f2516b933f9c91
glenda_1506 <glenda.xuanyin@gmail.com>
2024-11-05 22:37:01 +0800

    Add constants for messages and formatting

--- src/main/java/seedu/duke/UI.java
+++ src/main/java/seedu/duke/UI.java
@@ -1,4 +1,12 @@
 package seedu.duke;
 
 public class UI {
+    public static final String SEPARATOR = "_".repeat(30);;
+    public static final String WELCOME_MESSAGE = "Hiya! How can I assist?";
+    public static final String EXIT_MESSAGE = "Goodbye! :> Hope to see you again soon!";
+    public static final String DATA_LOADED_MESSAGE = "Data loaded successfully";
+    public static final String DATA_SAVED_MESSAGE = "Data has been saved!";
+    public static final String ERROR_LOADING_DATA_MESSAGE = "Error loading data: ";
+    public static final String ERROR_SAVING_DATA_MESSAGE = "Error saving data: ";
+    public static final String INVALID_INPUT_MESSAGE = "Please enter a valid command!";
 }
