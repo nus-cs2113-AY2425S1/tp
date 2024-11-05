6b9f7004d8312d50988735c38d6e80b474b98b14
BevLow <lowbeverlypersonal@gmail.com>
2024-11-06 00:13:06 +0800

    Add Test Cases

--- src/test/java/storage/StorageTest.java
+++ src/test/java/storage/StorageTest.java
@@ -1,5 +1,6 @@
 package storage;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import history.History;
 import org.junit.jupiter.api.AfterEach;
@@ -22,18 +23,8 @@ public class StorageTest {
 
     @BeforeEach
     public void setUp() throws NoSuchFieldException, IllegalAccessException {
-        try {
-            mockFileManager = Mockito.mock(FileManager.class);
-
-            storage = new Storage("./src/test/resources/test_data.json");
-
-            Field fileManagerField = Storage.class.getDeclaredField("fileManager");
-            fileManagerField.setAccessible(true);
-            fileManagerField.set(storage, mockFileManager);
-
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new RuntimeException("Failed to set up mock for FileManager", e);
-        }
+        mockFileManager = Mockito.mock(FileManager.class);
+        storage = new Storage("./src/test/resources/test_data.json");
     }
 
     @AfterEach
@@ -42,70 +33,48 @@ public class StorageTest {
         mockFileManager = null;
     }
 
-    @Test
-    public void testLoadProgrammeList_validList() throws Exception {
-        //ProgrammeList programmeList = storage.loadProgrammeList();
-
-        //assertNotNull(programmeList);
-        //Programme validProgramme = programmeList.getProgramme(0);
-        //assertNotNull(validProgramme);
-        //assertEquals(1, programmeList.getProgrammeListSize());
-        assertTrue(true);
+    private JsonObject createValidJson() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.add("programmeList", createProgrammeList());
+        jsonObject.add("history", createHistory());
+        return jsonObject;
     }
 
     @Test
-    public void testLoadProgrammeList_invalidList() throws Exception {
-        //ProgrammeList programmeList = storage.loadProgrammeList();
-
-        //assertNotNull(programmeList);
-        //Programme invalidProgramme = programmeList.getProgramme(1);
-        //assertNull(invalidProgramme);
-        //assertEquals(1, programmeList.getProgrammeListSize());
-        assertTrue(true);
-    }
-
-    @Test
-    public void testLoadProgrammeList_emptyList() throws Exception {
-        JsonObject emptyJsonObject = new JsonObject();
-        when(mockFileManager.loadProgrammeList()).thenReturn(emptyJsonObject);
-
+    public void testLoadProgrammeList_validJson() throws Exception {
+        JsonObject jsonObject = createValidJson();
+        when(mockFileManager.loadProgrammeList()).thenReturn(jsonObject);
         ProgrammeList programmeList = storage.loadProgrammeList();
-
         assertNotNull(programmeList);
-        assertEquals(0, programmeList.getProgrammeListSize());
+        assertEquals();
+    }
+
+    @Test
+    public void testLoadProgrammeList_invalidJson() throws Exception {
+    }
+
+    @Test
+    public void testLoadProgrammeList_null() throws Exception {
+        JsonObject jsonObject = null;
+        when(mockFileManager.loadProgrammeList()).thenReturn(jsonObject);
+        ProgrammeList programmeList = storage.loadProgrammeList();
+        assertNotNull(programmeList);
     }
 
     @Test
     public void testLoadHistory_validHistory() throws Exception {
-        //History result = storage.loadHistory();
-        //Day validDay = result.getDayByDate(LocalDate.parse("12/12/2024",
-        //        DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-
-        //assertNotNull(validDay);
-        //assertEquals(1, result.getHistorySize());
-        assertTrue(true);
     }
 
     @Test
     public void testLoadHistory_invalidHistory() throws Exception {
-        //History result = storage.loadHistory();
-        //Day validDay = result.getDayByDate(LocalDate.parse("13/12/2024",
-        //        DateTimeFormatter.ofPattern("dd/MM/yyyy")));
-
-        //assertNull(validDay);
-        //assertEquals(1, result.getHistorySize());
-        assertTrue(true);
     }
 
     @Test
     public void testLoadHistory_emptyHistory() throws Exception {
-        JsonObject emptyJsonObject = new JsonObject();
-        when(mockFileManager.loadHistory()).thenReturn(emptyJsonObject);
-
-        History result = storage.loadHistory();
-
-        assertNotNull(result);
-        assertEquals(0, result.getHistorySize());
+        JsonObject jsonObject = null;
+        when(mockFileManager.loadHistory()).thenReturn(jsonObject);
+        History history = storage.loadHistory();
+        assertNotNull(history);
     }
 
     @Test
@@ -122,5 +91,125 @@ public class StorageTest {
     @Test
     public void testSave_filePathIsRestricted() throws Exception {
         assertTrue(true);
+    }
+
+    private JsonObject createProgrammeList() {
+        JsonObject programmeListObject = new JsonObject();
+        programmeListObject.addProperty("currentActiveProgramme", 0);
+
+        JsonArray programmeArray = new JsonArray();
+        JsonObject programme = new JsonObject();
+        programme.addProperty("programmeName", "Starter");
+
+        JsonArray dayList = new JsonArray();
+
+        dayList.add(createDayOne());
+        dayList.add(createDayTwo());
+
+        programme.add("dayList", dayList);
+        programmeArray.add(programme);
+        programmeListObject.add("programmeList", programmeArray);
+
+        return programmeListObject;
+    }
+
+    private JsonObject createHistory() {
+        JsonObject history = new JsonObject();
+
+        JsonObject historyEntry = new JsonObject();
+        historyEntry.add("day", createDay());
+        historyEntry.add("mealList", createMeals());
+        historyEntry.add("water", createWater());
+        history.add("12-12-2024", historyEntry);
+
+        return history;
+    }
+
+    private JsonObject createDay() {
+        JsonObject day = new JsonObject();
+        day.addProperty("name", "ONE");
+
+        JsonArray exercisesHistory = new JsonArray();
+        JsonObject historyBenchPress = new JsonObject();
+        historyBenchPress.addProperty("sets", 3);
+        historyBenchPress.addProperty("reps", 12);
+        historyBenchPress.addProperty("weight", 30);
+        historyBenchPress.addProperty("calories", 200);
+        historyBenchPress.addProperty("name", "Bench_Press");
+
+        JsonObject historySquat = new JsonObject();
+        historySquat.addProperty("sets", 3);
+        historySquat.addProperty("reps", 12);
+        historySquat.addProperty("weight", 50);
+        historySquat.addProperty("calories", 200);
+        historySquat.addProperty("name", "Squat");
+
+        exercisesHistory.add(historyBenchPress);
+        exercisesHistory.add(historySquat);
+        day.add("exercises", exercisesHistory);
+
+        return day;
+    }
+
+    private JsonObject createMeals() {
+        JsonObject mealList = new JsonObject();
+        JsonArray meals = new JsonArray();
+        JsonObject pasta = new JsonObject();
+        pasta.addProperty("calories", 560);
+        pasta.addProperty("name", "pasta");
+        meals.add(pasta);
+        mealList.add("meals", meals);
+        return mealList;
+    }
+
+    private JsonObject createWater() {
+        JsonObject water = new JsonObject();
+        JsonArray waterList = new JsonArray();
+        waterList.add(300.0);
+        water.add("waterList", waterList);
+        return water;
+    }
+
+    private JsonObject createDayOne() {
+        JsonObject dayOne = new JsonObject();
+        dayOne.addProperty("name", "ONE");
+
+        JsonArray exercisesOne = new JsonArray();
+        JsonObject benchPress = new JsonObject();
+        benchPress.addProperty("sets", 3);
+        benchPress.addProperty("reps", 12);
+        benchPress.addProperty("weight", 30);
+        benchPress.addProperty("calories", 200);
+        benchPress.addProperty("name", "Bench_Press");
+
+        JsonObject squat = new JsonObject();
+        squat.addProperty("sets", 3);
+        squat.addProperty("reps", 12);
+        squat.addProperty("weight", 50);
+        squat.addProperty("calories", 200);
+        squat.addProperty("name", "Squat");
+
+        exercisesOne.add(benchPress);
+        exercisesOne.add(squat);
+        dayOne.add("exercises", exercisesOne);
+
+        return dayOne;
+    }
+
+    private JsonObject createDayTwo() {
+        JsonObject dayTwo = new JsonObject();
+        dayTwo.addProperty("name", "TWO");
+
+        JsonArray exercisesTwo = new JsonArray();
+        JsonObject bicepCurl = new JsonObject();
+        bicepCurl.addProperty("sets", 3);
+        bicepCurl.addProperty("reps", 12);
+        bicepCurl.addProperty("weight", 10);
+        bicepCurl.addProperty("calories", 100);
+        bicepCurl.addProperty("name", "Bicep_Curl");
+
+        exercisesTwo.add(bicepCurl);
+        dayTwo.add("exercises", exercisesTwo);
+        return dayTwo;
     }
 }
