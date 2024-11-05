8e53e66d42b4ffb937b93db7ae8cb712026b7927
glenn-chew <glennchew15@gmail.com>
2024-11-06 02:04:46 +0800

    Edit method names for filtering by name

--- src/main/java/seedu/manager/command/FilterCommand.java
+++ src/main/java/seedu/manager/command/FilterCommand.java
@@ -15,9 +15,11 @@ import java.time.format.DateTimeFormatter;
  * The filter command will filter out and display all events with the specified details
  */
 public class FilterCommand extends Command {
+    private EventList filteredEvents;
+
     public static final String COMMAND_WORD = "filter";
 
-    private static final String FILTER_BY_KEYWORD_MESSAGE = "Events successfully filtered by name!";
+    private static final String FILTER_BY_NAME_MESSAGE = "Events successfully filtered by name!";
     private static final String FILTER_BY_DATE_MESSAGE = "Events successfully filtered by date!";
     private static final String FILTER_BY_TIME_MESSAGE = "Events successfully filtered by time!";
     private static final String FILTER_BY_DATE_TIME_MESSAGE = "Events successfully filtered by date-time!";
@@ -58,48 +60,23 @@ public class FilterCommand extends Command {
     @Override
     public void execute() {
         StringBuilder outputMessage = new StringBuilder();
-        EventList filteredEvents = new EventList();
+        filteredEvents = new EventList();
 
         switch (flag) {
         case "-e":
-            String lowerCaseKeyword = filterWord.toLowerCase();
-            filteredEvents = eventList.filterByKeyword(lowerCaseKeyword);
-            outputMessage.append(FILTER_BY_KEYWORD_MESSAGE + "\n");
+            outputMessage.append(filterEventsByName());
             break;
         case "-d":
-            try {
-                LocalDate eventDate = LocalDate.parse(filterWord,
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-                filteredEvents = eventList.filterByDate(eventDate);
-                outputMessage.append(FILTER_BY_DATE_MESSAGE + "\n");
-            } catch (DateTimeException exception) {
-                outputMessage.append(INVALID_DATE_FORMAT_MESSAGE);
-            }
+            outputMessage.append(filterEventsByDate());
             break;
         case "-t":
-            try {
-                LocalTime eventTime = LocalTime.parse(filterWord,
-                        DateTimeFormatter.ofPattern("HH:mm"));
-                filteredEvents = eventList.filterByTime(eventTime);
-                outputMessage.append(FILTER_BY_TIME_MESSAGE + "\n");
-            } catch (DateTimeException exception) {
-                outputMessage.append(INVALID_TIME_FORMAT_MESSAGE);
-            }
+            outputMessage.append(filterEventsByTime());
             break;
         case "-x":
-            try {
-                LocalDateTime eventDateTime = LocalDateTime.parse(filterWord,
-                        DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-                filteredEvents = eventList.filterByDateTime(eventDateTime);
-                outputMessage.append(FILTER_BY_DATE_TIME_MESSAGE + "\n");
-            } catch (DateTimeException exception) {
-                outputMessage.append(INVALID_DATE_TIME_FORMAT_MESSAGE);
-            }
+            outputMessage.append(filterEventsByDateTime());
             break;
         case "-u":
-            Priority priority = Priority.valueOf(filterWord.trim().toUpperCase());
-            filteredEvents = eventList.filterByPriority(priority);
-            outputMessage.append(FILTER_BY_PRIORITY_MESSAGE + "\n");
+            outputMessage.append(filterEventsByPriority());
             break;
         default:
             break;
@@ -109,5 +86,75 @@ public class FilterCommand extends Command {
             outputMessage.append(String.format("%d. %s\n", i + 1, filteredEvents.getEvent(i).toString()));
         }
         this.message = outputMessage.toString();
+    }
+
+    /**
+     * Filters the even list by name and updates {@code filteredEvents} with the filtered list.
+     *
+     * @return successful message for filter by name.
+     */
+    private String filterEventsByName() {
+        String lowerCaseKeyword = filterWord.toLowerCase();
+        filteredEvents = eventList.filterByName(lowerCaseKeyword);
+        return FILTER_BY_NAME_MESSAGE + "\n";
+    }
+
+    /**
+     * Filters the even list by date and updates {@code filteredEvents} with the filtered list.
+     *
+     * @return successful message for filter by date or an error message if date is invalid.
+     */
+    private String filterEventsByDate() {
+        try {
+            LocalDate eventDate = LocalDate.parse(filterWord,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            filteredEvents = eventList.filterByDate(eventDate);
+            return FILTER_BY_DATE_MESSAGE + "\n";
+        } catch (DateTimeException exception) {
+            return INVALID_DATE_FORMAT_MESSAGE;
+        }
+    }
+
+    /**
+     * Filters the even list by time and updates {@code filteredEvents} with the filtered list.
+     *
+     * @return successful message for filter by time or an error message if time is invalid.
+     */
+    private String filterEventsByTime() {
+        try {
+            LocalTime eventTime = LocalTime.parse(filterWord,
+                    DateTimeFormatter.ofPattern("HH:mm"));
+            filteredEvents = eventList.filterByTime(eventTime);
+            return FILTER_BY_TIME_MESSAGE + "\n";
+        } catch (DateTimeException exception) {
+            return INVALID_TIME_FORMAT_MESSAGE;
+        }
+    }
+
+    /**
+     * Filters the even list by date-time and updates {@code filteredEvents} with the filtered list.
+     *
+     * @return successful message for filter by date-time or an error message if date is invalid.
+     */
+    private String filterEventsByDateTime() {
+        try {
+            LocalDateTime eventDateTime = LocalDateTime.parse(filterWord,
+                    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+            filteredEvents = eventList.filterByDateTime(eventDateTime);
+            return FILTER_BY_DATE_TIME_MESSAGE + "\n";
+        } catch (DateTimeException exception) {
+            return INVALID_DATE_TIME_FORMAT_MESSAGE;
+        }
+    }
+
+    /**
+     * Filters the even list by priority and updates {@code filteredEvents} with the filtered list.
+     *
+     * @return successful message for filter by priority
+     */
+    private String filterEventsByPriority() {
+        Priority priority = Priority.valueOf(filterWord.trim().toUpperCase());
+        filteredEvents = eventList.filterByPriority(priority);
+        return FILTER_BY_PRIORITY_MESSAGE + "\n";
     }
 }
