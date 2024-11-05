95200b3ec6d16d59c3a2d56486b5a087adfed626
glenda_1506 <glenda.xuanyin@gmail.com>
2024-11-05 21:59:48 +0800

    Add constructor for Parser class

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -1,4 +1,13 @@
 package seedu.duke;
 
 public class Parser {
+    private final ExpenseManager expenseManager;
+    private final CategoryManager categoryManager;
+    private final BudgetManager budgetManager;
+
+    public Parser(ExpenseManager expenseManager, CategoryManager categoryManager, BudgetManager budgetManager) {
+        this.expenseManager = expenseManager;
+        this.categoryManager = categoryManager;
+        this.budgetManager = budgetManager;
+    }
 }
