92fd6fbb6e730334e2cbb2dd315b3a648c96603d
glenda_1506 <glenda.xuanyin@gmail.com>
2024-11-06 19:37:29 +0800

    Add parser for limit

--- src/main/java/seedu/duke/InputParser.java
+++ src/main/java/seedu/duke/InputParser.java
@@ -19,12 +19,22 @@ public class InputParser {
         return componentBuilder.toString().trim();
     }
 
-    public int parseExpenseIndex(String input) {
+    public int parseIndex(String input) {
         String indexStr = parseComponent(input, "e/");
         try {
             return Integer.parseInt(indexStr) - 1; // Convert to 0-based index
         } catch (NumberFormatException e) {
             System.out.println("Invalid expense index format. Please enter a valid number after 'e/'.");
+            return -1;
+        }
+    }
+
+    public double parseLimit(String input) {
+        String limitStr = parseComponent(input, "l/");
+        try {
+            return Double.parseDouble(limitStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid limit format. Please enter a valid number after 'l/'.");
             return -1;
         }
     }
