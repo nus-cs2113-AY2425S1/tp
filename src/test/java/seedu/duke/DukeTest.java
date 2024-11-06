ebe696fb49c95ae94f467280159f3b37e1f8584b
glenda_1506 <glenda.xuanyin@gmail.com>
2024-11-06 10:30:24 +0800

    Remove irrelevant import to resolve checkstyle error

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 class ExpenseTest {
     @Test
