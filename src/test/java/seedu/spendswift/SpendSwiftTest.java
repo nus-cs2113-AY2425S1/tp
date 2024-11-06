e9c8c1f500addef6ec99d7a2f21dad1ee10805b7
MayFairMI6 <143886061+MayFairMI6@users.noreply.github.com>
2024-11-07 03:22:59 +0800

    Update SpendSwiftTest.java
    before each import corrected

--- src/test/java/seedu/spendswift/SpendSwiftTest.java
+++ src/test/java/seedu/spendswift/SpendSwiftTest.java
@@ -10,7 +10,7 @@ import seedu.spendswift.command.Category;
 import seedu.spendswift.command.Expense;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.BeforeEach;
+import  org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
