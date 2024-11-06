942089244c054ab2555b68fbb300cd5b0692973f
BevLow <lowbeverlypersonal@gmail.com>
2024-11-06 18:26:00 +0800

    Add Flag Exceptions

---
+++ src/main/java/exceptions/FlagExceptions.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class FlagExceptions extends BuffBuddyExceptions {
+    public FlagExceptions(String message) {
+        super(message);
+    }
+
+    public static FlagExceptions missingFlag(String flag) {
+        return new FlagExceptions("Flag " + flag + " not present");
+    }
+
+    public static FlagExceptions missingArguments() {
+        return new FlagExceptions("Missing arugments after flag, please refer to User Guide");
+    }
+}
