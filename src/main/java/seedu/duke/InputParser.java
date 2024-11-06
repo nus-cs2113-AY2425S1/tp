235337c779a5aa94b21c7ffd1f8a733474dbe1f6
glenda_1506 <glenda.xuanyin@gmail.com>
2024-11-06 19:34:52 +0800

    Add parser for each string variable input

--- src/main/java/seedu/duke/InputParser.java
+++ src/main/java/seedu/duke/InputParser.java
@@ -1,4 +1,21 @@
 package seedu.duke;
 
 public class InputParser {
+    private String parseComponent(String input, String prefix) {
+        String[] parts = input.split(" ");
+        StringBuilder componentBuilder = new StringBuilder();
+        boolean isComponentStarted = false;
+
+        for (String part : parts) {
+            if (part.startsWith(prefix)) {
+                isComponentStarted = true;
+                componentBuilder.append(part.substring(prefix.length())).append(" ");
+            } else if (isComponentStarted && !part.contains("/")) {
+                componentBuilder.append(part).append(" ");
+            } else if (isComponentStarted) {
+                break;
+            }
+        }
+        return componentBuilder.toString().trim();
+    }
 }
