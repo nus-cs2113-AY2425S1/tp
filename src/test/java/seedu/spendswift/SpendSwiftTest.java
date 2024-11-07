1f738a37f17d417aedc98af6841e3a4f14c83d6c
MayFairMI6 <143886061+MayFairMI6@users.noreply.github.com>
2024-11-07 18:00:56 +0800

    Update SpendSwiftTest.java
    missing semicolon added

--- src/test/java/seedu/spendswift/SpendSwiftTest.java
+++ src/test/java/seedu/spendswift/SpendSwiftTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 
 class ExpenseTest {
