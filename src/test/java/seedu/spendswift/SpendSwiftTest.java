950f4087cb3efd15bc658775e8d0fea1f827ebea
MayFairMI6 <143886061+MayFairMI6@users.noreply.github.com>
2024-11-07 15:28:45 +0800

    checkstyle corrections

--- src/test/java/seedu/spendswift/SpendSwiftTest.java
+++ src/test/java/seedu/spendswift/SpendSwiftTest.java
@@ -323,8 +323,8 @@ class ExpenseManagerTest {
 class BudgetManagerTest {
 
     
-   BudgetManager budgetManager = new BudgetManager();
-TrackerData trackerData = new TrackerData();
+    BudgetManager budgetManager = new BudgetManager();
+    TrackerData trackerData = new TrackerData();
   
 
     private Category findCategory(TrackerData trackerData, String categoryName) {
