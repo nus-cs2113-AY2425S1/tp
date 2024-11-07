46de3ad46c5a9c6e2b522f155dee3b2d699186c0
glenda_1506 <glenda.xuanyin@gmail.com>
2024-11-07 15:07:50 +0800

    Add help function

--- src/main/java/seedu/spendswift/UI.java
+++ src/main/java/seedu/spendswift/UI.java
@@ -11,6 +11,16 @@ public class UI {
     public static final String ERROR_LOADING_DATA_MESSAGE = "Error loading data: ";
     public static final String ERROR_SAVING_DATA_MESSAGE = "Error saving data: ";
     public static final String INVALID_INPUT_MESSAGE = "Please enter a valid command!";
+    public static final String HELP_INTRO_MESSAGE = "Here are the commands available:";
+    public static final String HELP_ADD_EXPENSE = "Add expense: add-expense n/NAME a/AMOUNT [c/CATEGORY]";
+    public static final String HELP_DELETE_EXPENSE = "Delete expense: delete-expense e/INDEX";
+    public static final String HELP_ADD_CATEGORY = "Add new category: add-category CATEGORY";
+    public static final String HELP_TAG_EXPENSE = "Tag an expense to a category: tag-expense e/INDEX c/CATEGORY";
+    public static final String HELP_VIEW_EXPENSES = "View all recorded expenses: view-expenses";
+    public static final String HELP_SET_BUDGET = "Set a budget limit for a category: set-budget c/CATEGORY l/LIMIT";
+    public static final String HELP_VIEW_BUDGET = "View current budget status for each category: view-budget";
+    public static final String HELP_TOGGLE_RESET = "On/Off automatic budget reset: toggle-reset";
+    public static final String HELP_EXIT = "Exit: bye";
 
     public void printWelcomeMessage() {
         System.out.println(WELCOME_MESSAGE);
@@ -42,5 +52,18 @@ public class UI {
 
     public void printFileNotFound() {
         System.out.println(FILE_NOT_FOUND);
+    }
+
+    public void printHelpMessage() {
+        System.out.println(HELP_INTRO_MESSAGE);
+        System.out.println(HELP_ADD_EXPENSE);
+        System.out.println(HELP_DELETE_EXPENSE);
+        System.out.println(HELP_ADD_CATEGORY);
+        System.out.println(HELP_TAG_EXPENSE);
+        System.out.println(HELP_VIEW_EXPENSES);
+        System.out.println(HELP_SET_BUDGET);
+        System.out.println(HELP_VIEW_BUDGET);
+        System.out.println(HELP_TOGGLE_RESET);
+        System.out.println(HELP_EXIT);
     }
 }
