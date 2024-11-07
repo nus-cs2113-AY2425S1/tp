2c0280389dfd4cd03aabad7e29a13b19042a93d9
gskang <windstick26@gmail.com>
2024-11-07 10:58:29 +0800

    Add test for Ui PrintErrorMessage() method

--- src/test/java/ymfc/ui/UiTest.java
+++ src/test/java/ymfc/ui/UiTest.java
@@ -2,8 +2,11 @@
 package ymfc.ui;
 
 import org.junit.jupiter.api.Test;
+import ymfc.exception.InvalidArgumentException;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,5 +19,24 @@ class UiTest {
         ByteArrayInputStream testInput = new ByteArrayInputStream(testString.getBytes());
         Ui ui = new Ui(testInput);
         assertEquals(testString, ui.readCommand());
+    }
+
+    @Test
+    void testPrintErrorMessage() {
+        String testString = "Test String";
+
+        // Capture System.out printing
+        ByteArrayOutputStream message = new ByteArrayOutputStream();
+        PrintStream testingStream = new PrintStream(message);
+        PrintStream systemStream = System.out;
+        System.setOut(testingStream);
+
+        Ui ui = new Ui(System.in);
+        ui.printErrorMessage(testString);
+
+        System.out.flush();
+        System.setOut(systemStream);
+
+        assertEquals(testString + System.lineSeparator(), message.toString());
     }
 }
