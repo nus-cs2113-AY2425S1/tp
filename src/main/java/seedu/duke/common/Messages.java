2e7803623dd641d71b8bd12cc28026ecd7010d78
Ajay Shanker <26241915+AjayShanker-geek@users.noreply.github.com>
2024-11-09 22:12:43 +0800

    Fix Flags from Check-style

--- src/main/java/seedu/duke/common/Messages.java
+++ src/main/java/seedu/duke/common/Messages.java
@@ -30,110 +30,125 @@ public class Messages {
     public static final String MESSAGE_WELCOME = "Welcome to " + APPLICATION_NAME + "!";
     public static final String MESSAGE_GOODBYE = "Goodbye! Hope to see you again soon!";
 
-public static final String MESSAGE_HELP_PATIENT = Colors.ANSI_BLUE + """
-    Here are the list of commands you can use in patient mode:
-    """ + Colors.ANSI_RESET + Colors.ANSI_YELLOW + """
+    public static final String MESSAGE_HELP_PATIENT = Colors.ANSI_BLUE + """
+            Here are the list of commands you can use in patient mode:
+            """ + Colors.ANSI_RESET + Colors.ANSI_YELLOW + """
 
-    COMMANDS:
-    """ + Colors.ANSI_RESET +
-    " 1. " + Colors.ANSI_RED + "add" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Add a patient to the system" + Colors.ANSI_RESET + "\n" +
+            COMMANDS:
+            """ + Colors.ANSI_RESET +
+            " 1. " + Colors.ANSI_RED + "add" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE
+            + "Add a patient to the system" + Colors.ANSI_RESET + "\n" +
             """
-                            Format : add NAME /tag TAG_NAME
-                            Example: add Alice /tag HighPriority
-                            Info   : /tag is optional
-            """ +
-    " 2. " + Colors.ANSI_RED + "delete" + Colors.ANSI_RESET + "  - " + Colors.ANSI_BLUE + "Delete a patient from the system" + Colors.ANSI_RESET + "\n" +
+                                    Format : add NAME /tag TAG_NAME
+                                    Example: add Alice /tag HighPriority
+                                    Info   : /tag is optional
+                    """ +
+            " 2. " + Colors.ANSI_RED + "delete" + Colors.ANSI_RESET + "  - " + Colors.ANSI_BLUE
+            + "Delete a patient from the system" + Colors.ANSI_RESET + "\n" +
             """
-                            Format : delete INDEX
-                            Example: delete 1
-                            Info   : INDEX is the index of the patient in the list
-            """ +
-     " 3. " + Colors.ANSI_RED + "list" + Colors.ANSI_RESET + "    - " + Colors.ANSI_BLUE + "List all patients in the system" + Colors.ANSI_RESET + "\n" +
-            """
-                            Example: list
-            """ +
-    " 4. " + Colors.ANSI_RED + "select" + Colors.ANSI_RESET + "  - " + Colors.ANSI_BLUE + "Select a patient in the system" + Colors.ANSI_RESET + "\n" +
-            """
-                            Format : select INDEX
-                            Example: select 1
-                            Info   : INDEX is the index of the patient in the list
-            """ +
-    " 5. " + Colors.ANSI_RED + "find" + Colors.ANSI_RESET + "    - " + Colors.ANSI_BLUE + "Find a patient in the system" + Colors.ANSI_RESET + "\n" +
-            """
-                            Format : find KEYWORD
-                            Example: find Alice
-                            Info   : KEYWORD is the name of the patient to search for
-            """ +
-    " 6. " + Colors.ANSI_RED + "help" + Colors.ANSI_RESET + "    - " + Colors.ANSI_BLUE + "Show help message" + Colors.ANSI_RESET + "\n" +
-            """
-                            Example: help
-            """ +
-    " 7. " + Colors.ANSI_RED + "exit" + Colors.ANSI_RESET + "    - " + Colors.ANSI_BLUE + "Show help message" + Colors.ANSI_RESET + "\n" +
-            """
-                            Example: exit
-            """;
-
-    public static final String MESSAGE_HELP_TASK = Colors.ANSI_BLUE + """
-        Here are the list of commands you can use in task mode:
-        """ + Colors.ANSI_RESET + Colors.ANSI_YELLOW + """
-
-        COMMANDS:
-        """ + Colors.ANSI_RESET +
-        " 1. " + Colors.ANSI_RED + "todo" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Adds a new todo task to the list of tasks" + Colors.ANSI_RESET + "\n" +
-        """
-                            Format : todo TODO_NAME /tag TAG_NAME
-                            Example: todo Check up /tag urgent
-                            Info   : /tag is optional
-        """ +
-        " 2. " + Colors.ANSI_RED + "deadline" + Colors.ANSI_RESET + " - " + Colors.ANSI_BLUE + "Adds a task with deadline to the list of tasks" + Colors.ANSI_RESET + "\n" +
-        """
-                            Format : deadline DEADLINE_NAME /by DATE_TIME /tag TAG_NAME
-                            Example: deadline Have medicine /by 19:00 /tag urgent
-                            Info   : /by is needed, /tag is optional
-        """ +
-        " 2. " + Colors.ANSI_RED + "repeat" + Colors.ANSI_RESET + "   - " + Colors.ANSI_BLUE + "Adds a task with reminder to repeat it to the list of tasks." + Colors.ANSI_RESET + "\n" +
-        """
-                            Format : repeat TODO_NAME /every RECUR_BASIS /tag TAG_NAME
-                            Example: repeat Drink supplements /every day /tag important
-                            Info   : /every is needed, /tag is optional
-        """ +
-        " 2. " + Colors.ANSI_RED + "delete" + Colors.ANSI_RESET + "   - " + Colors.ANSI_BLUE + "Delete a task from the system" + Colors.ANSI_RESET + "\n" +
-                """
                                     Format : delete INDEX
                                     Example: delete 1
-                                    Info   : INDEX is the index of the task in the list
-                """ +
-         " 3. " + Colors.ANSI_RED + "list" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "List all tasks in the system" + Colors.ANSI_RESET + "\n" +
-                """
+                                    Info   : INDEX is the index of the patient in the list
+                    """ +
+            " 3. " + Colors.ANSI_RED + "list" + Colors.ANSI_RESET + "    - " + Colors.ANSI_BLUE
+            + "List all patients in the system" + Colors.ANSI_RESET + "\n" +
+            """
                                     Example: list
-                """ +
-        " 4. " + Colors.ANSI_RED + "find" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Find a task in the system" + Colors.ANSI_RESET + "\n" +
-                """
+                    """ +
+            " 4. " + Colors.ANSI_RED + "select" + Colors.ANSI_RESET + "  - " + Colors.ANSI_BLUE
+            + "Select a patient in the system" + Colors.ANSI_RESET + "\n" +
+            """
+                                    Format : select INDEX
+                                    Example: select 1
+                                    Info   : INDEX is the index of the patient in the list
+                    """ +
+            " 5. " + Colors.ANSI_RED + "find" + Colors.ANSI_RESET + "    - " + Colors.ANSI_BLUE
+            + "Find a patient in the system" + Colors.ANSI_RESET + "\n" +
+            """
                                     Format : find KEYWORD
-                                    Example: find supplements
-                                    Info   : KEYWORD is the description of the task to search for
-                """ +
-        " 5. " + Colors.ANSI_RED + "mark" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Mark a task as done" + Colors.ANSI_RESET + "\n" +
-                """
-                                    Format : mark INDEX
-                                    Example: mark 1
-                                    Info   : INDEX is the index of the task in the list
-                """ +
-        " 6. " + Colors.ANSI_RED + "unmark" + Colors.ANSI_RESET + "   - " + Colors.ANSI_BLUE + "Mark a task as undone" + Colors.ANSI_RESET + "\n" +
-                """
-                                    Format : unmark INDEX
-                                    Example: unmark 1
-                                    Info   : INDEX is the index of the task in the list
-                """ +
-        " 7. " + Colors.ANSI_RED + "help" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Show help message" + Colors.ANSI_RESET + "\n" +
-                """
+                                    Example: find Alice
+                                    Info   : KEYWORD is the name of the patient to search for
+                    """ +
+            " 6. " + Colors.ANSI_RED + "help" + Colors.ANSI_RESET + "    - " + Colors.ANSI_BLUE + "Show help message"
+            + Colors.ANSI_RESET + "\n" +
+            """
                                     Example: help
-                """ +
-        " 8. " + Colors.ANSI_RED + "exit" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Exit the program" + Colors.ANSI_RESET + "\n" +
-                """
+                    """ +
+            " 7. " + Colors.ANSI_RED + "exit" + Colors.ANSI_RESET + "    - " + Colors.ANSI_BLUE + "Show help message"
+            + Colors.ANSI_RESET + "\n" +
+            """
                                     Example: exit
-                """
-        ;
+                    """;
 
+    public static final String MESSAGE_HELP_TASK = Colors.ANSI_BLUE + """
+            Here are the list of commands you can use in task mode:
+            """ + Colors.ANSI_RESET + Colors.ANSI_YELLOW + """
+
+            COMMANDS:
+            """ + Colors.ANSI_RESET +
+            " 1. " + Colors.ANSI_RED + "todo" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE
+            + "Adds a new todo task to the list of tasks" + Colors.ANSI_RESET + "\n" +
+            """
+                                        Format : todo TODO_NAME /tag TAG_NAME
+                                        Example: todo Check up /tag urgent
+                                        Info   : /tag is optional
+                    """ +
+            " 2. " + Colors.ANSI_RED + "deadline" + Colors.ANSI_RESET + " - " + Colors.ANSI_BLUE
+            + "Adds a task with deadline to the list of tasks" + Colors.ANSI_RESET + "\n" +
+            """
+                                        Format : deadline DEADLINE_NAME /by DATE_TIME /tag TAG_NAME
+                                        Example: deadline Have medicine /by 19:00 /tag urgent
+                                        Info   : /by is needed, /tag is optional
+                    """ +
+            " 2. " + Colors.ANSI_RED + "repeat" + Colors.ANSI_RESET + "   - " + Colors.ANSI_BLUE
+            + "Adds a task with reminder to repeat it to the list of tasks." + Colors.ANSI_RESET + "\n" +
+            """
+                                        Format : repeat TODO_NAME /every RECUR_BASIS /tag TAG_NAME
+                                        Example: repeat Drink supplements /every day /tag important
+                                        Info   : /every is needed, /tag is optional
+                    """ +
+            " 2. " + Colors.ANSI_RED + "delete" + Colors.ANSI_RESET + "   - " + Colors.ANSI_BLUE
+            + "Delete a task from the system" + Colors.ANSI_RESET + "\n" +
+            """
+                                        Format : delete INDEX
+                                        Example: delete 1
+                                        Info   : INDEX is the index of the task in the list
+                    """ +
+            " 3. " + Colors.ANSI_RED + "list" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE
+            + "List all tasks in the system" + Colors.ANSI_RESET + "\n" +
+            """
+                                        Example: list
+                    """ +
+            " 4. " + Colors.ANSI_RED + "find" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE
+            + "Find a task in the system" + Colors.ANSI_RESET + "\n" +
+            """
+                                        Format : find KEYWORD
+                                        Example: find supplements
+                                        Info   : KEYWORD is the description of the task to search for
+                    """ +
+            " 5. " + Colors.ANSI_RED + "mark" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Mark a task as done"
+            + Colors.ANSI_RESET + "\n" +
+            """
+                                        Format : mark INDEX
+                                        Example: mark 1
+                                        Info   : INDEX is the index of the task in the list
+                    """ +
+            " 6. " + Colors.ANSI_RED + "unmark" + Colors.ANSI_RESET + "   - " + Colors.ANSI_BLUE
+            + "Mark a task as undone" + Colors.ANSI_RESET + "\n" +
+            """
+                                        Format : unmark INDEX
+                                        Example: unmark 1
+                                        Info   : INDEX is the index of the task in the list
+                    """ +
+            " 7. " + Colors.ANSI_RED + "help" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Show help message"
+            + Colors.ANSI_RESET + "\n" +
+            """
+                                        Example: help
+                    """ +
+            " 8. " + Colors.ANSI_RED + "exit" + Colors.ANSI_RESET + "     - " + Colors.ANSI_BLUE + "Exit the program"
+            + Colors.ANSI_RESET + "\n" +
+            """
+                                        Example: exit
+                    """;
 
 }
