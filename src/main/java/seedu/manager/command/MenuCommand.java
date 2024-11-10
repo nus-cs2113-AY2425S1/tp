a7f5af562d49541d9900582f616391f455fa4d9e
jemehgoh <jeremygoheh@gmail.com>
2024-11-10 19:54:00 +0800

    Update MenuCommand output in UG to match program output

--- src/main/java/seedu/manager/command/MenuCommand.java
+++ src/main/java/seedu/manager/command/MenuCommand.java
@@ -17,9 +17,9 @@ public class MenuCommand extends Command {
             remove -e EVENT: Removes an event from the event list.
             remove -p PARTICIPANT -e EVENT: Removes a participant from an event.
             remove -m ITEM -e EVENT: Removes an item from an event.
-            edit -e EVENT -name EVENT_NAME -t TIME -v VENUE -u PRIORITY: Edits an event's info.
+            edit -e OLD_EVENT -name NEW_EVENT -t TIME -v VENUE -u PRIORITY: Edits an event's info.
             edit -p OLD_PARTICIPANT -name NEW_PARTICIPANT -email EMAIL -e EVENT: Edits a participant's info.
-            edit -m ITEM > NEW_ITEM -e EVENT: Edits an item's info.
+            edit -m OLD_ITEM > NEW_ITEM -e EVENT: Edits an item's info.
             view -e EVENT -y TYPE: Displays the list of participants or items of an event.
             mark -e EVENT -s STATUS: Marks an event as done or not done.
             mark -p PARTICIPANT -e EVENT -s STATUS: Marks a participant as present or absent.
