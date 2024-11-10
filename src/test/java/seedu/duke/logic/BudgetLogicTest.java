799f5a800ea4b5b3efa811ae4b7c3c8d09a16b6d
ctzeyong <80164379+ctzeyong@users.noreply.github.com>
2024-11-10 19:25:50 +0800

    Minor checkstyle edit

--- src/test/java/seedu/duke/logic/BudgetLogicTest.java
+++ src/test/java/seedu/duke/logic/BudgetLogicTest.java
@@ -16,7 +16,10 @@ import java.io.PrintStream;
 import java.time.LocalDate;
 import java.time.Month;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Tests for the {@link BudgetLogic} class.
