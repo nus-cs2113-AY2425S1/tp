12fe89f7bcee00e53e488614651145f911f40a72
AndrewNguyen4 <nguyenminhduc5etp@gmail.com>
2024-11-10 17:17:07 +0800

    Fix CI

--- src/test/java/wheresmymoney/ExpenseFilterTest.java
+++ src/test/java/wheresmymoney/ExpenseFilterTest.java
@@ -40,7 +40,7 @@ public class ExpenseFilterTest {
     public void listByFilter_categoryOnly_returnNone() {
         ExpenseList expenseList = initExpenseList();
         ArrayList<Expense> filteredExpenseList = expenseList.listByFilter("abc category", null, null);
-        assertNull(filteredExpenseList);
+        assert(filteredExpenseList.isEmpty());
     }
 
     @Test
@@ -80,9 +80,9 @@ public class ExpenseFilterTest {
         ExpenseList expenseList = initExpenseList();
         ArrayList<Expense> filteredExpenseList = expenseList.listByFilter(null, "04-04-2021", "05-05-2022");
         assert (filteredExpenseList.size() == 3);
-        assert (filteredExpenseList.get(0).equals(expenseList.getExpenseAtIndex(0)));
-        assert (filteredExpenseList.get(1).equals(expenseList.getExpenseAtIndex(1)));
-        assert (filteredExpenseList.get(2).equals(expenseList.getExpenseAtIndex(3)));
+        assert (filteredExpenseList.get(0).equals(expenseList.getExpenseAtIndex(1)));
+        assert (filteredExpenseList.get(1).equals(expenseList.getExpenseAtIndex(3)));
+        assert (filteredExpenseList.get(2).equals(expenseList.getExpenseAtIndex(4)));
     }
 
     @Test
