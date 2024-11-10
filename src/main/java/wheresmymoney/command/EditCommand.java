286baf7272e187a66a0d70f5670e5ef29663f15f
Progresst-8 <142147661+Progresst-8@users.noreply.github.com>
2024-11-10 14:19:25 +0800

    Restructure EditCommand.execute() for clarity

--- src/main/java/wheresmymoney/command/EditCommand.java
+++ src/main/java/wheresmymoney/command/EditCommand.java
@@ -26,45 +26,52 @@ public class EditCommand extends Command {
             RecurringExpenseList recurringExpenseList) throws WheresMyMoneyException {
         try {
             int index = Integer.parseInt(argumentsMap.get(Parser.ARGUMENT_MAIN)) - 1;
-            String oldCategory;
-            float oldPrice;
-            if (!this.isRecur()) {
-                oldCategory = expenseList.getExpenseAtIndex(index).getCategory();
-                oldPrice = expenseList.getExpenseAtIndex(index).getPrice();
-            } else {
-                oldCategory = recurringExpenseList.getRecurringExpenseAtIndex(index).getCategory();
-                oldPrice = recurringExpenseList.getRecurringExpenseAtIndex(index).getPrice();
-            }
-            
-            String newCategory = argumentsMap.get(Parser.ARGUMENT_CATEGORY);
-            float newPrice;
-            if (argumentsMap.containsKey(Parser.ARGUMENT_PRICE)) {
-                newPrice = Float.parseFloat(argumentsMap.get(Parser.ARGUMENT_PRICE));
-            } else {
-                newPrice = oldPrice;
-            }
-            if (newCategory == null) {
-                newCategory = oldCategory;
-            }
-            if (argumentsMap.containsKey(Parser.ARGUMENT_PRICE)) {
-                oldPrice = Float.parseFloat(argumentsMap.get(Parser.ARGUMENT_PRICE));
-                if (oldPrice <= 0) {
-                    throw new InvalidInputException("Price cannot be less than or equal to 0.");
-                }
-            }
-            
-            String description = argumentsMap.get(Parser.ARGUMENT_DESCRIPTION);
-            String dateAdded = argumentsMap.get(Parser.ARGUMENT_DATE);
             if (this.isRecur()) {
+                String oldCategory = recurringExpenseList.getRecurringExpenseAtIndex(index).getCategory();
+                Float oldPrice = recurringExpenseList.getRecurringExpenseAtIndex(index).getPrice();
+                String newCategory = argumentsMap.get(Parser.ARGUMENT_CATEGORY);
+                
+                String description = argumentsMap.get(Parser.ARGUMENT_DESCRIPTION);
+                String dateAdded = argumentsMap.get(Parser.ARGUMENT_DATE);
                 String frequency = argumentsMap.get(Parser.ARGUMENT_FREQUENCY);
+                
                 recurringExpenseList.editRecurringExpense(
                         index, oldPrice, description, newCategory, dateAdded, frequency);
             } else {
-                expenseList.editExpense(index, oldPrice, description, newCategory, dateAdded);
-                categoryFacade.editCategory(oldCategory, newCategory, oldPrice, newPrice);
+                String oldCategory = expenseList.getExpenseAtIndex(index).getCategory();
+                Float oldPrice = expenseList.getExpenseAtIndex(index).getPrice();
+                String newCategory = getNewCategory(oldCategory);
+                Float newPrice = getNewPrice(oldPrice);
+                
+                String description = argumentsMap.get(Parser.ARGUMENT_DESCRIPTION);
+                String dateAdded = argumentsMap.get(Parser.ARGUMENT_DATE);
+            
+                expenseList.editExpense(
+                        index, oldPrice, description, newCategory, dateAdded);
+                categoryFacade.editCategory(
+                        oldCategory, newCategory, oldPrice, newPrice);
             }
         } catch (NullPointerException | NumberFormatException e) {
-            throw new InvalidInputException("Invalid Arguments.");
+            throw new InvalidInputException("Invalid Arguments." + e.getMessage());
+        }
+    }
+    
+    private String getNewCategory(String oldCategory) {
+        if (argumentsMap.containsKey(Parser.ARGUMENT_CATEGORY)) {
+            return argumentsMap.get(Parser.ARGUMENT_PRICE);
+        } else {
+            return oldCategory;
+        }
+    }
+    private Float getNewPrice(Float oldPrice) throws InvalidInputException {
+        if (argumentsMap.containsKey(Parser.ARGUMENT_PRICE)) {
+            Float newPrice = Float.parseFloat(argumentsMap.get(Parser.ARGUMENT_PRICE));
+            if (newPrice <= 0) {
+                throw new InvalidInputException("Price cannot be less than or equal to 0.");
+            }
+            return newPrice;
+        } else {
+            return oldPrice;
         }
     }
     
