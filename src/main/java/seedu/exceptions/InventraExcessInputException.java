ca92df9d689c72dc0d06db9e8b9a52c5165dd8b0
lucas-sc0 <e0959193@u.nus.edu>
2024-11-11 20:52:39 +0800

    FIx checkstyle errors

--- src/main/java/seedu/exceptions/InventraExcessInputException.java
+++ src/main/java/seedu/exceptions/InventraExcessInputException.java
@@ -1,16 +1,17 @@
 package seedu.exceptions;
 
 public class InventraExcessInputException extends InventraException {
-  private final int expected;
-  private final int actual;
+    private final int expected;
+    private final int actual;
 
   public InventraExcessInputException(int expected, int actual) {
-    this.expected = expected;
-    this.actual = actual;
+      this.expected = expected;
+      this.actual = actual;
   }
 
   @Override
   public String getMessage() {
-    return String.format("Input length exceeds the expected number. Expected less than %d, but received: %d.", expected, actual);
+      return String.format("Input length exceeds the expected number. " +
+              "Expected less than %d, but received: %d.", expected, actual);
   }
 }
