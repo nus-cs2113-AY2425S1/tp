0fe53bc8b3f22eb76d1037109bfa29330b7fcd42
wahkia <gohah021@gmail.com>
2024-11-11 17:32:21 +0800

    Fix Checkstyle prroblems

--- src/test/java/seedu/exchangecoursemapper/DataIntegrityCheckerTest.java
+++ src/test/java/seedu/exchangecoursemapper/DataIntegrityCheckerTest.java
@@ -13,7 +13,9 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DataIntegrityCheckerTest {
 
