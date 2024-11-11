91fc8b18dc2a78981cdea966b4f354a39820b055
glenda_1506 <glenda.xuanyin@gmail.com>
2024-11-11 09:39:09 +0800

    Comment out unused library import to resolve checkstyle error

--- src/test/java/seedu/spendswift/SpendSwiftTest.java
+++ src/test/java/seedu/spendswift/SpendSwiftTest.java
@@ -13,7 +13,7 @@ import seedu.spendswift.command.BudgetManager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
