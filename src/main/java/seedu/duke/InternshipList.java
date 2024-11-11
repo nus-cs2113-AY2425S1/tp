775e010f4c6e21590c6f5333f9d644e6cd399521
Toby-Yu <e1344677@u.nus.edu>
2024-11-11 22:16:07 +0800

    add the respo code comment

--- src/main/java/seedu/duke/InternshipList.java
+++ src/main/java/seedu/duke/InternshipList.java
@@ -250,6 +250,7 @@ public class InternshipList {
         });
     }
 
+    //@@author Toby-Yu
     /**
      * Lists internships sorted by the first skill alphabetically (case-insensitive).
      */
