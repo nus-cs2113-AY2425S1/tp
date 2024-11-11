0407a5fa64a4cd6b1010fce34852e64e2807f4a4
Ayushi <e0905342@u.nus.edu>
2024-11-11 12:44:05 +0800

    health profile comments

--- src/main/java/fittrack/healthprofile/FoodWaterIntake.java
+++ src/main/java/fittrack/healthprofile/FoodWaterIntake.java
@@ -5,15 +5,30 @@ import java.util.ArrayList;
 
 import static fittrack.messages.Messages.SEPARATOR;
 
+
+/**
+ * Manages daily food and water intake by storing food and water entries and
+ * providing methods to add, delete, and list daily intake records.
+ */
 public class FoodWaterIntake {
 
     private ArrayList<FoodEntry> foodList = new ArrayList<>();
     private ArrayList<WaterEntry> waterList = new ArrayList<>();
 
+    /**
+     * Adds a food entry to the food list.
+     *
+     * @param food The food entry to be added.
+     */
     public void addFood(FoodEntry food) {
         foodList.add(food);
     }
 
+    /**
+     * Deletes a food entry from the food list by index.
+     *
+     * @param foodIndex The index of the food entry to delete.
+     */
     public void deleteFood(int foodIndex) {
 
         if (foodIndex >= 0 && foodIndex < foodList.size()) {
@@ -24,10 +39,20 @@ public class FoodWaterIntake {
         }
     }
 
+    /**
+     * Adds a water entry to the water list.
+     *
+     * @param water The water entry to be added.
+     */
     public void addWater(WaterEntry water) {
         waterList.add(water);
     }
 
+    /**
+     * Deletes a water entry from the water list by index.
+     *
+     * @param waterIndex The index of the water entry to delete.
+     */
     public void deleteWater(int waterIndex) {
         if (waterIndex >= 0 && waterIndex < waterList.size()) {
             WaterEntry removed = waterList.remove(waterIndex);
@@ -37,7 +62,9 @@ public class FoodWaterIntake {
         }
     }
 
-    // Lists all food/water intake for the current day from 00:00 local time
+    /**
+     * Lists all food and water intake entries for the current day.
+     */
     public void listDailyIntake() {
 
         System.out.println(SEPARATOR);
@@ -47,6 +74,9 @@ public class FoodWaterIntake {
         System.out.println(SEPARATOR);
     }
 
+    /**
+     * Lists all food entries recorded for the current day, including total calories.
+     */
     public void listDailyFoodIntake() {
         LocalDate currentDate = LocalDate.now();
 
@@ -60,7 +90,7 @@ public class FoodWaterIntake {
 
         System.out.println("Food Entries for " + currentDate + ":");
 
-        // Determine if food entry is from current date. If it is, print.
+        // Print each food entry if it was added on the current date
         for (FoodEntry entry : foodList) {
             if (entry.getLocalDate().equals(currentDate)) {
                 System.out.print(listIndex + ". ");
@@ -73,6 +103,9 @@ public class FoodWaterIntake {
         System.out.println("Total daily Calories: " + dailyTotalCalories);
     }
 
+    /**
+     * Lists all water intake entries recorded for the current day, including total water intake.
+     */
     public void listDailyWaterIntake() {
         LocalDate currentDate = LocalDate.now();
 
@@ -86,7 +119,7 @@ public class FoodWaterIntake {
 
         System.out.println("Water Entries for " + currentDate + ":");
 
-        // Determine if food entry is from current date
+        // Print each water entry if it was added on the current date
         for (WaterEntry entry : waterList) {
             if (entry.getLocalDate().equals(currentDate)) {
                 System.out.print(listIndex + ". ");
@@ -99,10 +132,20 @@ public class FoodWaterIntake {
         System.out.println("Total daily water: " + dailyTotalWater + " ml");
     }
 
+    /**
+     * Returns the list of all recorded food entries.
+     *
+     * @return ArrayList of FoodEntry objects.
+     */
     public ArrayList<FoodEntry> getFoodList() {
         return foodList;
     }
 
+    /**
+     * Returns the list of all recorded water entries.
+     *
+     * @return ArrayList of WaterEntry objects.
+     */
     public ArrayList<WaterEntry> getWaterList() {
         return waterList;
     }
