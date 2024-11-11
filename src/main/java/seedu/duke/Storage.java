be7c253dee02d123d554ac36a829a7ee312444ba
Jaden Lim <jadenlimjc@gmail.com>
2024-11-11 16:39:49 +0800

    fixed checkstyle errors

--- src/main/java/seedu/duke/Storage.java
+++ src/main/java/seedu/duke/Storage.java
@@ -20,7 +20,7 @@ import java.util.logging.Logger;
 public class Storage {
     //define filepath
     private static final String FILE_PATH = "./data/EasInternship.txt";
-    private static final Logger logger = Logger.getLogger("EasInternship");
+    private static final Logger LOGGER = Logger.getLogger("EasInternship");
 
 
     /**
@@ -34,7 +34,7 @@ public class Storage {
             //create directory if file does not exist
             if (!dir.exists()) {
                 dir.mkdirs();
-                logger.log(Level.INFO, "Directory created");
+                LOGGER.log(Level.INFO, "Directory created");
             }
             FileWriter writer = new FileWriter(FILE_PATH);
             List<Internship> internships = internshipList.getAllInternships();
@@ -73,12 +73,12 @@ public class Storage {
                 writer.write(" " + favInternship.getId());
             }
             writer.write("\n");
-            logger.log(Level.INFO, "Data saved");
+            LOGGER.log(Level.INFO, "Data saved");
 
             writer.close();
         } catch (IOException e) {
             System.out.println("Error while saving tasks: " + e.getMessage());
-            logger.log(Level.WARNING, "Error while saving tasks", e);
+            LOGGER.log(Level.WARNING, "Error while saving tasks", e);
         }
     }
 
@@ -90,7 +90,7 @@ public class Storage {
         File file = new File(FILE_PATH);
         if (!file.exists()) {
             System.out.println("No data file found.");
-            logger.log(Level.INFO, "No data file found.");
+            LOGGER.log(Level.INFO, "No data file found.");
             return;
         }
         List<Integer> favouriteIds = new ArrayList<>();
@@ -142,15 +142,15 @@ public class Storage {
                     if (favInternship != null) {
                         internshipList.favouriteInternships.add(favInternship);
                     } else {
-                        logger.log(Level.WARNING, "Internship with ID " + favInternshipId + " not found.");
+                        LOGGER.log(Level.WARNING, "Internship with ID " + favInternshipId + " not found.");
                     }
 
                 }
             }
-            logger.log(Level.INFO, "Data loaded");
+            LOGGER.log(Level.INFO, "Data loaded");
         } catch (IOException e) {
             System.out.println("Error while loading tasks: " + e.getMessage());
-            logger.log(Level.WARNING, "Error while loading tasks", e);
+            LOGGER.log(Level.WARNING, "Error while loading tasks", e);
         }
     }
 
