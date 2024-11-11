dddbd4d60f3c855ba055958a72b4a6d532447ce5
Ayushi <e0905342@u.nus.edu>
2024-11-11 13:19:58 +0800

    calculator comments

--- src/main/java/fittrack/calculator/SitAndReachCalculator.java
+++ src/main/java/fittrack/calculator/SitAndReachCalculator.java
@@ -8,19 +8,37 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class SitAndReachCalculator extends Calculator {
+    // Static map to hold the sit and reach data for both genders and age groups
     private static final Map<LookUpKey, TreeMap<Integer, Integer>> sitAndReachTable = new HashMap<>();
+
+    // Flag to determine whether the values should be sorted in descending order
     private static final boolean SHOULD_SORT_DESCENDING = true;
 
+    // Static block to initialize male and female data
     static {
         initialiseMaleData();
         initialiseFemaleData();
     }
 
+    /**
+     * Calculates the points based on the gender, age, and the sit and reach length.
+     *
+     * @param gender the gender of the individual
+     * @param age the age of the individual
+     * @param length the length achieved in the sit and reach test
+     * @return the points corresponding to the length achieved
+     */
     public static int calculatePoints(Gender gender, int age, int length) {
+        // Retrieves the points from the lookup table based on gender, age, and length
         return getPointsFromTable(sitAndReachTable, gender, age, length, IS_HIGHER_NUMBER_BETTER);
     }
 
+    /**
+     * Initializes the male data for the sit and reach test, adding the data for each age group
+     * with the corresponding length values and performance points.
+     */
     private static void initialiseMaleData() {
+        // 2D array of age-specific sit and reach data for males
         int[][][] ageTables = {
                 {{40, 5}, {36, 4}, {32, 3}, {28, 2}, {23, 1}},
                 {{42, 5}, {38, 4}, {34, 3}, {30, 2}, {25, 1}},
@@ -32,10 +50,16 @@ public class SitAndReachCalculator extends Calculator {
                 {{49, 5}, {45, 4}, {41, 3}, {37, 2}, {32, 1}},
                 {{48, 5}, {44, 4}, {40, 3}, {36, 2}, {32, 1}}
         };
+        // Adds the male data to the lookup table
         addAllTables(sitAndReachTable, Gender.MALE, ageTables, SHOULD_SORT_DESCENDING);
     }
 
+    /**
+     * Initializes the female data for the sit and reach test, adding the data for each age group
+     * with the corresponding length values and performance points.
+     */
     private static void initialiseFemaleData() {
+        // 2D array of age-specific sit and reach data for females
         int[][][] ageTables = {
                 {{40, 5}, {37, 4}, {34, 3}, {30, 2}, {25, 1}},
                 {{42, 5}, {38, 4}, {36, 3}, {32, 2}, {27, 1}},
@@ -47,6 +71,7 @@ public class SitAndReachCalculator extends Calculator {
                 {{47, 5}, {44, 4}, {41, 3}, {36, 2}, {32, 1}},
                 {{44, 5}, {41, 4}, {38, 3}, {35, 2}, {31, 1}}
         };
+        // Adds the female data to the lookup table
         addAllTables(sitAndReachTable, Gender.FEMALE, ageTables, SHOULD_SORT_DESCENDING);
     }
 }
