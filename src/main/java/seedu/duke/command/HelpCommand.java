2bf749a64baa6d2b620f6fae8d437a0addef2518
limkongkiat <102126318+limkongkiat@users.noreply.github.com>
2024-11-11 20:50:40 +0800

    Update Help Command and corresponding test

--- src/main/java/seedu/duke/command/HelpCommand.java
+++ src/main/java/seedu/duke/command/HelpCommand.java
@@ -30,12 +30,17 @@ public class HelpCommand extends Command{
                 "3. income DESCRIPTION /a AMOUNT [/d DATE] [/c CATEGORY]\n" +
                 "   - Adds a new income with an optional date and category\n" +
                 "    - Categories include: SALARY, INVESTMENT, GIFT, OTHER, UNCATEGORIZED\n" +
-                "4. edit INDEX [/des DESCRIPTION] [/a AMOUNT] [/d DATE] [/c CATEGORY]\n" +
+                "4. edit [INDEX] [/des DESCRIPTION] [/a AMOUNT] [/d DATE] [/c CATEGORY]\n" +
                 "   - Edits the transaction at the specified INDEX with optional fields\n" +
-                "5. delete INDEX\n" +
+                "    - If no INDEX is specified, last amended transaction will be edited by default\n" +
+                "5. delete [INDEX] [/to END_INDEX]\n" +
                 "   - Deletes the transaction at the specified INDEX\n" +
-                "6. budget\n" +
+                "    - If no INDEX is specified, last last amended transaction will be deleted by default\n" +
+                "    - If /to flag is included: Deletes transactions INDEX to END_INDEX inclusive\n" +
+                "    - Bonus: delete all - deletes all transactions\n" +
+                "6. budget [BUDGET]\n" +
                 "   - Set/modify your monthly budget\n" +
+                "   - Delete set budget by setting BUDGET to 0\n" +
                 "7. exit\n" +
                 "   - Exits the program\n" +
                 "8. help\n" +
